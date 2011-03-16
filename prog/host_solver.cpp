bbc56db005d28f98ed00a0dda2125f08b4229622
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2011-03-16 08:50:24 +0100

    CP: implemented CG in normal and eoprec-form. Testing could
    only be done with the diagonal part since the fermionmatrix is not 
    hermitian

--- prog/host_solver.cpp
+++ prog/host_solver.cpp
@@ -1,9 +1,8 @@
-//!!CP: LZ should update this...
-
 #include "host_solver.h"
 
 hmc_error solver(hmc_spinor_field* in, hmc_spinor_field* out, hmc_spinor_field* b, hmc_gaugefield* gaugefield, hmc_float kappa, hmc_float mu, hmc_float theta, hmc_float chem_pot_re, hmc_float chem_pot_im, int cgmax){
   //CP: in the end this should be done with a compiler option
+  int use_cg = 0;
   if(!use_eo){
     hmc_eoprec_spinor_field even[EOPREC_SPINORFIELDSIZE];
     hmc_eoprec_spinor_field odd[EOPREC_SPINORFIELDSIZE];
@@ -20,10 +19,13 @@ hmc_error solver(hmc_spinor_field* in, hmc_spinor_field* out, hmc_spinor_field* 
     create_point_source_eoprec(be,bo,1,0,0,kappa,mu,theta, chem_pot_re, chem_pot_im, gaugefield);
 		
 		//calculate even-solution even
+		if(use_cg)
+			cg_eoprec(even, be, gaugefield, kappa, mu, theta, chem_pot_re, chem_pot_im, cgmax);
+		else
     bicgstab_eoprec(even, be, gaugefield, kappa, mu, theta, chem_pot_re, chem_pot_im, cgmax);
 		
 		//desired solution is g = L^-1 x
-		//calculate L^-1x_even 
+		//calculate L^-1x_even = g_even
     hmc_eoprec_spinor_field spintmp[EOPREC_SPINORFIELDSIZE];
     dslash_eoprec(even, tmp,gaugefield,kappa, theta, chem_pot_re, chem_pot_im, ODD); //use newood as tmp variable
     M_inverse_sitediagonal(tmp, spintmp,kappa,mu);
@@ -31,26 +33,30 @@ hmc_error solver(hmc_spinor_field* in, hmc_spinor_field* out, hmc_spinor_field* 
 		//calculate odd-solution odd
     M_inverse_sitediagonal(bo, odd, kappa,mu);
 		
+		//calculate g_odd
 		hmc_complex one = hmc_complex_one;
 		saxpy_eoprec(spintmp, odd, &one, odd);
 
     convert_from_kappa_format_eoprec(even,even, kappa);
     convert_from_kappa_format_eoprec(odd, odd, kappa);
+		//write out g
     convert_from_eoprec(even,odd,out);
   
     return HMC_SUCCESS;
   }
   else{
     convert_to_kappa_format(in, kappa);
-    bicgstab(in, b, gaugefield, kappa, mu, theta, chem_pot_re, chem_pot_im, cgmax);
+		if(use_cg)
+			cg(in, b, gaugefield, kappa, mu, theta, chem_pot_re, chem_pot_im, cgmax);
+		else
+			bicgstab(in, b, gaugefield, kappa, mu, theta, chem_pot_re, chem_pot_im, cgmax);
     convert_from_kappa_format(in, out, kappa);
     
     return HMC_SUCCESS;
   }
-  
 }
 
-hmc_error bicgstab(hmc_spinor_field* inout, hmc_eoprec_spinor_field* source, hmc_gaugefield* gaugefield, hmc_float kappa, hmc_float mu, hmc_float theta, hmc_float chem_pot_re, hmc_float chem_pot_im, int cgmax){
+hmc_error bicgstab(hmc_spinor_field* inout, hmc_spinor_field* source, hmc_gaugefield* gaugefield, hmc_float kappa, hmc_float mu, hmc_float theta, hmc_float chem_pot_re, hmc_float chem_pot_im, int cgmax){
 
   //BiCGStab according to hep-lat/9404013
 
@@ -161,7 +167,6 @@ hmc_error bicgstab_eoprec(hmc_eoprec_spinor_field* inout,hmc_eoprec_spinor_field
   hmc_complex minusone = hmc_complex_minusone;
 
   for(int iter=0; iter<cgmax; iter++){
-
     if(iter%iter_refresh==0) {
       //fresh start
       Aee(inout,rn,gaugefield,kappa,mu, theta, chem_pot_re, chem_pot_im);
@@ -174,7 +179,7 @@ hmc_error bicgstab_eoprec(hmc_eoprec_spinor_field* inout,hmc_eoprec_spinor_field
       set_zero_spinorfield_eoprec(v);
       set_zero_spinorfield_eoprec(p);
       
-      printf("true residue squared: %e\n",global_squarenorm_eoprec(rn));
+//       printf("true residue squared: %e\n",global_squarenorm_eoprec(rn));
     }
 
     rho_next = scalar_product_eoprec(rhat,rn);
@@ -204,33 +209,20 @@ hmc_error bicgstab_eoprec(hmc_eoprec_spinor_field* inout,hmc_eoprec_spinor_field
 
     saxsbypz_eoprec(p, s, inout, &alpha, &omega, inout);
   
-    //CP: is this formula right?? There should be no t i guess...
-    //        9. xi = xi−1 + αpi + ωis
-//     for (int n=0; n<EOPREC_SPINORFIELDSIZE; n++) {
-//       tmp1 = complexmult(&omega,&s[n]);
-//       tmp2 = complexmult(&alpha,&p[n]);
-//       tmp3 = complexmult(&omega,&t[n]);
-//       out[n].re += tmp1.re + tmp2.re;
-//       out[n].im += tmp1.im + tmp2.im;
-//       rn[n].re = s[n].re - tmp3.re;
-//       rn[n].im = s[n].im - tmp3.im;
-//     }
-
     hmc_float resid = global_squarenorm_eoprec(rn);
-    printf("%d\t%e\n",iter,resid);
+//     printf("%d\t%e\n",iter,resid);
 
     if(resid<epssquare) {
       hmc_eoprec_spinor_field* aux = new hmc_eoprec_spinor_field[EOPREC_SPINORFIELDSIZE];
       Aee(inout,aux,gaugefield,kappa,mu, theta, chem_pot_re, chem_pot_im);
       saxpy_eoprec(aux, source, &one, aux);
       hmc_float trueresid = global_squarenorm_eoprec(aux);
-      printf("true residue squared: %e\n",trueresid);
+//       printf("true residue squared: %e\n",trueresid);
 
       if(trueresid<epssquare) return HMC_SUCCESS;
 
       delete [] aux;
     }
-
   }
 
   delete [] rn;
@@ -241,3 +233,135 @@ hmc_error bicgstab_eoprec(hmc_eoprec_spinor_field* inout,hmc_eoprec_spinor_field
   delete [] t;
   return HMC_SUCCESS;
 }
+
+hmc_error cg(hmc_spinor_field* inout, hmc_spinor_field* source, hmc_gaugefield* gaugefield, hmc_float kappa, hmc_float mu, hmc_float theta, hmc_float chem_pot_re, hmc_float chem_pot_im, int cgmax){
+ 
+	hmc_spinor_field* rn = new hmc_spinor_field[SPINORFIELDSIZE];
+  hmc_spinor_field* xnn = new hmc_spinor_field[SPINORFIELDSIZE];
+  hmc_spinor_field* pn = new hmc_spinor_field[SPINORFIELDSIZE];
+  hmc_spinor_field* pnn = new hmc_spinor_field[SPINORFIELDSIZE];
+  hmc_spinor_field* rnn = new hmc_spinor_field[SPINORFIELDSIZE];
+  hmc_spinor_field* tmp = new hmc_spinor_field[SPINORFIELDSIZE];
+  hmc_complex alpha;
+	hmc_complex beta;
+	hmc_complex tmp1;
+	hmc_complex tmp2;
+  hmc_complex one = hmc_complex_one;
+  hmc_complex minusone = hmc_complex_minusone;
+	
+	int iter;
+  //main loop
+
+  for(iter = 0; iter < cgmax; iter ++){  
+    if(iter%iter_refresh==0){
+			//fresh start
+			M(inout,rn,gaugefield,kappa,mu, theta, chem_pot_re, chem_pot_im);
+			saxpy(rn, source, &one, rn);
+			copy_spinor(rn, pn);
+			printf("true residue squared: %e\n",global_squarenorm(rn));
+		}
+		
+		M(pn,tmp,gaugefield,kappa,mu, theta, chem_pot_re, chem_pot_im);
+		tmp1 = scalar_product(rn, pn);
+		tmp2 = scalar_product(pn, tmp);
+		alpha = complexdivide(&tmp1, &tmp2);
+		
+		saxpy(tmp, rn, &alpha, rnn);
+		alpha = complexmult(&alpha, &minusone);
+		saxpy(pn, inout, &alpha, xnn);
+		
+		hmc_float resid = global_squarenorm(rnn);
+    
+    if (resid < epssquare){
+			printf("residue small enough: %e\n",resid);
+			copy_spinor(xnn, inout);
+			break;
+    }
+    else{
+			tmp1 = scalar_product(rn, rn);
+			tmp2 = scalar_product(rnn, rnn);
+			beta = complexdivide(&tmp2, &tmp1);
+			beta = complexmult(&beta, &minusone);
+			saxpy(pn, rnn, &beta, pnn);
+			//copy spinors for next iteration
+			copy_spinor(pnn, pn);
+			copy_spinor(rnn, rn);
+			copy_spinor(xnn, inout);
+    }
+  }
+	
+	delete [] rn;
+	delete [] xnn;
+	delete [] pn;
+	delete [] pnn;
+	delete [] rnn;
+	delete [] tmp;
+	
+	return HMC_SUCCESS;
+}
+
+hmc_error cg_eoprec(hmc_eoprec_spinor_field* inout, hmc_eoprec_spinor_field* source, hmc_gaugefield* gaugefield, hmc_float kappa, hmc_float mu, hmc_float theta, hmc_float chem_pot_re, hmc_float chem_pot_im, int cgmax){
+ 
+	hmc_eoprec_spinor_field* rn = new hmc_eoprec_spinor_field[EOPREC_SPINORFIELDSIZE];
+  hmc_eoprec_spinor_field* xnn = new hmc_eoprec_spinor_field[EOPREC_SPINORFIELDSIZE];
+  hmc_eoprec_spinor_field* pn = new hmc_eoprec_spinor_field[EOPREC_SPINORFIELDSIZE];
+  hmc_eoprec_spinor_field* pnn = new hmc_eoprec_spinor_field[EOPREC_SPINORFIELDSIZE];
+  hmc_eoprec_spinor_field* rnn = new hmc_eoprec_spinor_field[EOPREC_SPINORFIELDSIZE];
+  hmc_eoprec_spinor_field* tmp = new hmc_eoprec_spinor_field[EOPREC_SPINORFIELDSIZE];
+  hmc_complex alpha;
+	hmc_complex beta;
+	hmc_complex tmp1;
+	hmc_complex tmp2;
+  hmc_complex one = hmc_complex_one;
+  hmc_complex minusone = hmc_complex_minusone;
+	
+	int iter;
+
+  for(iter = 0; iter < cgmax; iter ++){  
+    if(iter%iter_refresh==0){
+			//fresh start
+			Aee(inout,rn,gaugefield,kappa,mu, theta, chem_pot_re, chem_pot_im);
+			saxpy_eoprec(rn, source, &one, rn);
+			copy_spinor_eoprec(rn, pn);
+// 			printf("true residue squared: %e\n",global_squarenorm(rn));
+		}
+		
+		Aee(pn,tmp,gaugefield,kappa,mu, theta, chem_pot_re, chem_pot_im);
+		tmp1 = scalar_product_eoprec(rn, pn);
+		tmp2 = scalar_product_eoprec(pn, tmp);
+		alpha = complexdivide(&tmp1, &tmp2);
+		
+		saxpy_eoprec(tmp, rn, &alpha, rnn);
+		alpha = complexmult(&alpha, &minusone);
+		saxpy_eoprec(pn, inout, &alpha, xnn);
+		
+		hmc_float resid = global_squarenorm_eoprec(rnn);
+    
+    if (resid < epssquare){
+			printf("residue small enough: %e\n",resid);
+			copy_spinor_eoprec(xnn, inout);
+			break;
+    }
+    else{
+			tmp1 = scalar_product_eoprec(rn, rn);
+			tmp2 = scalar_product_eoprec(rnn, rnn);
+			beta = complexdivide(&tmp2, &tmp1);
+			beta = complexmult(&beta, &minusone);
+			saxpy_eoprec(pn, rnn, &beta, pnn);
+			//copy spinors for next iteration
+			copy_spinor_eoprec(pnn, pn);
+			copy_spinor_eoprec(rnn, rn);
+			copy_spinor_eoprec(xnn, inout);
+    }
+  }
+	
+	delete [] rn;
+	delete [] xnn;
+	delete [] pn;
+	delete [] pnn;
+	delete [] rnn;
+	delete [] tmp;
+	
+	return HMC_SUCCESS;
+}
+
--- prog/host_solver.h
+++ prog/host_solver.h
@@ -27,5 +27,8 @@ hmc_error solver(hmc_spinor_field* in, hmc_spinor_field* out, hmc_spinor_field* 
 
 hmc_error bicgstab(hmc_spinor_field* inout,hmc_spinor_field* source,hmc_gaugefield* gaugefield,hmc_float kappa,hmc_float mu, hmc_float theta, hmc_float chem_pot_re, hmc_float chem_pot_im, int cgmax);
 hmc_error bicgstab_eoprec(hmc_eoprec_spinor_field* inout,hmc_eoprec_spinor_field* source,hmc_gaugefield* gaugefield,hmc_float kappa,hmc_float mu, hmc_float theta, hmc_float chem_pot_re, hmc_float chem_pot_im,int cgmax);
-  
+
+hmc_error cg(hmc_spinor_field* inout, hmc_eoprec_spinor_field* source, hmc_gaugefield* gaugefield, hmc_float kappa, hmc_float mu, hmc_float theta, hmc_float chem_pot_re, hmc_float chem_pot_im, int cgmax);
+hmc_error cg_eoprec(hmc_eoprec_spinor_field* inout, hmc_eoprec_spinor_field* source, hmc_gaugefield* gaugefield, hmc_float kappa, hmc_float mu, hmc_float theta, hmc_float chem_pot_re, hmc_float chem_pot_im, int cgmax);
+
 #endif
