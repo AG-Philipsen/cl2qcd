fe079ac9c227e031d4e8f60e6a387a713140e6de
Matthias Bach <bach@compeng.uni-frankfurt.de>
2011-03-24 14:26:39 +0100

    Make Doxygen include klepsydra documentation

---
+++ prog/hmc.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file
+ *
+ * Documentation file for OpTiMaL.
+ */
+
+/** \mainpage OpTiMaL
+ *
+ * \section intro_sec Introduction
+ *
+ * OpTiMaL is the application for OpenCL based Twisted Mass Lattice QCD.
+ */
+
