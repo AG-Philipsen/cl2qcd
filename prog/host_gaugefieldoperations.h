7fafe3c749b07cc2988e85fee72e982be994181c
Matthias Bach <bach@compeng.uni-frankfurt.de>
2011-05-03 14:53:02 +0200

    Code cleanup

--- prog/host_gaugefieldoperations.h
+++ prog/host_gaugefieldoperations.h
@@ -27,6 +27,17 @@ extern string const version;
 
 void print_info_source(sourcefileparameters* params);
 
+/**
+ * Initialize the gaugefield as specified by the input file.
+ *
+ * @param[out]    gaugefield The gaugefield to be initialized
+ * @param[in]     parameters The parsed input file
+ * @param[in,out] timer      Timer for logging of execution time
+ * @return Error code as defined in hmcerrs.h:
+ *         @li HMC_XMLERROR     if file to read gaugefield from cannot be parsed
+ *         @li HMC_INVALIDVALUE if no valid start condition has been selected
+ *         @li HMC_SUCCESS otherwise
+ */
 hmc_error init_gaugefield(hmc_gaugefield* gaugefield, inputparameters* parameters, usetimer* timer);
 
 hmc_error save_gaugefield(hmc_gaugefield* gaugefield, inputparameters* parameters, int number);
