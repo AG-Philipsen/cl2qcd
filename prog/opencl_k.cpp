bcf0b073f117ad6a4328cbfc30c0f5fac45060c4
Christian Schäfer <cschaefer@th.physik.uni-frankfurt.de>
2011-05-19 16:22:53 +0200

    Added kappa measurement to opencl_k.h/.cpp

--- prog/opencl_k.cpp
+++ prog/opencl_k.cpp
@@ -17,10 +17,107 @@ hmc_error Opencl_k::fill_collect_options(stringstream* collect_options){
 
 hmc_error Opencl_k::fill_buffers(){
 	hmc_error err = Opencl::fill_buffers();
+	cl_int clerr = CL_SUCCESS;
+	
+	cout << "Create buffer for transport coefficient kappa_karsch..." << endl;
+	clmem_kappa_karsch = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(hmc_float) * global_work_size, 0, &clerr);
+	if(clerr != CL_SUCCESS) {
+		cout << "... failed, aborting." << endl;
+		exit(HMC_OCLERROR);
+	}
+	
+	cout << "Create buffer for transport coefficient kappa_clover..." << endl;
+	clmem_kappa_clover = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(hmc_float) * global_work_size, 0, &clerr);
+	if(clerr != CL_SUCCESS) {
+		cout << "... failed, aborting." << endl;
+		exit(HMC_OCLERROR);
+	}
+	
 	return HMC_SUCCESS;  
 }
 
 hmc_error Opencl_k::fill_kernels(){
   	hmc_error err = Opencl::fill_kernels();
+	cl_int clerr = CL_SUCCESS;
+	
+	cout << "Create TK kappa kernels..." << endl;
+	kappa_karsch_gpu = clCreateKernel(clprogram, "kappa_karsch_gpu", &clerr);
+	if(clerr != CL_SUCCESS) {
+		cout << "... failed, aborting." << endl;
+		exit(HMC_OCLERROR);
+	}
+	
+	cout << "Create TK clover kernels..." << endl;
+	kappa_clover_gpu = clCreateKernel(clprogram, "kappa_clover_gpu", &clerr);
+	if(clerr != CL_SUCCESS) {
+		cout << "... failed, aborting." << endl;
+		exit(HMC_OCLERROR);
+	}
+	
+	
 	return HMC_SUCCESS;  
 }
+
+
+
+hmc_error Opencl_k::run_kappa_karsch_gpu(const hmc_float beta, usetimer * const timer, hmc_float * kappa_karsch_out)
+{
+	//variables
+	cl_int clerr = CL_SUCCESS;
+	timer->reset();
+	const cl_uint num_groups = (global_work_size + local_work_size - 1) / local_work_size;
+	
+#ifdef _USE_GPU_
+	size_t global_work_size = min(VOLSPACE * NTIME / 2, NUMRNDSTATES);
+#else
+	size_t global_work_size = min(max_compute_units, (cl_uint) NUMRNDSTATES);
+#endif
+	
+	global_work_size = local_work_size * num_groups;
+	
+	//buffers for kappa
+	// init scratch buffers if not already done
+	int global_buf_size_float = sizeof(hmc_float) * num_groups;
+
+	if( clmem_kappa_karsch_buf_glob == 0 ) {
+		clmem_kappa_karsch_buf_glob = clCreateBuffer(context, CL_MEM_READ_WRITE, global_buf_size_float, 0, &clerr);
+		if(clerr != CL_SUCCESS) {
+			cout << "creating clmem_plaq_buf_glob failed, aborting..." << endl;
+			exit(HMC_OCLERROR);
+		}
+	}
+
+	clerr = clSetKernelArg(kappa_karsch_gpu, 0, sizeof(cl_mem), &clmem_gaugefield);
+	if(clerr != CL_SUCCESS) {
+		cout << "clSetKernelArg0 at heatbath_even failed, aborting..." << endl;
+		exit(HMC_OCLERROR);
+	}
+	clerr = clSetKernelArg(kappa_karsch_gpu, 1, sizeof(hmc_float), &beta);
+	if(clerr != CL_SUCCESS) {
+		cout << "clSetKernelArg1 at heatbath_even failed, aborting..." << endl;
+		exit(HMC_OCLERROR);
+	}
+	clerr = clSetKernelArg(kappa_karsch_gpu, 2, sizeof(cl_mem), &clmem_kappa_karsch_buf_glob);
+	if(clerr != CL_SUCCESS) {
+		cout << "clSetKernelArg3 at heatbath_even failed, aborting..." << endl;
+		exit(HMC_OCLERROR);
+	}
+	
+	enqueueKernel(kappa_karsch_gpu, global_work_size, local_work_size);
+
+	//read out values
+	clerr = clEnqueueReadBuffer(queue, clmem_kappa_karsch, CL_FALSE, 0, sizeof(hmc_float), kappa_karsch_out, 0, NULL, NULL);
+	if(clerr != CL_SUCCESS) {
+		cout << "... failed, aborting." << endl;
+		exit(HMC_OCLERROR);
+	}
+	
+	// wait for results to have been read back
+	clFinish(queue);
+	
+	timer->add();
+	return HMC_SUCCESS;
+
+}
+
+
--- prog/opencl_k.h
+++ prog/opencl_k.h
@@ -36,37 +36,16 @@ class Opencl_k : public Opencl {
 	 */
 	virtual hmc_error fill_kernels();
       
-      
-      
-//     private:
-//         std::vector<std::string> cl_kernels_file;
-// 	int isinit;
-// 	cl_context context;
-// 	cl_command_queue queue;
-// 	cl_program clprogram;
-// 	cl_kernel heatbath_odd;
-// 	cl_kernel heatbath_even;
-// 	cl_kernel overrelax_odd;
-// 	cl_kernel overrelax_even;
-// 	cl_kernel plaquette;
-// 	cl_kernel plaquette_reduction;
-// 	cl_kernel polyakov;
-// 	cl_kernel polyakov_reduction;
-// 
-// 	//heatbath variables
-// 	cl_mem clmem_gaugefield;
-// 	cl_mem clmem_rndarray;
-// 	cl_mem clmem_plaq;
-// 	cl_mem clmem_plaq_buf_glob;
-// 	cl_mem clmem_splaq_buf_glob;
-// 	cl_mem clmem_tplaq_buf_glob;
-// 	cl_mem clmem_splaq;
-// 	cl_mem clmem_tplaq;
-// 	cl_mem clmem_polyakov;
-// 	cl_mem clmem_polyakov_buf_glob;
-// 	//!!CP: this is not needed at the moment and since is not copied to the device anywhere!!
-// 	cl_mem clmem_theta_gaugefield;
-  
+	hmc_error run_kappa_karsch_gpu(const hmc_float beta, usetimer * const timer, hmc_float * kappa_karsch_out);
+	
+	cl_mem clmem_kappa_karsch;
+	cl_mem clmem_kappa_karsch_buf_glob;
+	cl_mem clmem_kappa_clover;
+	
+	
+	cl_kernel kappa_karsch_gpu;
+	cl_kernel kappa_clover_gpu;
+ 
 };
 
 #endif
