784e93943ffeb0d706fccd176b6d0bc6cdf78e4a
Matthias Bach <bach@compeng.uni-frankfurt.de>
2011-06-09 18:15:25 +0200

    Source format

--- prog/gaugefield_inversion.cpp
+++ prog/gaugefield_inversion.cpp
@@ -1,109 +1,114 @@
 #include "gaugefield_inversion.h"
 
 
-hmc_error Gaugefield_inversion::init_devices(cl_device_type* devicetypes, usetimer* timer){
-  if(get_num_ocl_devices() != 1) {
-    //LZ: so far, we only use !!! 1 !!! device
-    //this needs generalisation to several devices and subsets!!!!!
-    cerr << "only 1 device possible..." << endl;
-  }
-  
-  if(get_num_ocl_devices() > 0) {
-    Opencl_fermions* dev_tmp = new Opencl_fermions[get_num_ocl_devices()];
-    set_devices(dev_tmp);
-  }
-
-  
-  for(int n = 0; n < get_num_ocl_devices(); n++) {
-	  cout << "init device #" << n << endl;
-	  get_devices_fermions()[n].init(devicetypes[n], timer, get_parameters());	  
-  }
-  return HMC_SUCCESS;
-}
-
-hmc_error Gaugefield_inversion::finalize(){
-  hmc_error err = HMC_SUCCESS;
-  err |= Gaugefield::finalize();
-  for(int n = 0; n < get_num_ocl_devices(); n++)
-    err |= get_devices_fermions()[n].finalize_fermions();	  
-  return err;
-}
-
-hmc_error Gaugefield_inversion::free_devices(){
-  if(get_num_ocl_devices() > 0)
-    delete [] get_devices_fermions();
-  return HMC_SUCCESS;
-}
-
-Opencl_fermions * Gaugefield_inversion::get_devices_fermions (){
-  return  (Opencl_fermions*)get_devices();
-}
-
-hmc_error Gaugefield_inversion::perform_inversion_pointsource_ps_corr_host(){
-  //CP: one needs bicgstab here for M
-  int use_cg = FALSE;
-	
-  int use_eo = get_parameters()->get_use_eo();
-
-  hmc_spinor_field in[SPINORFIELDSIZE];
-  hmc_spinor_field phi[SPINORFIELDSIZE];
-  init_spinorfield_cold(in);
-
-  //pseudo scalar, flavour multiplet
-  hmc_complex correlator_ps[NSPACE];
-  for(int z=0; z<NSPACE; z++) {
-    correlator_ps[z].re = 0;
-    correlator_ps[z].im = 0;
-  }
-
-  for(int k=0; k<NC*NSPIN; k++) {
-    if(use_eo == FALSE){
-      hmc_spinor_field b[SPINORFIELDSIZE];
-      create_point_source(get_parameters(),k,0,0,b);
-      solver(get_parameters(), in, b, get_gf(), use_cg, phi);
-    }
-    else{
-      hmc_eoprec_spinor_field be[EOPREC_SPINORFIELDSIZE];
-      hmc_eoprec_spinor_field bo[EOPREC_SPINORFIELDSIZE];
-      
-      create_point_source_eoprec(get_parameters(), k,0,0, get_gf(), be,bo);
-      solver_eoprec(get_parameters(), in, be, bo, get_gf(), use_cg, phi);
-      
-    }
-    for(int timepos = 0; timepos<NTIME; timepos++) {
-      for(int spacepos = 0; spacepos<VOLSPACE; spacepos++) {
-	for(int alpha = 0; alpha<NSPIN; alpha++) {
-	  for(int c = 0; c<NC; c++) {
-	    // int j = spinor_element(alpha,c);
-	    int n = spinor_field_element(alpha, c, spacepos, timepos);
-	    int z = get_spacecoord(spacepos, 3);
-	    hmc_complex tmp = phi[n];
-	    hmc_complex ctmp = complexconj(&tmp);
-	    hmc_complex incr = complexmult(&ctmp,&tmp);
-	    correlator_ps[z].re += incr.re;
-	    correlator_ps[z].im += incr.im;
-	  }
+hmc_error Gaugefield_inversion::init_devices(cl_device_type* devicetypes, usetimer* timer)
+{
+	if(get_num_ocl_devices() != 1) {
+		//LZ: so far, we only use !!! 1 !!! device
+		//this needs generalisation to several devices and subsets!!!!!
+		cerr << "only 1 device possible..." << endl;
 	}
-      }
-    }
-  }
-  
-  printf("pseudo scalar correlator:\n");
-  for(int z=0; z<NSPACE; z++) {
-    printf("%d\t(%e,%e)\n",z,correlator_ps[z].re,correlator_ps[z].im);
-  }
 
-  return HMC_SUCCESS;
+	if(get_num_ocl_devices() > 0) {
+		Opencl_fermions* dev_tmp = new Opencl_fermions[get_num_ocl_devices()];
+		set_devices(dev_tmp);
+	}
+
+
+	for(int n = 0; n < get_num_ocl_devices(); n++) {
+		cout << "init device #" << n << endl;
+		get_devices_fermions()[n].init(devicetypes[n], timer, get_parameters());
+	}
+	return HMC_SUCCESS;
+}
+
+hmc_error Gaugefield_inversion::finalize()
+{
+	hmc_error err = HMC_SUCCESS;
+	err |= Gaugefield::finalize();
+	for(int n = 0; n < get_num_ocl_devices(); n++)
+		err |= get_devices_fermions()[n].finalize_fermions();
+	return err;
+}
+
+hmc_error Gaugefield_inversion::free_devices()
+{
+	if(get_num_ocl_devices() > 0)
+		delete [] get_devices_fermions();
+	return HMC_SUCCESS;
+}
+
+Opencl_fermions * Gaugefield_inversion::get_devices_fermions ()
+{
+	return  (Opencl_fermions*)get_devices();
+}
+
+hmc_error Gaugefield_inversion::perform_inversion_pointsource_ps_corr_host()
+{
+	//CP: one needs bicgstab here for M
+	int use_cg = FALSE;
+
+	int use_eo = get_parameters()->get_use_eo();
+
+	hmc_spinor_field in[SPINORFIELDSIZE];
+	hmc_spinor_field phi[SPINORFIELDSIZE];
+	init_spinorfield_cold(in);
+
+	//pseudo scalar, flavour multiplet
+	hmc_complex correlator_ps[NSPACE];
+	for(int z = 0; z < NSPACE; z++) {
+		correlator_ps[z].re = 0;
+		correlator_ps[z].im = 0;
+	}
+
+	for(int k = 0; k < NC * NSPIN; k++) {
+		if(use_eo == FALSE) {
+			hmc_spinor_field b[SPINORFIELDSIZE];
+			create_point_source(get_parameters(), k, 0, 0, b);
+			solver(get_parameters(), in, b, get_gf(), use_cg, phi);
+		} else {
+			hmc_eoprec_spinor_field be[EOPREC_SPINORFIELDSIZE];
+			hmc_eoprec_spinor_field bo[EOPREC_SPINORFIELDSIZE];
+
+			create_point_source_eoprec(get_parameters(), k, 0, 0, get_gf(), be, bo);
+			solver_eoprec(get_parameters(), in, be, bo, get_gf(), use_cg, phi);
+
+		}
+		for(int timepos = 0; timepos < NTIME; timepos++) {
+			for(int spacepos = 0; spacepos < VOLSPACE; spacepos++) {
+				for(int alpha = 0; alpha < NSPIN; alpha++) {
+					for(int c = 0; c < NC; c++) {
+						// int j = spinor_element(alpha,c);
+						int n = spinor_field_element(alpha, c, spacepos, timepos);
+						int z = get_spacecoord(spacepos, 3);
+						hmc_complex tmp = phi[n];
+						hmc_complex ctmp = complexconj(&tmp);
+						hmc_complex incr = complexmult(&ctmp, &tmp);
+						correlator_ps[z].re += incr.re;
+						correlator_ps[z].im += incr.im;
+					}
+				}
+			}
+		}
+	}
+
+	printf("pseudo scalar correlator:\n");
+	for(int z = 0; z < NSPACE; z++) {
+		printf("%d\t(%e,%e)\n", z, correlator_ps[z].re, correlator_ps[z].im);
+	}
+
+	return HMC_SUCCESS;
 }
 
 
-hmc_error Gaugefield_inversion::perform_inversion_pointsource_ps_corr_devices(usetimer* copytimer, usetimer* singletimer, usetimer* Mtimer, usetimer* scalarprodtimer, usetimer* latimer, usetimer* dslashtimer, usetimer* Mdiagtimer, usetimer* solvertimer){
+hmc_error Gaugefield_inversion::perform_inversion_pointsource_ps_corr_devices(usetimer* copytimer, usetimer* singletimer, usetimer* Mtimer, usetimer* scalarprodtimer, usetimer* latimer, usetimer* dslashtimer, usetimer* Mdiagtimer, usetimer* solvertimer)
+{
 
-  //this uses a BiCGStab inverter on device
+	//this uses a BiCGStab inverter on device
 
 
-  //global and local work sizes;
-  //LZ: should eventually be moved inside opencl_fermions class
+	//global and local work sizes;
+	//LZ: should eventually be moved inside opencl_fermions class
 #ifdef _USE_GPU_
 	const size_t ls = NUM_THREADS; /// @todo have local work size depend on kernel properties (and device? autotune?)
 #else
@@ -120,60 +125,58 @@ hmc_error Gaugefield_inversion::perform_inversion_pointsource_ps_corr_devices(us
 	gs = ls * num_groups;
 
 
-  int use_eo = get_parameters()->get_use_eo();
+	int use_eo = get_parameters()->get_use_eo();
 
-  cout << "calc simple_propagator on the device..." << endl;
-  hmc_spinor_field phi[SPINORFIELDSIZE];
-	
-  hmc_spinor_field in[SPINORFIELDSIZE];
-  if(use_eo==FALSE){
-    init_spinorfield_cold(in);
-    get_devices_fermions()[0].copy_spinorfield_to_device(in, copytimer);
-  }
-  else{
-    //!!CP: this should be fine since only half the field is used but of course it is not nice...
-    init_spinorfield_cold_eoprec(in);
-    get_devices_fermions()[0].copy_eoprec_spinorfield_to_device(in, copytimer);		
-  }
+	cout << "calc simple_propagator on the device..." << endl;
+	hmc_spinor_field phi[SPINORFIELDSIZE];
 
-  //pseudo scalar, flavour multiplet
-  hmc_complex correlator_ps[NSPACE];
-  for(int z=0; z<NSPACE; z++) {
-    correlator_ps[z].re = 0;
-    correlator_ps[z].im = 0;
-  }
-	
-  for(int k=0; k<NC*NSPIN; k++) {
-    if(use_eo == FALSE){
-      get_devices_fermions()[0].create_point_source_device(k,0,0,ls, gs, latimer);
-      get_devices_fermions()[0].solver_device(phi, copytimer, singletimer, Mtimer, scalarprodtimer, latimer, dslashtimer, Mdiagtimer, solvertimer, ls, gs, get_parameters()->get_cgmax());
-    }
-    else{
-      get_devices_fermions()[0].create_point_source_eoprec_device(k,0,0,ls, gs, latimer, dslashtimer, Mdiagtimer);
-      get_devices_fermions()[0].solver_eoprec_device(phi, copytimer, singletimer, Mtimer, scalarprodtimer, latimer, dslashtimer, Mdiagtimer, solvertimer, ls, gs, get_parameters()->get_cgmax());
-    }
-    for(int timepos = 0; timepos<NTIME; timepos++) {
-      for(int spacepos = 0; spacepos<VOLSPACE; spacepos++) {
-	for(int alpha = 0; alpha<NSPIN; alpha++) {
-	  for(int c = 0; c<NC; c++) {
-	    // int j = spinor_element(alpha,c);
-	    int n = spinor_field_element(alpha, c, spacepos, timepos);
-	    int z = get_spacecoord(spacepos, 3);
-	    hmc_complex tmp = phi[n];
-	    hmc_complex ctmp = complexconj(&tmp);
-	    hmc_complex incr = complexmult(&ctmp,&tmp);
-	    correlator_ps[z].re += incr.re;
-	    correlator_ps[z].im += incr.im;
-	  }
+	hmc_spinor_field in[SPINORFIELDSIZE];
+	if(use_eo == FALSE) {
+		init_spinorfield_cold(in);
+		get_devices_fermions()[0].copy_spinorfield_to_device(in, copytimer);
+	} else {
+		//!!CP: this should be fine since only half the field is used but of course it is not nice...
+		init_spinorfield_cold_eoprec(in);
+		get_devices_fermions()[0].copy_eoprec_spinorfield_to_device(in, copytimer);
 	}
-      }
-    }
-  }
-  
-  printf("pseudo scalar correlator:\n");
-  for(int z=0; z<NSPACE; z++) {
-    printf("%d\t(%e,%e)\n",z,correlator_ps[z].re,correlator_ps[z].im);
-  }
-  
-  return HMC_SUCCESS;
+
+	//pseudo scalar, flavour multiplet
+	hmc_complex correlator_ps[NSPACE];
+	for(int z = 0; z < NSPACE; z++) {
+		correlator_ps[z].re = 0;
+		correlator_ps[z].im = 0;
+	}
+
+	for(int k = 0; k < NC * NSPIN; k++) {
+		if(use_eo == FALSE) {
+			get_devices_fermions()[0].create_point_source_device(k, 0, 0, ls, gs, latimer);
+			get_devices_fermions()[0].solver_device(phi, copytimer, singletimer, Mtimer, scalarprodtimer, latimer, dslashtimer, Mdiagtimer, solvertimer, ls, gs, get_parameters()->get_cgmax());
+		} else {
+			get_devices_fermions()[0].create_point_source_eoprec_device(k, 0, 0, ls, gs, latimer, dslashtimer, Mdiagtimer);
+			get_devices_fermions()[0].solver_eoprec_device(phi, copytimer, singletimer, Mtimer, scalarprodtimer, latimer, dslashtimer, Mdiagtimer, solvertimer, ls, gs, get_parameters()->get_cgmax());
+		}
+		for(int timepos = 0; timepos < NTIME; timepos++) {
+			for(int spacepos = 0; spacepos < VOLSPACE; spacepos++) {
+				for(int alpha = 0; alpha < NSPIN; alpha++) {
+					for(int c = 0; c < NC; c++) {
+						// int j = spinor_element(alpha,c);
+						int n = spinor_field_element(alpha, c, spacepos, timepos);
+						int z = get_spacecoord(spacepos, 3);
+						hmc_complex tmp = phi[n];
+						hmc_complex ctmp = complexconj(&tmp);
+						hmc_complex incr = complexmult(&ctmp, &tmp);
+						correlator_ps[z].re += incr.re;
+						correlator_ps[z].im += incr.im;
+					}
+				}
+			}
+		}
+	}
+
+	printf("pseudo scalar correlator:\n");
+	for(int z = 0; z < NSPACE; z++) {
+		printf("%d\t(%e,%e)\n", z, correlator_ps[z].re, correlator_ps[z].im);
+	}
+
+	return HMC_SUCCESS;
 }
--- prog/opencl_k.cpp
+++ prog/opencl_k.cpp
@@ -2,80 +2,84 @@
 
 using namespace std;
 
-hmc_error Opencl_k::fill_kernels_file (){
+hmc_error Opencl_k::fill_kernels_file ()
+{
 	//give a list of all kernel-files
-	hmc_error err =	Opencl::fill_kernels_file();
+	hmc_error err = Opencl::fill_kernels_file();
 	if(err != HMC_SUCCESS) {
 		cout << "... failed, aborting." << endl;
 		exit(HMC_OCLERROR);
 	}
 	cl_kernels_file.push_back("opencl_tk_kappa.cl");
-	return HMC_SUCCESS;  
+	return HMC_SUCCESS;
 }
 
-hmc_error Opencl_k::fill_collect_options(stringstream* collect_options){
+hmc_error Opencl_k::fill_collect_options(stringstream* collect_options)
+{
 
 	hmc_error err = Opencl::fill_collect_options(collect_options);
 	if(err != HMC_SUCCESS) {
 		cout << "... failed, aborting." << endl;
 		exit(HMC_OCLERROR);
 	}
-	return HMC_SUCCESS;  
+	return HMC_SUCCESS;
 }
 
-hmc_error Opencl_k::fill_buffers(){
+hmc_error Opencl_k::fill_buffers()
+{
 	hmc_error err = Opencl::fill_buffers();
-	
+
 	if(err != HMC_SUCCESS) {
 		cout << "... failed, aborting." << endl;
 		exit(HMC_OCLERROR);
 	}
-	
+
 	cl_int clerr = CL_SUCCESS;
-	
+
 	cout << "Create buffer for transport coefficient kappa_karsch..." << endl;
 	clmem_kappa_karsch = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(hmc_float) * global_work_size, 0, &clerr);
 	if(clerr != CL_SUCCESS) {
 		cout << "... failed, aborting." << endl;
 		exit(HMC_OCLERROR);
 	}
-	
+
 	cout << "Create buffer for transport coefficient kappa_clover..." << endl;
 	clmem_kappa_clover = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(hmc_float) * global_work_size, 0, &clerr);
 	if(clerr != CL_SUCCESS) {
 		cout << "... failed, aborting." << endl;
 		exit(HMC_OCLERROR);
 	}
-	
-	return HMC_SUCCESS;  
+
+	return HMC_SUCCESS;
 }
 
-hmc_error Opencl_k::fill_kernels(){
-  	hmc_error err = Opencl::fill_kernels();
-	
+hmc_error Opencl_k::fill_kernels()
+{
+	hmc_error err = Opencl::fill_kernels();
+
 	if(err != HMC_SUCCESS) {
 		cout << "... failed, aborting." << endl;
 		exit(HMC_OCLERROR);
 	}
-	
+
 	cl_int clerr = CL_SUCCESS;
-	
+
 	cout << "Create TK kappa kernels..." << endl;
 	kappa_karsch_gpu = clCreateKernel(clprogram, "kappa_karsch_gpu", &clerr);
 	if(clerr != CL_SUCCESS) {
 		cout << "... failed, aborting." << endl;
 		exit(HMC_OCLERROR);
 	}
-	
+
 	cout << "Create TK clover kernels..." << endl;
 	kappa_clover_gpu = clCreateKernel(clprogram, "kappa_clover_gpu", &clerr);
 	if(clerr != CL_SUCCESS) {
 		cout << "... failed, aborting." << endl;
 		exit(HMC_OCLERROR);
 	}
-	
-	
-	return HMC_SUCCESS;  
+
+
+	return HMC_SUCCESS;
 }
 
 
@@ -85,7 +89,7 @@ hmc_error Opencl_k::run_kappa_karsch_gpu(const hmc_float beta, usetimer * timer,
 	//variables
 	cl_int clerr = CL_SUCCESS;
 	timer->reset();
-	
+
 	// decide on work-sizes
 #ifdef _USE_GPU_
 	const size_t local_work_size = NUM_THREADS; /// @todo have local work size depend on kernel properties (and device? autotune?)
@@ -129,7 +133,7 @@ hmc_error Opencl_k::run_kappa_karsch_gpu(const hmc_float beta, usetimer * timer,
 		cout << "clSetKernelArg3 at kappa_karsch_gpu failed, aborting..." << endl;
 		exit(HMC_OCLERROR);
 	}
-	
+
 	enqueueKernel(kappa_karsch_gpu, global_work_size, local_work_size);
 
 	//read out values
@@ -138,10 +142,10 @@ hmc_error Opencl_k::run_kappa_karsch_gpu(const hmc_float beta, usetimer * timer,
 		cout << "... failed, aborting." << endl;
 		exit(HMC_OCLERROR);
 	}
-	
+
 	// wait for results to have been read back
 	clFinish(queue);
-	
+
 	timer->add();
 	return HMC_SUCCESS;
 
@@ -150,12 +154,12 @@ hmc_error Opencl_k::run_kappa_karsch_gpu(const hmc_float beta, usetimer * timer,
 
 hmc_error Opencl_k::run_kappa_clover_gpu(const hmc_float beta, usetimer * timer, hmc_float * kappa_clover_out)
 {
-	
-	
+
+
 	//variables
 	cl_int clerr = CL_SUCCESS;
 	timer->reset();
-	
+
 	// decide on work-sizes
 #ifdef _USE_GPU_
 	const size_t local_work_size = NUM_THREADS; /// @todo have local work size depend on kernel properties (and device? autotune?)
@@ -171,8 +175,8 @@ hmc_error Opencl_k::run_kappa_clover_gpu(const hmc_float beta, usetimer * timer,
 
 	const cl_uint num_groups = (global_work_size + local_work_size - 1) / local_work_size;
 	global_work_size = local_work_size * num_groups;
-	
-	
+
+
 	//buffers for kappa
 	// init scratch buffers if not already done
 	int global_buf_size_float = sizeof(hmc_float) * num_groups;
@@ -209,17 +213,18 @@ hmc_error Opencl_k::run_kappa_clover_gpu(const hmc_float beta, usetimer * timer,
 		cout << "... failed, aborting." << endl;
 		exit(HMC_OCLERROR);
 	}
-	
+
 	// wait for results to have been read back
 	clFinish(queue);
-	
+
 	timer->add();
 	return HMC_SUCCESS;
 
 }
 
-hmc_error Opencl_k::finalize(){
-     if(get_init_status() == 1) {
+hmc_error Opencl_k::finalize()
+{
+	if(get_init_status() == 1) {
 		if(clFlush(queue) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clFinish(queue) != CL_SUCCESS) exit(HMC_OCLERROR);
 
@@ -233,7 +238,7 @@ hmc_error Opencl_k::finalize(){
 		if(clReleaseKernel(polyakov) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clReleaseKernel(plaquette_reduction) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clReleaseKernel(polyakov_reduction) != CL_SUCCESS) exit(HMC_OCLERROR);
-		
+
 		if(clReleaseKernel(kappa_karsch_gpu) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clReleaseKernel(kappa_clover_gpu) != CL_SUCCESS) exit(HMC_OCLERROR);
 
@@ -246,15 +251,15 @@ hmc_error Opencl_k::finalize(){
 		if(clReleaseMemObject(clmem_tplaq) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clReleaseMemObject(clmem_splaq) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clReleaseMemObject(clmem_polyakov) != CL_SUCCESS) exit(HMC_OCLERROR);
-		
+
 		if(clReleaseMemObject(clmem_kappa_karsch) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clReleaseMemObject(clmem_kappa_clover) != CL_SUCCESS) exit(HMC_OCLERROR);
-		
+
 		if(clmem_plaq_buf_glob) if(clReleaseMemObject(clmem_plaq_buf_glob) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clmem_tplaq_buf_glob) if(clReleaseMemObject(clmem_tplaq_buf_glob) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clmem_splaq_buf_glob) if(clReleaseMemObject(clmem_splaq_buf_glob) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clmem_polyakov_buf_glob) if(clReleaseMemObject(clmem_polyakov_buf_glob) != CL_SUCCESS) exit(HMC_OCLERROR);
-		
+
 		if(clmem_kappa_karsch_buf_glob) if(clReleaseMemObject(clmem_kappa_karsch_buf_glob) != CL_SUCCESS) exit(HMC_OCLERROR);
 		if(clmem_kappa_clover_buf_glob) if(clReleaseMemObject(clmem_kappa_clover_buf_glob) != CL_SUCCESS) exit(HMC_OCLERROR);
 
