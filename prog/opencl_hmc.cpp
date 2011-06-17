9effa7511f7d0ae0f65cdee7f64a5bdb242767df
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2011-06-17 14:07:37 +0200

    Implemented gaussian spinorfield and gaugemomenta wrapper
    functions

--- prog/opencl_hmc.cpp
+++ prog/opencl_hmc.cpp
@@ -130,7 +130,7 @@ hmc_error Opencl_hmc::init_hmc_variables(inputparameters* parameters, usetimer *
 		exit(HMC_OCLERROR);
 	}
 	logger.debug() << "Create kernel generate_gaussian_gaugemomenta...";
-	generate_gaussian_spinorfield = clCreateKernel(clprogram, "generate_gaussian_gaugemomenta", &clerr);
+	generate_gaussian_gaugemomenta = clCreateKernel(clprogram, "generate_gaussian_gaugemomenta", &clerr);
 	if(clerr != CL_SUCCESS) {
 		logger.fatal() << "... failed, aborting.";
 		exit(HMC_OCLERROR);
@@ -142,7 +142,7 @@ hmc_error Opencl_hmc::init_hmc_variables(inputparameters* parameters, usetimer *
 		exit(HMC_OCLERROR);
 	}
 	logger.debug() << "Create kernel md_update_gaugemomenta...";
-	md_update_gaugefield = clCreateKernel(clprogram, "md_update_gaugemomenta", &clerr);
+	md_update_gaugemomenta = clCreateKernel(clprogram, "md_update_gaugemomenta", &clerr);
 	if(clerr != CL_SUCCESS) {
 		logger.fatal() << "... failed, aborting.";
 		exit(HMC_OCLERROR);
@@ -190,7 +190,6 @@ hmc_error Opencl_hmc::finalize_hmc(){
 	if(clReleaseKernel(generate_gaussian_spinorfield)!=CL_SUCCESS) return HMC_RELEASEKERNELERR;
 	if(clReleaseKernel(s_gauge)!=CL_SUCCESS) return HMC_RELEASEKERNELERR;
 	if(clReleaseKernel(s_fermion)!=CL_SUCCESS) return HMC_RELEASEKERNELERR;
-	/** @todo these kernels somehow cant be released, perhaps just because they are empty.. */
 	if(clReleaseKernel(generate_gaussian_gaugemomenta)!=CL_SUCCESS) return HMC_RELEASEKERNELERR;
 	if(clReleaseKernel(md_update_gaugefield)!=CL_SUCCESS) return HMC_RELEASEKERNELERR;
 	if(clReleaseKernel(md_update_gaugemomenta)!=CL_SUCCESS) return HMC_RELEASEKERNELERR;
@@ -203,19 +202,51 @@ hmc_error Opencl_hmc::finalize_hmc(){
 ////////////////////////////////////////////////////
 //Methods needed for the HMC-algorithm
 
-hmc_error Opencl_hmc::generate_gaussian_gaugemomenta_device(const size_t local_work_size, const size_t global_work_size, usetimer * timer){
+hmc_error Opencl_hmc::generate_gaussian_gaugemomenta_device(const size_t ls, const size_t gs, usetimer * timer){
 	(*timer).reset();
-	
-	
+	int clerr;
+	//this is always applied to clmem_new_p
+	clerr = clSetKernelArg(generate_gaussian_gaugemomenta,0,sizeof(cl_mem),&clmem_new_p);
+  if(clerr!=CL_SUCCESS) {
+    cout<<"clSetKernelArg 0 failed, aborting..."<<endl;
+    exit(HMC_OCLERROR);
+  }
+	clerr = clSetKernelArg(generate_gaussian_gaugemomenta,1,sizeof(cl_mem),&clmem_rndarray);
+  if(clerr!=CL_SUCCESS) {
+    cout<<"clSetKernelArg 1 failed, aborting..."<<endl;
+    exit(HMC_OCLERROR);
+  }
+  clerr = clEnqueueNDRangeKernel(queue,generate_gaussian_gaugemomenta,1,0,&gs,&ls,0,0,NULL);
+  if(clerr!=CL_SUCCESS) {
+    cout<<"enqueue generate_gaussian_gaugemomenta kernel failed, aborting..."<<endl;
+    exit(HMC_OCLERROR);
+  }		
+	clFinish(queue);	
 	
 	(*timer).add();
 	return HMC_SUCCESS;
 }
 
-hmc_error Opencl_hmc::generate_gaussian_spinorfield_device(const size_t local_work_size, const size_t global_work_size, usetimer * timer){
+hmc_error Opencl_hmc::generate_gaussian_spinorfield_device(const size_t ls, const size_t gs, usetimer * timer){
 	(*timer).reset();
-	
-	
+	int clerr;
+	//this is always applied to clmem_phi_inv
+	clerr = clSetKernelArg(generate_gaussian_spinorfield,0,sizeof(cl_mem),&clmem_phi_inv);
+  if(clerr!=CL_SUCCESS) {
+    cout<<"clSetKernelArg 0 failed, aborting..."<<endl;
+    exit(HMC_OCLERROR);
+  }
+	clerr = clSetKernelArg(generate_gaussian_spinorfield,1,sizeof(cl_mem),&clmem_rndarray);
+  if(clerr!=CL_SUCCESS) {
+    cout<<"clSetKernelArg 1 failed, aborting..."<<endl;
+    exit(HMC_OCLERROR);
+  }
+  clerr = clEnqueueNDRangeKernel(queue,generate_gaussian_spinorfield,1,0,&gs,&ls,0,0,NULL);
+  if(clerr!=CL_SUCCESS) {
+    cout<<"enqueue generate_gaussian_spinorfield kernel failed, aborting..."<<endl;
+    exit(HMC_OCLERROR);
+  }		
+	clFinish(queue);	
 	
 	(*timer).add();
 	return HMC_SUCCESS;
@@ -237,7 +268,58 @@ hmc_error Opencl_hmc::md_update_spinorfield_device(const size_t local_work_size,
 
 hmc_error Opencl_hmc::leapfrog_device(const size_t local_work_size, const size_t global_work_size, usetimer * timer){
 	(*timer).reset();
-	
+	//it is assumed that gaugefield and gaugemomentum have been set to the old ones already
+// hmc_error leapfrog(inputparameters * parameters, 
+// 									 #ifdef _FERMIONS_
+// 									 hmc_spinor_field * phi, hmc_spinor_field * phi_inv, 
+// 									 #endif
+// 									 hmc_gaugefield * u_out, hmc_algebraelement2 * p_out	){
+// 	// CP: it operates directly on the fields p_out and u_out
+// 	int steps = (*parameters).get_integrationsteps1() ;	
+// 	hmc_float stepsize = ((*parameters).get_tau()) /((hmc_float) steps);
+// 	int k;
+// 	hmc_float stepsize_half = 0.5*stepsize;
+// 	
+// 	hmc_algebraelement2* force_vec = new hmc_algebraelement2[GAUGEMOMENTASIZE2];
+// 
+// 	//initial step
+// 	cout << "\tinitial step:" << endl;
+// 	//here, phi is inverted using the orig. gaugefield
+// 	force(parameters, u_out ,
+// 		#ifdef _FERMIONS_
+// 		phi, phi_inv, 
+// 		#endif
+// 		force_vec);
+// 	hmc_float tmp;
+// 	md_update_gauge_momenta(stepsize_half, p_out, force_vec);
+// 	//intermediate steps
+// 	if(steps > 1) cout << "\tperform " << steps << " intermediate steps " << endl;
+// 	for(k = 1; k<steps; k++){
+// 		md_update_gaugefield(stepsize, p_out, u_out);
+// 		force(parameters, u_out ,
+// 			#ifdef _FERMIONS_
+// 			phi, phi_inv, 
+// 			#endif
+// 			force_vec);
+// 		md_update_gauge_momenta(stepsize, p_out, force_vec);
+// 	}
+// 	
+// 	//final step
+// 	cout << "\tfinal step" << endl;
+// 	md_update_gaugefield(stepsize, p_out, u_out);
+// 	force(parameters, u_out ,
+// 		#ifdef _FERMIONS_
+// 		phi, phi_inv, 
+// 		#endif
+// 		force_vec);
+// 	md_update_gauge_momenta(stepsize_half, p_out,force_vec); 
+// 	
+// 	delete [] force_vec;
+// 	
+// 	cout << "\tfinished leapfrog" << endl;
+// 	return HMC_SUCCESS;
+// }
+
 	
 	
 	(*timer).add();
@@ -248,7 +330,67 @@ hmc_error Opencl_hmc::leapfrog_device(const size_t local_work_size, const size_t
 hmc_error Opencl_hmc::force_device(const size_t local_work_size, const size_t global_work_size, usetimer * timer){
 	(*timer).reset();
 	
+				
+//this has to go into a wrapper function!!
+/*
+//CP: this essentially calculates a hmc_gauge_momentum vector
+//CP: if fermions are used, here is the point where the inversion has to be performed
+hmc_error force(inputparameters * parameters, hmc_gaugefield * field
+	#ifdef _FERMIONS_
+	, hmc_spinor_field * phi, hmc_spinor_field * phi_inv
+	#endif
+	, hmc_algebraelement2 * out){
+	cout << "\t\tstart calculating the force..." << endl;
+	//CP: make sure that the output field is set to zero
+	set_zero_gaugemomenta(out);
+	//add contributions
+	cout << "\t\tcalc gauge_force..." << endl;
+	gauge_force(parameters, field, out);
+#ifdef _FERMIONS_
+	cout << "\t\tinvert fermion field..." << endl;
+	//the algorithm needs two spinor-fields
+	hmc_spinor_field* X = new hmc_spinor_field[SPINORFIELDSIZE];
+	//CP: to begin with, consider only the cg-solver
+	//source is at 0
+	int k = 0;
+	int use_cg = TRUE;
+	//CP: at the moment, use_eo = 0 so that even-odd is not used!!!!!
 	
+	//debugging
+	int err = 0;
+	
+	if(use_cg){
+		if(!use_eo){
+			//the inversion calculates Y = (QplusQminus)^-1 phi = phi_inv
+			hmc_spinor_field b[SPINORFIELDSIZE];
+			create_point_source(parameters,k,0,0,b);
+			cout << "\t\t\tstart solver" << endl;
+			err = solver(parameters, phi, b, field, use_cg, phi_inv);
+			if (err != HMC_SUCCESS) cout << "\t\tsolver did not solve!!" << endl;
+			else cout << "\t\tsolver solved!" << endl;
+		}
+		else{
+			hmc_eoprec_spinor_field be[EOPREC_SPINORFIELDSIZE];
+			hmc_eoprec_spinor_field bo[EOPREC_SPINORFIELDSIZE];
+			
+			create_point_source_eoprec(parameters, k,0,0, field, be,bo);
+			solver_eoprec(parameters, phi, be, bo, field, use_cg, phi_inv);
+		}
+		cout << "\t\t\tcalc X" << endl;
+		//X = Qminus Y = Qminus phi_inv 
+		Qminus(parameters, phi_inv, field, X);
+	}
+	else{
+		//here, one has first to invert (with BiCGStab) Qplus phi = X and then invert Qminus X => Qminus^-1 Qplus^-1 phi = (QplusQminus)^-1 phi = Y = phi_inv
+	}
+/** @todo control the fields here again!!! 
+	fermion_force(parameters, field, phi_inv, X, out);
+	
+	delete [] X;
+#endif
+	return HMC_SUCCESS;
+}
+*/
 	
 	(*timer).add();
 	return HMC_SUCCESS;
