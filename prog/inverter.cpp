5a90cf1ad4e0dd11575af81b782db624f5380797
Matthias Bach <bach@compeng.uni-frankfurt.de>
2011-07-04 15:31:51 +0200

    Source format

--- prog/inverter.cpp
+++ prog/inverter.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 	char* inputfile = argv[1];
 	inputparameters parameters;
 	parameters.readfile(inputfile);
-	print_info(&parameters,&cout);
+	print_info(&parameters, &cout);
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Initialization
@@ -27,44 +27,41 @@ int main(int argc, char* argv[])
 	Gaugefield_inversion gaugefield;
 	cl_device_type devicetypes[parameters.get_num_dev()];
 
-	if(parameters.get_num_dev() == 1){
-	#ifdef _USEGPU_
+	if(parameters.get_num_dev() == 1) {
+#ifdef _USEGPU_
 		devicetypes[0] = CL_DEVICE_TYPE_GPU;
-	#else
+#else
 		devicetypes[0] = CL_DEVICE_TYPE_CPU;
-	#endif
-	}
-	else if(parameters.get_num_dev() == 2){
+#endif
+	} else if(parameters.get_num_dev() == 2) {
 		devicetypes[0] = CL_DEVICE_TYPE_GPU;
 		devicetypes[1] = CL_DEVICE_TYPE_CPU;
-	}
-	else{
+	} else {
 		logger.fatal() << "Number of devices too big, aborting..." ;
 		return HMC_STDERR;
 	}
 	cerr << "init gaugefield" << endl;
 	gaugefield.init(parameters.get_num_dev(), devicetypes, &parameters, &inittime);
 	//cerr << "print initial gaugeobservables..." << endl;
-	//	gaugefield.print_gaugeobservables(&polytime, &plaqtime);
+	//  gaugefield.print_gaugeobservables(&polytime, &plaqtime);
 	cerr << "copy gaugefield" << endl;
 	gaugefield.copy_gaugefield_to_devices(&copytimer);
-	
+
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// inverter
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	cout << "perform inversion on device.." << endl;
 
-	if(parameters.get_num_dev() == 1){
-		gaugefield.perform_inversion_pointsource_ps_corr_devices(&copytimer,&singletimer,&Mtimer,&scalarprodtimer,&latimer,&dslashtimer,&Mdiagtimer,&solvertimer);
+	if(parameters.get_num_dev() == 1) {
+		gaugefield.perform_inversion_pointsource_ps_corr_devices(&copytimer, &singletimer, &Mtimer, &scalarprodtimer, &latimer, &dslashtimer, &Mdiagtimer, &solvertimer);
 		/** @todo improve ls, gs, here*/
-		gaugefield.get_devices_fermions()[0].ps_correlator_device(1,1, &noop);
-	}
-	else{
-		gaugefield.perform_inversion_pointsource_ps_corr_devices(&copytimer,&singletimer,&Mtimer,&scalarprodtimer,&latimer,&dslashtimer,&Mdiagtimer,&solvertimer);
+		gaugefield.get_devices_fermions()[0].ps_correlator_device(1, 1, &noop);
+	} else {
+		gaugefield.perform_inversion_pointsource_ps_corr_devices(&copytimer, &singletimer, &Mtimer, &scalarprodtimer, &latimer, &dslashtimer, &Mdiagtimer, &solvertimer);
 		gaugefield.get_devices_fermions()[0].get_spinorfield_from_device(host_spinorfield, &copytimer);
 		gaugefield.get_devices_fermions()[1].copy_spinorfield_to_device(host_spinorfield, &copytimer);
 		/** @todo improve ls, gs, here*/
-		gaugefield.get_devices_fermions()[1].ps_correlator_device(1,1, &latimer);
+		gaugefield.get_devices_fermions()[1].ps_correlator_device(1, 1, &latimer);
 	}
 	cout << "inversion done" << endl;
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
