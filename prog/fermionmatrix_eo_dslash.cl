AG-Philipsen/cl2qcd

commit 4e8bdeab00fb490d5c133edab7f3936b4d397d7b

split up eoprec-dslash

prog/fermionmatrix_eo_dslash.cl
@@ -1,260 +1,3 @@
-// //"local" dslash working on a particular link (n,t) of an eoprec field
-// //NOTE: each component is multiplied by +KAPPA, so the resulting spinor has to be mutliplied by -1 to obtain the correct dslash!!!
-// //the difference to the "normal" dslash is that the coordinates of the neighbors have to be transformed into an eoprec index
-// spinor inline dslash_eoprec_local(__global spinorfield_eoprec * in,__global ocl_s_gaugefield * field, int n, int t){
-// 	spinor out_tmp, plus;
-// 	int dir, nn, nn_eo;
-// 	su3vec psi, phi;
-// 	Matrixsu3 U;
-// 	hmc_float kappa_minus = KAPPA;
-// 	
-// 	out_tmp = set_spinor_zero();
-// 
-// 	//go through the different directions
-// 	///////////////////////////////////
-// 	// mu = 0
-// 	///////////////////////////////////
-// 	dir = 0;
-// 	///////////////////////////////////
-// 	//mu = +0
-// 	nn = (t+1)%NTIME;
-// 	//transform normal indices to eoprec index
-// 	nn_eo = get_n_eoprec(n, nn);
-// 	plus = get_spinor_from_eoprec_field(in, nn_eo);
-// 	U = field[get_global_link_pos(dir, n, t)];
-// 	///////////////////////////////////
-// 	// Calculate psi/phi = (1 - gamma_0) y
-// 	// with 1 - gamma_0:
-// 	// | 1  0 -1  0 |        |       psi.e0 - psi.e2 |
-// 	// | 0  1  0 -1 |  psi = |       psi.e1 - psi.e3 |
-// 	// |-1  0  1  0 |        |(-1)*( psi.e1 - psi.e3 |
-// 	// | 0 -1  0  1 |        |(-1)*( psi.e0 - psi.e2 |
-// 	///////////////////////////////////
-// 	// psi = 0. component of (1-gamma_0)y
-// 	psi = su3vec_dim(plus.e0, plus.e2);
-// 	// phi = U*psi
-// 	phi =  su3matrix_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
-// 	out_tmp.e2 = su3vec_dim(out_tmp.e2, psi);
-// 	// psi = 1. component of (1-gamma_0)y
-// 	psi = su3vec_dim(plus.e1, plus.e3);
-// 	// phi = U*psi
-// 	phi =  su3matrix_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
-// 	out_tmp.e3 = su3vec_dim(out_tmp.e3, psi);
-// 
-// 	/////////////////////////////////////
-// 	//mu = -0
-// 	nn = (t-1+NTIME)%NTIME;
-// 	//transform normal indices to eoprec index
-// 	nn_eo = get_n_eoprec(n, nn);
-// 	plus = get_spinor_from_eoprec_field(in, nn_eo);
-// 	U = field[get_global_link_pos(dir, n, nn)];
-// 	///////////////////////////////////
-// 	// Calculate psi/phi = (1 + gamma_0) y
-// 	// with 1 + gamma_0:
-// 	// | 1  0  1  0 |       | psi.e0 + i*psi.e2 |
-// 	// | 0  1  0  1 | psi = | psi.e1 + i*psi.e3 |
-// 	// | 1  0  1  0 |       | psi.e1 + i*psi.e2 |
-// 	// | 0  1  0  1 |       | psi.e0 + i*psi.e3 |
-// 	///////////////////////////////////
-// 	// psi = 0. component of (1+gamma_0)y
-// 	psi = su3vec_acc(plus.e0, plus.e2);
-// 	// phi = U*psi
-// 	phi = su3matrix_dagger_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
-// 	out_tmp.e2 = su3vec_acc(out_tmp.e2, psi);
-// 	// psi = 1. component of (1+gamma_0)y
-// 	psi = su3vec_acc(plus.e1, plus.e3);
-// 	// phi = U*psi
-// 	phi = su3matrix_dagger_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
-// 	out_tmp.e3 = su3vec_acc(out_tmp.e3, psi);		
-// 
-// 	//CP: all actions correspond to the mu = 0 ones
-// 	///////////////////////////////////
-// 	// mu = 1
-// 	///////////////////////////////////
-// 	dir = 1;
-// 	
-// 	///////////////////////////////////
-// 	// mu = +1
-// 	nn = get_neighbor(n,dir);
-// 	//transform normal indices to eoprec index
-// 	nn_eo = get_n_eoprec(nn, t);
-// 	plus = get_spinor_from_eoprec_field(in, nn_eo);
-// 	U = field[get_global_link_pos(dir, n, t)];
-// 	///////////////////////////////////
-// 	// Calculate (1 - gamma_1) y
-// 	// with 1 - gamma_1:
-// 	// | 1  0  0 -i |       |      psi.e0 - i*psi.e3  |
-// 	// | 0  1 -i  0 | psi = |      psi.e1 - i*psi.e2  |
-// 	// | 0  i  1  0 |       |(i)*( psi.e1 - i*psi.e2) |
-// 	// | i  0  0  1 |       |(i)*( psi.e0 - i*psi.e3) |
-// 	///////////////////////////////////
-// 	psi = su3vec_dim_i(plus.e0, plus.e3);
-// 	phi = su3matrix_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
-// 	out_tmp.e3 = su3vec_acc_i(out_tmp.e3, psi);
-// 	
-// 	psi = su3vec_dim_i(plus.e1, plus.e2);
-// 	phi = su3matrix_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
-// 	out_tmp.e2 = su3vec_acc_i(out_tmp.e2, psi);		
-// 
-// 	///////////////////////////////////
-// 	//mu = -1
-// 	nn = get_lower_neighbor(n,dir);
-// 	//transform normal indices to eoprec index
-// 	nn_eo = get_n_eoprec(nn, t);
-// 	plus = get_spinor_from_eoprec_field(in, nn_eo);
-// 	U = field[get_global_link_pos(dir, nn, t)];
-// 	///////////////////////////////////
-// 	// Calculate (1 + gamma_1) y
-// 	// with 1 + gamma_1:
-// 	// | 1  0  0  i |       |       psi.e0 + i*psi.e3  |
-// 	// | 0  1  i  0 | psi = |       psi.e1 + i*psi.e2  |
-// 	// | 0 -i  1  0 |       |(-i)*( psi.e1 + i*psi.e2) |
-// 	// |-i  0  0  1 |       |(-i)*( psi.e0 + i*psi.e3) |
-// 	///////////////////////////////////
-// 	psi = su3vec_acc_i(plus.e0, plus.e3);
-// 	phi = su3matrix_dagger_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
-// 	out_tmp.e3 = su3vec_dim_i(out_tmp.e3, psi);
-// 	
-// 	psi = su3vec_acc_i(plus.e1, plus.e2);
-// 	phi = su3matrix_dagger_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
-// 	out_tmp.e2 = su3vec_dim_i(out_tmp.e2, psi);		
-// 	
-// 	///////////////////////////////////
-// 	// mu = 2
-// 	///////////////////////////////////
-// 	dir = 2;
-// 	
-// 	///////////////////////////////////
-// 	// mu = +2
-// 	nn = get_neighbor(n,dir);
-// 	//transform normal indices to eoprec index
-// 	nn_eo = get_n_eoprec(nn, t);
-// 	plus = get_spinor_from_eoprec_field(in, nn_eo);
-// 	U = field[get_global_link_pos(dir, n, t)];
-// 	///////////////////////////////////
-// 	// Calculate (1 - gamma_2) y
-// 	// with 1 - gamma_2:
-// 	// | 1  0  0  1 |       |       psi.e0 + psi.e3  |
-// 	// | 0  1 -1  0 | psi = |       psi.e1 - psi.e2  |
-// 	// | 0 -1  1  0 |       |(-1)*( psi.e1 + psi.e2) |
-// 	// | 1  0  0  1 |       |     ( psi.e0 + psi.e3) |
-// 	///////////////////////////////////
-// 	psi = su3vec_acc(plus.e0, plus.e3);
-// 	phi = su3matrix_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
-// 	out_tmp.e3 = su3vec_acc(out_tmp.e3, psi);
-// 	
-// 	psi = su3vec_dim(plus.e1, plus.e2);
-// 	phi = su3matrix_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
-// 	out_tmp.e2 = su3vec_dim(out_tmp.e2, psi);	
-// 	
-// 	///////////////////////////////////
-// 	//mu = -2
-// 	nn = get_lower_neighbor(n,dir);
-// 	//transform normal indices to eoprec index
-// 	nn_eo = get_n_eoprec(nn, t);
-// 	plus = get_spinor_from_eoprec_field(in, nn_eo);
-// 	U = field[get_global_link_pos(dir, nn, t)];
-// 	///////////////////////////////////
-// 	// Calculate (1 + gamma_2) y
-// 	// with 1 + gamma_2:
-// 	// | 1  0  0 -1 |       |       psi.e0 - psi.e3  |
-// 	// | 0  1  1  0 | psi = |       psi.e1 + psi.e2  |
-// 	// | 0  1  1  0 |       |     ( psi.e1 + psi.e2) |
-// 	// |-1  0  0  1 |       |(-1)*( psi.e0 - psi.e3) |
-// 	///////////////////////////////////
-// 	psi = su3vec_dim(plus.e0, plus.e3);
-// 	phi = su3matrix_dagger_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
-// 	out_tmp.e3 = su3vec_dim(out_tmp.e3, psi);
-// 	
-// 	psi = su3vec_acc(plus.e1, plus.e2);
-// 	phi = su3matrix_dagger_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
-// 	out_tmp.e2 = su3vec_acc(out_tmp.e2, psi);	
-// 
-// 	///////////////////////////////////
-// 	// mu = 3
-// 	///////////////////////////////////
-// 	dir = 3;
-// 	
-// 	///////////////////////////////////
-// 	// mu = +3
-// 	nn = get_neighbor(n,dir);
-// 	//transform normal indices to eoprec index
-// 	nn_eo = get_n_eoprec(nn, t);
-// 	plus = get_spinor_from_eoprec_field(in, nn_eo);
-// 	U = field[get_global_link_pos(dir, n, t)];
-// 	///////////////////////////////////
-// 	// Calculate (1 - gamma_3) y
-// 	// with 1 - gamma_3:
-// 	// | 1  0 -i  0 |        |       psi.e0 - i*psi.e2  |
-// 	// | 0  1  0  i |  psi = |       psi.e1 + i*psi.e3  |
-// 	// | i  0  1  0 |        |   i *(psi.e0 - i*psi.e2) |
-// 	// | 0 -i  0  1 |        | (-i)*(psi.e1 + i*psi.e3) |
-// 	///////////////////////////////////
-// 	psi = su3vec_dim_i(plus.e0, plus.e2);
-// 	phi = su3matrix_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
-// 	out_tmp.e2 = su3vec_acc_i(out_tmp.e2, psi);
-// 	
-// 	psi = su3vec_acc_i(plus.e1, plus.e3);
-// 	phi = su3matrix_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
-// 	out_tmp.e3 = su3vec_dim_i(out_tmp.e3, psi);	
-// 
-// 	///////////////////////////////////
-// 	//mu = -3
-// 	nn = get_lower_neighbor(n,dir);
-// 	//transform normal indices to eoprec index
-// 	nn_eo = get_n_eoprec(nn, t);
-// 	plus = get_spinor_from_eoprec_field(in, nn_eo);
-// 	U = field[get_global_link_pos(dir, nn, t)];
-// 	///////////////////////////////////
-// 	// Calculate (1 + gamma_3) y
-// 	// with 1 + gamma_3:
-// 	// | 1  0  i  0 |       |       psi.e0 + i*psi.e2  |
-// 	// | 0  1  0 -i | psi = |       psi.e1 - i*psi.e3  |
-// 	// |-i  0  1  0 |       | (-i)*(psi.e0 + i*psi.e2) |
-// 	// | 0  i  0  1 |       |   i *(psi.e1 - i*psi.e3) |
-// 	///////////////////////////////////
-// 	psi = su3vec_acc_i(plus.e0, plus.e2);
-// 	phi = su3matrix_dagger_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
-// 	out_tmp.e2 = su3vec_dim_i(out_tmp.e2, psi);
-// 	
-// 	psi = su3vec_dim_i(plus.e1, plus.e3);
-// 	phi = su3matrix_dagger_times_su3vec(U, psi);
-// 	psi = su3vec_times_real(phi, kappa_minus);
-// 	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
-// 	out_tmp.e3 = su3vec_acc_i(out_tmp.e3, psi);
-// 
-// 	return out_tmp;
-// }
 
 //Here, two cases are possible:
 //evenodd = ODD or EVEN
@@ -269,11 +12,21 @@ __kernel void dslash_eoprec(__global spinorfield_eoprec* in, __global spinorfiel
 	int num_groups = get_num_groups(0);
 	int group_id = get_group_id (0);
 	int n,t;
-	spinor out_tmp;
+	spinor out_tmp = set_spinor_zero();
+	spinor out_tmp2 = set_spinor_zero();
 	for(int id_tmp = id; id_tmp < EOPREC_SPINORFIELDSIZE; id_tmp += global_size) {	
 		if(evenodd == ODD) get_odd_site(id_tmp, &n, &t);
 		else get_even_site(id_tmp, &n, &t);
-		out_tmp = dslash_eoprec_local(in, field, n, t);
+		
+		//calc dslash (this includes mutliplication with kappa)
+		out_tmp2 = dslash_local_eoprec_0(in, field, n, t);
+		out_tmp = spinor_dim(out_tmp, out_tmp2);
+		out_tmp2 = dslash_local_eoprec_1(in, field, n, t);
+		out_tmp = spinor_dim(out_tmp, out_tmp2);
+		out_tmp2 = dslash_local_eoprec_2(in, field, n, t);
+		out_tmp = spinor_dim(out_tmp, out_tmp2);
+		out_tmp2 = dslash_local_eoprec_3(in, field, n, t);
+		out_tmp = spinor_dim(out_tmp, out_tmp2);
 		put_spinor_to_eoprec_field(out_tmp, out, id_tmp);
 	}
 }

prog/fermionmatrix_eo.cl
@@ -300,3 +300,337 @@ spinor inline dslash_eoprec_local(__global spinorfield_eoprec * in,__global ocl_
 	return out_tmp;
 }
 
+//"local" dslash working on a particular link (n,t) of an eoprec field
+//NOTE: each component is multiplied by +KAPPA, so the resulting spinor has to be mutliplied by -1 to obtain the correct dslash!!!
+//the difference to the "normal" dslash is that the coordinates of the neighbors have to be transformed into an eoprec index
+spinor inline dslash_eoprec_local_0(__global spinorfield_eoprec * in,__global ocl_s_gaugefield * field, int n, int t){
+	spinor out_tmp, plus;
+	int dir, nn, nn_eo;
+	su3vec psi, phi;
+	Matrixsu3 U;
+	//this is used to save the BC-conditions...
+	hmc_complex bc_tmp;
+	out_tmp = set_spinor_zero();
+
+	//go through the different directions
+	///////////////////////////////////
+	// mu = 0
+	///////////////////////////////////
+	dir = 0;
+	///////////////////////////////////
+	//mu = +0
+	nn = (t+1)%NTIME;
+	//transform normal indices to eoprec index
+	nn_eo = get_n_eoprec(n, nn);
+	plus = get_spinor_from_eoprec_field(in, nn_eo);
+	U = field[get_global_link_pos(dir, n, t)];
+	//if chemical potential is activated, U has to be multiplied by appropiate factor
+#ifdef _CP_REAL_
+	U = multiply_matrixsu3_by_real (U, EXPCPR);
+#endif
+#ifdef _CP_IMAG_
+	hmc_complex cpi_tmp = {COSCPI, SINCPI};
+	U = multiply_matrixsu3_by_complex (U, cpi_tmp );
+#endif
+	bc_tmp.re = KAPPA_TEMPORAL_RE;
+	bc_tmp.im = KAPPA_TEMPORAL_IM;
+	///////////////////////////////////
+	// Calculate psi/phi = (1 - gamma_0) plus/y
+	// with 1 - gamma_0:
+	// | 1  0  1  0 |        | psi.e0 + psi.e2 |
+	// | 0  1  0  1 |  psi = | psi.e1 + psi.e3 |
+	// | 1  0  1  0 |        | psi.e1 + psi.e3 |
+	// | 0  1  0  1 |        | psi.e0 + psi.e2 |
+	///////////////////////////////////
+	// psi = 0. component of (1-gamma_0)y
+	psi = su3vec_acc(plus.e0, plus.e2);
+	// phi = U*psi
+	phi =  su3matrix_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
+	out_tmp.e2 = su3vec_acc(out_tmp.e2, psi);
+	// psi = 1. component of (1-gamma_0)y
+	psi = su3vec_acc(plus.e1, plus.e3);
+	// phi = U*psi
+	phi =  su3matrix_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
+	out_tmp.e3 = su3vec_acc(out_tmp.e3, psi);
+
+	/////////////////////////////////////
+	//mu = -0
+	nn = (t-1+NTIME)%NTIME;
+	//transform normal indices to eoprec index
+	nn_eo = get_n_eoprec(n, nn);
+	plus = get_spinor_from_eoprec_field(in, nn_eo);
+	U = field[get_global_link_pos(dir, n, nn)];
+	//if chemical potential is activated, U has to be multiplied by appropiate factor
+	//this is the same as at mu=0 in the imag. case, since U is taken to be U^+ later:
+	//	(exp(iq)U)^+ = exp(-iq)U^+
+	//as it should be
+	//in the real case, one has to take exp(q) -> exp(-q)
+#ifdef _CP_REAL_
+	U = multiply_matrixsu3_by_real (U, MEXPCPR);
+#endif
+#ifdef _CP_IMAG_
+	hmc_complex cpi_tmp = {COSCPI, SINCPI};
+	U = multiply_matrixsu3_by_complex (U, cpi_tmp );
+#endif
+	//in direction -mu, one has to take the complex-conjugated value of bc_tmp. this is done right here.
+	bc_tmp.re = KAPPA_TEMPORAL_RE;
+	bc_tmp.im = MKAPPA_TEMPORAL_IM;
+	///////////////////////////////////
+	// Calculate psi/phi = (1 + gamma_0) y
+	// with 1 + gamma_0:
+	// | 1  0 -1  0 |       | psi.e0 - psi.e2 |
+	// | 0  1  0 -1 | psi = | psi.e1 - psi.e3 |
+	// |-1  0  1  0 |       | psi.e1 - psi.e2 |
+	// | 0 -1  0  1 |       | psi.e0 - psi.e3 |
+	///////////////////////////////////
+	// psi = 0. component of (1+gamma_0)y
+	psi = su3vec_dim(plus.e0, plus.e2);
+	// phi = U*psi
+	phi = su3matrix_dagger_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
+	out_tmp.e2 = su3vec_dim(out_tmp.e2, psi);
+	// psi = 1. component of (1+gamma_0)y
+	psi = su3vec_dim(plus.e1, plus.e3);
+	// phi = U*psi
+	phi = su3matrix_dagger_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
+	out_tmp.e3 = su3vec_dim(out_tmp.e3, psi);		
+
+	return out_tmp;
+}
+	
+spinor inline dslash_eoprec_local_1(__global spinorfield_eoprec * in,__global ocl_s_gaugefield * field, int n, int t){
+	spinor out_tmp, plus;
+	int dir, nn, nn_eo;
+	su3vec psi, phi;
+	Matrixsu3 U;
+	//this is used to save the BC-conditions...
+	hmc_complex bc_tmp;
+	out_tmp = set_spinor_zero();
+
+	//CP: all actions correspond to the mu = 0 ones
+	///////////////////////////////////
+	// mu = 1
+	///////////////////////////////////
+	dir = 1;
+	
+	///////////////////////////////////
+	// mu = +1
+	nn = get_neighbor(n,dir);
+	//transform normal indices to eoprec index
+	nn_eo = get_n_eoprec(nn, t);
+	plus = get_spinor_from_eoprec_field(in, nn_eo);
+	U = field[get_global_link_pos(dir, n, t)];
+	bc_tmp.re = KAPPA_SPATIAL_RE;
+	bc_tmp.im = KAPPA_SPATIAL_IM;
+	/////////////////////////////////
+	//Calculate (1 - gamma_1) y
+	//with 1 - gamma_1:
+	//| 1  0  0  i |       |       psi.e0 + i*psi.e3  |
+	//| 0  1  i  0 | psi = |       psi.e1 + i*psi.e2  |
+	//| 0  i  1  0 |       |(-i)*( psi.e1 + i*psi.e2) |
+	//| i  0  0  1 |       |(-i)*( psi.e0 + i*psi.e3) |
+	/////////////////////////////////
+	psi = su3vec_acc_i(plus.e0, plus.e3);
+	phi = su3matrix_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
+	out_tmp.e3 = su3vec_dim_i(out_tmp.e3, psi);
+	
+	psi = su3vec_acc_i(plus.e1, plus.e2);
+	phi = su3matrix_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
+	out_tmp.e2 = su3vec_dim_i(out_tmp.e2, psi);		
+
+	///////////////////////////////////
+	//mu = -1
+	nn = get_lower_neighbor(n,dir);
+	//transform normal indices to eoprec index
+	nn_eo = get_n_eoprec(nn, t);
+	plus = get_spinor_from_eoprec_field(in, nn_eo);
+	U = field[get_global_link_pos(dir, nn, t)];
+	//in direction -mu, one has to take the complex-conjugated value of bc_tmp. this is done right here.
+	bc_tmp.re = KAPPA_SPATIAL_RE;
+	bc_tmp.im = MKAPPA_SPATIAL_IM;
+	///////////////////////////////////
+	// Calculate (1 + gamma_1) y
+	// with 1 + gamma_1:
+	// | 1  0  0 -i |       |       psi.e0 - i*psi.e3  |
+	// | 0  1 -i  0 | psi = |       psi.e1 - i*psi.e2  |
+	// | 0  i  1  0 |       |(-i)*( psi.e1 - i*psi.e2) |
+	// | i  0  0  1 |       |(-i)*( psi.e0 - i*psi.e3) |
+	///////////////////////////////////
+	psi = su3vec_dim_i(plus.e0, plus.e3);
+	phi = su3matrix_dagger_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
+	out_tmp.e3 = su3vec_acc_i(out_tmp.e3, psi);
+	
+	psi = su3vec_dim_i(plus.e1, plus.e2);
+	phi = su3matrix_dagger_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
+	out_tmp.e2 = su3vec_acc_i(out_tmp.e2, psi);	
+	
+	return out_tmp;
+}
+	
+spinor inline dslash_eoprec_local_2(__global spinorfield_eoprec * in,__global ocl_s_gaugefield * field, int n, int t){
+	spinor out_tmp, plus;
+	int dir, nn, nn_eo;
+	su3vec psi, phi;
+	Matrixsu3 U;
+	//this is used to save the BC-conditions...
+	hmc_complex bc_tmp;
+	out_tmp = set_spinor_zero();
+
+	///////////////////////////////////
+	// mu = 2
+	///////////////////////////////////
+	dir = 2;
+	
+	///////////////////////////////////
+	// mu = +2
+	nn = get_neighbor(n,dir);
+	//transform normal indices to eoprec index
+	nn_eo = get_n_eoprec(nn, t);
+	plus = get_spinor_from_eoprec_field(in, nn_eo);
+	U = field[get_global_link_pos(dir, n, t)];
+	bc_tmp.re = KAPPA_SPATIAL_RE;
+	bc_tmp.im = KAPPA_SPATIAL_IM;
+	///////////////////////////////////
+	// Calculate (1 - gamma_2) y
+	// with 1 - gamma_2:
+	// | 1  0  0  1 |       |       psi.e0 + psi.e3  |
+	// | 0  1 -1  0 | psi = |       psi.e1 - psi.e2  |
+	// | 0 -1  1  0 |       |(-1)*( psi.e1 + psi.e2) |
+	// | 1  0  0  1 |       |     ( psi.e0 + psi.e3) |
+	///////////////////////////////////
+	psi = su3vec_acc(plus.e0, plus.e3);
+	phi = su3matrix_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
+	out_tmp.e3 = su3vec_acc(out_tmp.e3, psi);
+	
+	psi = su3vec_dim(plus.e1, plus.e2);
+	phi = su3matrix_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
+	out_tmp.e2 = su3vec_dim(out_tmp.e2, psi);	
+	
+	///////////////////////////////////
+	//mu = -2
+	nn = get_lower_neighbor(n,dir);
+	//transform normal indices to eoprec index
+	nn_eo = get_n_eoprec(nn, t);
+	plus = get_spinor_from_eoprec_field(in, nn_eo);
+	U = field[get_global_link_pos(dir, nn, t)];
+	//in direction -mu, one has to take the complex-conjugated value of bc_tmp. this is done right here.
+	bc_tmp.re = KAPPA_SPATIAL_RE;
+	bc_tmp.im = MKAPPA_SPATIAL_IM;
+	///////////////////////////////////
+	// Calculate (1 + gamma_2) y
+	// with 1 + gamma_2:
+	// | 1  0  0 -1 |       |       psi.e0 - psi.e3  |
+	// | 0  1  1  0 | psi = |       psi.e1 + psi.e2  |
+	// | 0  1  1  0 |       |     ( psi.e1 + psi.e2) |
+	// |-1  0  0  1 |       |(-1)*( psi.e0 - psi.e3) |
+	///////////////////////////////////
+	psi = su3vec_dim(plus.e0, plus.e3);
+	phi = su3matrix_dagger_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
+	out_tmp.e3 = su3vec_dim(out_tmp.e3, psi);
+	
+	psi = su3vec_acc(plus.e1, plus.e2);
+	phi = su3matrix_dagger_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
+	out_tmp.e2 = su3vec_acc(out_tmp.e2, psi);	
+
+	return out_tmp;
+}
+	
+spinor inline dslash_eoprec_local_3(__global spinorfield_eoprec * in,__global ocl_s_gaugefield * field, int n, int t){
+	spinor out_tmp, plus;
+	int dir, nn, nn_eo;
+	su3vec psi, phi;
+	Matrixsu3 U;
+	//this is used to save the BC-conditions...
+	hmc_complex bc_tmp;
+	out_tmp = set_spinor_zero();
+
+	///////////////////////////////////
+	// mu = 3
+	///////////////////////////////////
+	dir = 3;
+	
+	///////////////////////////////////
+	// mu = +3
+	nn = get_neighbor(n,dir);
+	//transform normal indices to eoprec index
+	nn_eo = get_n_eoprec(nn, t);
+	plus = get_spinor_from_eoprec_field(in, nn_eo);
+	U = field[get_global_link_pos(dir, n, t)];
+	bc_tmp.re = KAPPA_SPATIAL_RE;
+	bc_tmp.im = KAPPA_SPATIAL_IM;
+	///////////////////////////////////
+	// Calculate (1 - gamma_3) y
+	// with 1 - gamma_3:
+	// | 1  0  i  0 |        |       psi.e0 + i*psi.e2  |
+	// | 0  1  0 -i |  psi = |       psi.e1 - i*psi.e3  |
+	// |-i  0  1  0 |        |   i *(psi.e0 + i*psi.e2) |
+	// | 0  i  0  1 |        | (-i)*(psi.e1 - i*psi.e3) |
+	///////////////////////////////////
+	psi = su3vec_acc_i(plus.e0, plus.e2);
+	phi = su3matrix_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
+	out_tmp.e2 = su3vec_dim_i(out_tmp.e2, psi);
+	
+	psi = su3vec_dim_i(plus.e1, plus.e3);
+	phi = su3matrix_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
+	out_tmp.e3 = su3vec_acc_i(out_tmp.e3, psi);	
+
+	///////////////////////////////////
+	//mu = -3
+	nn = get_lower_neighbor(n,dir);
+	//transform normal indices to eoprec index
+	nn_eo = get_n_eoprec(nn, t);
+	plus = get_spinor_from_eoprec_field(in, nn_eo);
+	U = field[get_global_link_pos(dir, nn, t)];
+	//in direction -mu, one has to take the complex-conjugated value of bc_tmp. this is done right here.
+	bc_tmp.re = KAPPA_SPATIAL_RE;
+	bc_tmp.im = MKAPPA_SPATIAL_IM;
+	///////////////////////////////////
+	// Calculate (1 + gamma_3) y
+	// with 1 + gamma_3:
+	// | 1  0 -i  0 |       |       psi.e0 - i*psi.e2  |
+	// | 0  1  0  i | psi = |       psi.e1 + i*psi.e3  |
+	// | i  0  1  0 |       | (-i)*(psi.e0 - i*psi.e2) |
+	// | 0 -i  0  1 |       |   i *(psi.e1 + i*psi.e3) |
+	///////////////////////////////////
+	psi = su3vec_dim_i(plus.e0, plus.e2);
+	phi = su3matrix_dagger_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e0 = su3vec_acc(out_tmp.e0, psi);
+	out_tmp.e2 = su3vec_acc_i(out_tmp.e2, psi);
+	
+	psi = su3vec_acc_i(plus.e1, plus.e3);
+	phi = su3matrix_dagger_times_su3vec(U, psi);
+	psi = su3vec_times_complex(phi, bc_tmp);
+	out_tmp.e1 = su3vec_acc(out_tmp.e1, psi);
+	out_tmp.e3 = su3vec_dim_i(out_tmp.e3, psi);
+
+	return out_tmp;
+}
+

commit 48ac4d6f074451a830e85a66a871a9507e1a8ab2

Fix errors breaking Inverter1_CPU_TM_EO
There was a compile and a loger error introduced in 4e8bdeab.

prog/fermionmatrix_eo_dslash.cl
@@ -4,28 +4,29 @@
 //	ODD corresponds to the D_oe case: Dslash acts on even indices (the "x+mu" in the formulae) and the function
 //	saves the outcoming spinor with an odd index.
 //	EVEN is then D_eo.
-__kernel void dslash_eoprec(__global spinorfield_eoprec* in, __global spinorfield_eoprec* out, __global ocl_s_gaugefield* field, int evenodd){
+__kernel void dslash_eoprec(__global spinorfield_eoprec* in, __global spinorfield_eoprec* out, __global ocl_s_gaugefield* field, int evenodd)
+{
 	int local_size = get_local_size(0);
 	int global_size = get_global_size(0);
 	int id = get_global_id(0);
 	int loc_idx = get_local_id(0);
 	int num_groups = get_num_groups(0);
 	int group_id = get_group_id (0);
-	int n,t;
-	spinor out_tmp = set_spinor_zero();
-	spinor out_tmp2 = set_spinor_zero();
-	for(int id_tmp = id; id_tmp < EOPREC_SPINORFIELDSIZE; id_tmp += global_size) {	
+	int n, t;
+	for(int id_tmp = id; id_tmp < EOPREC_SPINORFIELDSIZE; id_tmp += global_size) {
 		if(evenodd == ODD) get_odd_site(id_tmp, &n, &t);
 		else get_even_site(id_tmp, &n, &t);
-		
+
+		spinor out_tmp = set_spinor_zero();
+		spinor out_tmp2;
 		//calc dslash (this includes mutliplication with kappa)
-		out_tmp2 = dslash_local_eoprec_0(in, field, n, t);
+		out_tmp2 = dslash_eoprec_local_0(in, field, n, t);
 		out_tmp = spinor_dim(out_tmp, out_tmp2);
-		out_tmp2 = dslash_local_eoprec_1(in, field, n, t);
+		out_tmp2 = dslash_eoprec_local_1(in, field, n, t);
 		out_tmp = spinor_dim(out_tmp, out_tmp2);
-		out_tmp2 = dslash_local_eoprec_2(in, field, n, t);
+		out_tmp2 = dslash_eoprec_local_2(in, field, n, t);
 		out_tmp = spinor_dim(out_tmp, out_tmp2);
-		out_tmp2 = dslash_local_eoprec_3(in, field, n, t);
+		out_tmp2 = dslash_eoprec_local_3(in, field, n, t);
 		out_tmp = spinor_dim(out_tmp, out_tmp2);
 		put_spinor_to_eoprec_field(out_tmp, out, id_tmp);
 	}