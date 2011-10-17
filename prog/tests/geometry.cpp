acfeca31cbf10b6871ce11e637f34c68da833725
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2011-10-17 12:29:55 +0200

    Added geometry test

---
+++ prog/tests/geometry.cpp
@@ -0,0 +1,119 @@
+#include "../opencl_module_fermions.h"
+#include "../gaugefield_hybrid.h"
+
+// use the boost test framework
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Fermionmatrix
+#include <boost/test/unit_test.hpp>
+
+Random rnd(15);
+extern std::string const version;
+std::string const version = "0.1";
+
+class Device : public Opencl_Module {
+
+	cl_kernel testKernel;
+
+public:
+	Device(cl_command_queue queue, inputparameters* params, int maxcomp, string double_ext) : Opencl_Module_Fermions() {
+		Opencl_Module_Fermions::init(queue, 0, params, maxcomp, double_ext); /* init in body for proper this-pointer */
+	};
+	~Device() {
+		finalize();
+	};
+
+	void runTestKernel(cl_mem in, cl_mem out, cl_mem gf, int gs, int ls);
+	void fill_kernels();
+	void clear_kernels();
+};
+
+class Dummyfield : public Gaugefield_hybrid {
+
+public:
+		Dummyfield(cl_device_type device_type) : Gaugefield_hybrid() {
+
+		init(1, device_type, &params);
+	};
+
+	virtual void init_tasks();
+	virtual void finalize_opencl();
+
+	void runTestKernel();
+
+private:
+	void fill_buffers();
+	void clear_buffers();
+	inputparameters params;
+};
+
+
+BOOST_AUTO_TEST_CASE( GEOMETRY )
+{
+	logger.info() << "Init CPU device";
+	Dummyfield dummy(CL_DEVICE_TYPE_GPU);
+	dummy.runTestKernel();
+	logger.info() << "Init GPU device";
+	Dummyfield dummy2(CL_DEVICE_TYPE_GPU);
+	dummy2.runTestKernel();
+	BOOST_MESSAGE("Tested GEOMETRY");
+}
+
+void Dummyfield::init_tasks()
+{
+	opencl_modules = new Opencl_Module* [get_num_tasks()];
+	opencl_modules[0] = new Device(queue[0], get_parameters(), get_max_compute_units(0), get_double_ext(0));
+
+	fill_buffers();
+}
+
+void Dummyfield::finalize_opencl()
+{
+	clear_buffers();
+	Gaugefield_hybrid::finalize_opencl();
+}
+
+void Dummyfield::fill_buffers()
+{
+	return;
+}
+
+void Device::fill_kernels()
+{
+	//one only needs some kernels up to now. to save time during compiling they are put in here by hand
+	Opencl_Module::fill_kernels();
+
+	//to this end, one has to set the needed files by hand
+	basic_opencl_code = ClSourcePackage() << "opencl_header.cl" << "opencl_geometry.cl";
+	testKernel = createKernel("geometry_test") << basic_opencl_code << "geometry_test.cl";
+}
+
+void Dummyfield::clear_buffers()
+{
+}
+
+void Device::clear_kernels()
+{
+	clReleaseKernel(testKernel);
+}
+
+void Device::runTestKernel(cl_mem out, cl_mem in, cl_mem gf, int gs, int ls)
+{
+	cl_int err;
+	
+	enqueueKernel(testKernel, gs, ls);
+}
+
+void Dummyfield::runTestKernel()
+{
+	int gs, ls;
+	if(opencl_modules[0]->get_device_type() == CL_DEVICE_TYPE_GPU) {
+		gs = get_parameters()->get_eoprec_spinorfieldsize();
+		ls = 64;
+	} else if(opencl_modules[0]->get_device_type() == CL_DEVICE_TYPE_CPU) {
+		gs = opencl_modules[0]->get_max_compute_units();
+		ls = 1;
+	}
+	logger.info() << "test kernel with global_work_size: " << gs << " and local_work_size: " << ls;
+	static_cast<Device*>(opencl_modules[0])->runTestKernel(out, in, *(get_clmem_gaugefield()), gs, ls);
+}
+
