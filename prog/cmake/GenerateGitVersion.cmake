a333a5ad48c5a720c51f955307f004c3ae600736
Matthias Bach <bach@compeng.uni-frankfurt.de>
2012-03-07 11:15:11 +0100

    Don't fail build if cmake can't find git

--- prog/cmake/GenerateGitVersion.cmake
+++ prog/cmake/GenerateGitVersion.cmake
@@ -1,17 +1,22 @@
-include(FindGit REQUIRED)
+find_package(Git)
 
-execute_process( COMMAND ${GIT_EXECUTABLE} log -n 1 --pretty=format:%H
-                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-                 OUTPUT_VARIABLE GIT_COMMIT_ID
-                 OUTPUT_STRIP_TRAILING_WHITESPACE )
-execute_process( COMMAND ${GIT_EXECUTABLE} diff --shortstat
-                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-                 OUTPUT_VARIABLE GIT_DIRTY
-                 OUTPUT_STRIP_TRAILING_WHITESPACE )
-if( GIT_DIRTY )
-	set( GIT_COMMIT_ID "${GIT_COMMIT_ID}*" )
-endif( GIT_DIRTY )
+if(GIT_FOUND)
+	execute_process( COMMAND ${GIT_EXECUTABLE} log -n 1 --pretty=format:%H
+	                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+	                 OUTPUT_VARIABLE GIT_COMMIT_ID
+	                 OUTPUT_STRIP_TRAILING_WHITESPACE )
+	execute_process( COMMAND ${GIT_EXECUTABLE} diff --shortstat
+	                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+	                 OUTPUT_VARIABLE GIT_DIRTY
+	                 OUTPUT_STRIP_TRAILING_WHITESPACE )
+	if( GIT_DIRTY )
+		set( GIT_COMMIT_ID "${GIT_COMMIT_ID}*" )
+	endif( GIT_DIRTY )
 
+else(GIT_FOUND)
+	message(WARNING "Failed to find git, can't write git commit id to executables")
+	set(GIT_COMMIT_ID "N/A")
+endif(GIT_FOUND)
 file(WRITE gitcommitid.h.new "#define GIT_COMMIT_ID \"${GIT_COMMIT_ID}\"\n")
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
 		gitcommitid.h.new gitcommitid.h)
