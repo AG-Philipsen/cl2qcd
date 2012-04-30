e9e0aabb02ae5379bf99a8d5288aaba3e6bd5049
Matthias Bach <bach@compeng.uni-frankfurt.de>
2012-04-30 11:55:11 +0200

    Properly seed device PRNG

--- prog/opencl_module_ran.cpp
+++ prog/opencl_module_ran.cpp
@@ -68,7 +68,7 @@ void Opencl_Module_Ran::fill_kernels()
 	size_t ls, gs;
 	cl_uint num_groups;
 	this->get_work_sizes(init_kernel, this->get_device_type(), &ls, &gs, &num_groups);
-	cl_uint seed = 1; //@ FIXME set proper value
+	cl_uint seed = get_parameters()->get_host_seed() + 1 + device_rank; // +1 ensures that seed is not equal even if host and device seed are both 0
 	clerr = clSetKernelArg(init_kernel, 0, sizeof(cl_uint), &seed);
 	if(clerr != CL_SUCCESS) throw Opencl_Error(clerr, "clSetKernelArg", __FILE__, __LINE__);
 	clerr = clSetKernelArg(init_kernel, 1, sizeof(cl_mem), &clmem_rndarray);
