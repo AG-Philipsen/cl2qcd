ed5de0083d92686ab764dc98c891ba85114fe88e
Matthias Bach <bach@compeng.uni-frankfurt.de>
2012-08-23 10:34:59 +0200

    Improved inclusion of subdirectories in build

--- prog/cmake/Macros.cmake
+++ prog/cmake/Macros.cmake
@@ -1,8 +1,26 @@
+# Utility functions for specifying the build of OpTiMaL
+#
+# (c) 2012 Matthias Bach <bach@compeng.uni-frankfurt.de>
+
+# Create a list of files just as you'd use set.
+# Will automatically replace all filenames by absolute ones
 macro(set_abs_paths DEST)
 	set(FILES "${ARGV}")
 	list(REMOVE_AT FILES 0) # get rid of dest value
 	foreach(_FILE ${FILES})
 		get_filename_component(_TMP ${_FILE} ABSOLUTE)
 		list(APPEND ${DEST} ${_TMP})
-	endforeach(_FILE ${FILES})
-endmacro(set_abs_paths DEST)
+	endforeach()
+endmacro()
+
+# Add adds one ore more modules to the library
+# The module must declare a library called MODULE in the
+# subdirectory called MODULE
+macro(add_modules DEST MODULE)
+	set(_MODULES "${ARGV}")
+	list(REMOVE_AT _MODULES 0) # get rid of dest value
+	foreach(_MODULE ${_MODULES})
+		add_subdirectory(${_MODULE})
+	endforeach()
+	target_link_libraries(${DEST} ${_MODULES})
+endmacro()
