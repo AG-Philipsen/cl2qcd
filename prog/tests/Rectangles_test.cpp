cf025a26f7dedae56139149c663c88158f5775b7
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2012-09-27 16:41:18 +0200

    Code cleanup

--- prog/tests/Rectangles_test.cpp
+++ prog/tests/Rectangles_test.cpp
@@ -9,35 +9,45 @@
 
 extern std::string const version;
 std::string const version = "0.1";
-std::string const exec_name = "rectangles";
 
-class Device : public Opencl_Module_Hmc {
-	meta::Counter counter1, counter2, counter3, counter4;
+class TestDevice : public Opencl_Module {
 public:
-	Device(const meta::Inputparameters& params, hardware::Device * device) : Opencl_Module_Hmc(params, device, &counter1, &counter2, &counter3, &counter4) {
-		Opencl_Module_Hmc::init(); /* init in body for proper this-pointer */
-	};
-	~Device() {
-		finalize();
-	};
-	void fill_kernels();
-	void clear_kernels();
+  //  TestDevice(cl_command_queue queue, const meta::Inputparameters& params, int maxcomp, std::string double_ext, unsigned int dev_rank) : Opencl_Module(params) {
+  TestDevice(const meta::Inputparameters& params, hardware::Device * device) : Opencl_Module(params, device) {
+    Opencl_Module::init(); /* init in body for proper this-pointer */
+  };
+  ~TestDevice() {
+    finalize();
+  };
+  void fill_kernels();
+  void clear_kernels();
 };
 
-class Dummyfield : public Gaugefield_hybrid {
+void TestDevice::fill_kernels()
+{
+	Opencl_Module::fill_kernels();
+}
+
+void TestDevice::clear_kernels()
+{
+	Opencl_Module::clear_kernels();
+}
+
+
+class TestGaugefield : public Gaugefield_hybrid {
 public:
-	Dummyfield(const hardware::System * system) : Gaugefield_hybrid(system) {
+	TestGaugefield(const hardware::System * system) : Gaugefield_hybrid(system) {
 		auto inputfile = system->get_inputparameters();
 		init(1, inputfile.get_use_gpu() ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU);
-		meta::print_info_hmc(exec_name.c_str(), inputfile);
+		std::string name = "test program";
+		meta::print_info_hmc(name.c_str(), inputfile);
 		logger.info() << "gaugeobservables: ";
 		this->print_gaugeobservables_from_task(0, 0);
 	};
 	virtual void init_tasks() override;
 	virtual void finalize_opencl() override;
 
-	hmc_float get_rect();
-	Device * get_device();
+	TestDevice * get_device();
 
 private:
 	void fill_buffers();
@@ -45,26 +55,26 @@ private:
 	cl_mem rect_value;
 };
 
-void Dummyfield::init_tasks()
+void TestGaugefield::init_tasks()
 {
 	opencl_modules = new Opencl_Module* [get_num_tasks()];
-	opencl_modules[0] = new Device(get_parameters(), get_device_for_task(0));
+	opencl_modules[0] = new TestDevice(get_parameters(), get_device_for_task(0));
 
 	fill_buffers();
 }
 
-Device* Dummyfield::get_device()
+TestDevice* TestGaugefield::get_device()
 {
-	return static_cast<Device*>(opencl_modules[0]);
+	return static_cast<TestDevice*>(opencl_modules[0]);
 }
 
-void Dummyfield::finalize_opencl()
+void TestGaugefield::finalize_opencl()
 {
 	clear_buffers();
 	Gaugefield_hybrid::finalize_opencl();
 }
 
-void Dummyfield::fill_buffers()
+void TestGaugefield::fill_buffers()
 {
 	// don't invoke parent function as we don't require the original buffers
 	cl_int err;
@@ -72,82 +82,68 @@ void Dummyfield::fill_buffers()
 	rect_value = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(hmc_float), 0, &err);
 }
 
-void Device::fill_kernels()
-{
-	Opencl_Module_Hmc::fill_kernels();
-}
-
-void Dummyfield::clear_buffers()
+void TestGaugefield::clear_buffers()
 {
 	// don't invoke parent function as we don't require the original buffers
 	clReleaseMemObject(rect_value);
 }
 
-void Device::clear_kernels()
-{
-	Opencl_Module::clear_kernels();
-}
-
-hmc_float Dummyfield::get_rect()
-{
-	hmc_float rect_out;
-	Device * device = static_cast<Device*>(opencl_modules[0]);
-	device->gaugeobservables_rectangles(device->get_gaugefield(), &rect_out);
-	return rect_out;
-}
-
 meta::Inputparameters create_parameters()
 {
-	int param_expect = 4;
-	logger.info() << "expect parameters:";
-	logger.info() << "\texec_name\tinputfile\tgpu_usage\trec12_usage";
-	//get number of parameters
-	const char* inputfile;
-	const char* gpu_opt;
-	const char* rec12_opt;
-
-	int num_par = boost::unit_test::framework::master_test_suite().argc;
-	if(num_par < param_expect) {
-		logger.fatal() << "need more inputparameters! Got only " << num_par << ", expected " << param_expect << "! Use inputfile values instead!";
-		//exit(-1);
-		switch(num_par) {
-			case 1:
-				logger.fatal() << "Need at least an inputfile! Aborting...";
-				exit(-1);
-				break;
-			case 2:
-				//get input file that has been passed as an argument
-				inputfile =  boost::unit_test::framework::master_test_suite().argv[1];
-				logger.info() << "inputfile used: " << inputfile;
-				gpu_opt = "";
-				rec12_opt = "";
-				break;
-			case 3:
-				//get input file that has been passed as an argument
-				inputfile =  boost::unit_test::framework::master_test_suite().argv[1];
-				logger.info() << "inputfile used: " << inputfile;
-				//get use_gpu = true/false that has been passed as an argument
-				gpu_opt =  boost::unit_test::framework::master_test_suite().argv[2];
-				logger.info() << "GPU usage: " << gpu_opt;
-				rec12_opt = "";
-				break;
-		}
-	} else {
-		//get input file that has been passed as an argument
-		inputfile =  boost::unit_test::framework::master_test_suite().argv[1];
-		logger.info() << "inputfile used: " << inputfile;
-		//get use_gpu = true/false that has been passed as an argument
-		gpu_opt =  boost::unit_test::framework::master_test_suite().argv[2];
-		logger.info() << "GPU usage: " << gpu_opt;
-		//get use_rec12 = true/false that has been passed as an argument
-		rec12_opt =  boost::unit_test::framework::master_test_suite().argv[3];
-		logger.info() << "rec12 usage: " << rec12_opt;
-	}
-	const char* _params_cpu[] = {"foo", inputfile, gpu_opt, rec12_opt};
-	meta::Inputparameters params(param_expect, _params_cpu);
-	return params;
+  const int param_expect = 4;
+  logger.info() << "expect parameters:";
+  logger.info() << "\texec_name\tinputfile\tgpu_usage\trec12_usage";
+  std::string inputfile, gpu_opt , rec12_opt;
+  //get number of parameters                                                                                                                                                                                                                 
+  int num_par = boost::unit_test::framework::master_test_suite().argc;
+  if(num_par < param_expect){
+    logger.fatal() << "Got only " << num_par << " Inputparameters, expected " << param_expect << "! Use inputfile values instead!";
+  } else if(num_par > param_expect){
+    logger.warn() << "Got " << num_par << " Inputparameters, expected only " << param_expect << "! Use only the first " << param_expect << " values";
+  }
+  
+  switch(num_par){
+  case 0:
+    logger.fatal() << "Something went terribly wrong! Did not even get executable name! Aborting...";
+    exit(-1);
+    break;
+  case 1:
+    logger.fatal() << "Need at least an inputfile! Aborting...";
+    exit(-1);
+    break;
+  case 2:
+    //get input file that has been passed as an argument                                                                                                                                                                                     
+    inputfile =  boost::unit_test::framework::master_test_suite().argv[1];
+    logger.info() << "inputfile used: " << inputfile;
+    gpu_opt = "";
+    rec12_opt = "";
+    break;
+  case 3:
+    //get input file that has been passed as an argument                                                                                                                                                                                     
+    inputfile =  boost::unit_test::framework::master_test_suite().argv[1];
+    logger.info() << "inputfile used: " << inputfile;
+    //get use_gpu = true/false that has been passed as an argument                                                                                                                                                                           
+    gpu_opt =  boost::unit_test::framework::master_test_suite().argv[2];
+    logger.info() << "GPU usage: " << gpu_opt;
+    rec12_opt = "";
+    logger.info() << rec12_opt;
+    break;
+  default:
+    //get input file that has been passed as an argument                                                                                                                                                                                     
+    inputfile =  boost::unit_test::framework::master_test_suite().argv[1];
+    logger.info() << "inputfile used: " << inputfile;
+    //get use_gpu = true/false that has been passed as an argument                                                                                                                                                                           
+    gpu_opt =  boost::unit_test::framework::master_test_suite().argv[2];
+    logger.info() << "GPU usage: " << gpu_opt;
+    //get use_rec12 = true/false that has been passed as an argument                                                                                                                                                                         
+    rec12_opt =  boost::unit_test::framework::master_test_suite().argv[3];
+    logger.info() << "rec12 usage: " << rec12_opt;
+    break;
+  }
+  const char* _params_cpu[] = {"foo", inputfile.c_str(), gpu_opt.c_str() , rec12_opt.c_str()};
+  meta::Inputparameters params(num_par, _params_cpu);
+  return params;
 }
-
 
 BOOST_AUTO_TEST_CASE( RECTANGLES )
 {
@@ -158,13 +154,15 @@ BOOST_AUTO_TEST_CASE( RECTANGLES )
 	logger.info() << "Init device";
 	meta::Inputparameters params = create_parameters();
 	hardware::System system(params);
-	Dummyfield cpu(&system);
+	TestGaugefield cpu(&system);
 
 	logger.info() << "calc rectangles value:";
 	hmc_float cpu_rect;
-	Device * device = cpu.get_device();
+	TestDevice * device = cpu.get_device();
 	device->gaugeobservables_rectangles(device->get_gaugefield(), &cpu_rect);
 	logger.info() << cpu_rect;
+	logger.info() << "Finalize device";
+	cpu.finalize();
 
 	logger.info() << "Choosing reference value and acceptance precision";
 	hmc_float ref_val = params.get_test_ref_value();
