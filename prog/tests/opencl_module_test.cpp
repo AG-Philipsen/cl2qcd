3a80e377e1beffb16a1355490b2aab30c051a888
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2012-09-27 18:05:07 +0200

    Refined module testing

--- prog/tests/opencl_module_test.cpp
+++ prog/tests/opencl_module_test.cpp
@@ -7,31 +7,11 @@
 #define BOOST_TEST_MODULE OPENCL_MODULE
 #include <boost/test/unit_test.hpp>
 
+//some functionality
+#include "test_util.h"
+
 extern std::string const version;
 std::string const version = "0.1";
-
-class TestDevice : public Opencl_Module {
-public:
-  TestDevice(const meta::Inputparameters& params, hardware::Device * device) : Opencl_Module(params, device) {
-    Opencl_Module::init(); /* init in body for proper this-pointer */
-  };
-  ~TestDevice() {
-    finalize();
-  };
-  void fill_kernels();
-  void clear_kernels();
-};
-
-void TestDevice::fill_kernels()
-{
-	Opencl_Module::fill_kernels();
-}
-
-void TestDevice::clear_kernels()
-{
-	Opencl_Module::clear_kernels();
-}
-
 
 class TestGaugefield : public Gaugefield_hybrid {
 public:
@@ -46,7 +26,7 @@ public:
 	virtual void init_tasks() override;
 	virtual void finalize_opencl() override;
 
-	TestDevice * get_device();
+  Opencl_Module * get_device();
 
 private:
 	void fill_buffers();
@@ -57,14 +37,16 @@ private:
 void TestGaugefield::init_tasks()
 {
 	opencl_modules = new Opencl_Module* [get_num_tasks()];
-	opencl_modules[0] = new TestDevice(get_parameters(), get_device_for_task(0));
+	//here we want to test Opencl_Module
+	opencl_modules[0] = new Opencl_Module(get_parameters(), get_device_for_task(0));
+	opencl_modules[0]->init();
 
 	fill_buffers();
 }
 
-TestDevice* TestGaugefield::get_device()
+Opencl_Module* TestGaugefield::get_device()
 {
-	return static_cast<TestDevice*>(opencl_modules[0]);
+	return static_cast<Opencl_Module*>(opencl_modules[0]);
 }
 
 void TestGaugefield::finalize_opencl()
@@ -87,71 +69,6 @@ void TestGaugefield::clear_buffers()
 	clReleaseMemObject(rect_value);
 }
 
-meta::Inputparameters create_parameters(std::string inputfile)
-{
-  const int param_expect = 4;
-  logger.info() << "expect parameters:";
-  logger.info() << "\texec_name\tsource-dir\tgpu_usage\trec12_usage";
-  std::string inputfile_location, gpu_opt , rec12_opt;
-  //get number of parameters                                                                                                                                                                                                                 
-  int num_par = boost::unit_test::framework::master_test_suite().argc;
-  if(num_par < param_expect){
-    logger.fatal() << "Got only " << num_par << " Inputparameters, expected " << param_expect << "! Use inputfile values instead!";
-  } else if(num_par > param_expect){
-    logger.warn() << "Got " << num_par << " Inputparameters, expected only " << param_expect << "! Use only the first " << param_expect << " values";
-  }
-  
-  switch(num_par){
-  case 0:
-    logger.fatal() << "Something went terribly wrong! Did not even get executable name! Aborting...";
-    exit(-1);
-    break;
-  case 1:
-    logger.fatal() << "Need at least an source-dir! Aborting...";
-    exit(-1);
-    break;
-  case 2:
-    //get input file location that has been passed as an argument
-    inputfile_location = boost::unit_test::framework::master_test_suite().argv[1];
-    inputfile_location += inputfile;
-    logger.info() << "inputfile used: " << inputfile_location;
-    gpu_opt = "";
-    rec12_opt = "";
-    break;
-  case 3:
-    //get input file location that has been passed as an argument
-    inputfile_location = boost::unit_test::framework::master_test_suite().argv[1];
-    inputfile_location += inputfile;
-    logger.info() << "inputfile used: " << inputfile_location;
-    //get use_gpu = true/false that has been passed as an argument
-    gpu_opt =  boost::unit_test::framework::master_test_suite().argv[2];
-    logger.info() << "GPU usage: " << gpu_opt;
-    rec12_opt = "";
-    logger.info() << rec12_opt;
-    break;
-  default:
-    //get input file location that has been passed as an argument
-    inputfile_location = boost::unit_test::framework::master_test_suite().argv[1];
-    inputfile_location += inputfile;
-    logger.info() << "inputfile used: " << inputfile_location;
-    //get use_gpu = true/false that has been passed as an argument
-    gpu_opt =  boost::unit_test::framework::master_test_suite().argv[2];
-    logger.info() << "GPU usage: " << gpu_opt;
-    //get use_rec12 = true/false that has been passed as an argument
-    rec12_opt =  boost::unit_test::framework::master_test_suite().argv[3];
-    logger.info() << "rec12 usage: " << rec12_opt;
-    break;
-  }
-  const char* _params_cpu[] = {"foo", inputfile_location.c_str(), gpu_opt.c_str() , rec12_opt.c_str()};
-  meta::Inputparameters params(num_par, _params_cpu);
-  return params;
-}
-
-void printKernelInfo(std::string name)
-{
-  logger.info() << "Test kernel\t\"" << name << "\"\tagainst reference value";
-}
-
 void test_rectangles(std::string inputfile)
 {
   std::string kernelName = "rectangles";
@@ -163,7 +80,7 @@ void test_rectangles(std::string inputfile)
 
   logger.info() << "calc rectangles value:";
   hmc_float cpu_rect;
-  TestDevice * device = cpu.get_device();
+  Opencl_Module * device = cpu.get_device();
   device->gaugeobservables_rectangles(device->get_gaugefield(), &cpu_rect);
   logger.info() << cpu_rect;
  
