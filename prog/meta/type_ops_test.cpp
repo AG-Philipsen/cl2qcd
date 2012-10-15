1692d0fec0afe80524e2321acfbfae94b46b5a68
Matthias Bach <bach@compeng.uni-frankfurt.de>
2012-10-15 14:24:13 +0200

    Tests for spinor ops

--- prog/meta/type_ops_test.cpp
+++ prog/meta/type_ops_test.cpp
@@ -69,3 +69,28 @@ BOOST_AUTO_TEST_CASE(su3vec_ops)
 	BOOST_REQUIRE_NE(ei, zero);
 	BOOST_REQUIRE_EQUAL(ei, ei);
 }
+
+BOOST_AUTO_TEST_CASE(spinor_ops)
+{
+	const hmc_complex c_one = {1., 0.};
+	const hmc_complex c_zero = {0., 0.};
+	const hmc_complex c_ei = {0., 1.};
+
+	const su3vec v_one = {c_one, c_zero, c_zero};
+	const su3vec v_zero = {c_zero, c_zero, c_zero};
+	const su3vec v_ei = {c_one, c_ei, c_zero};
+
+	const spinor one = {v_one, v_one, v_one, v_one};
+	const spinor zero = {v_zero, v_zero, v_zero, v_zero};
+	const spinor ei = {v_one, v_ei, v_zero, v_ei};
+
+	BOOST_REQUIRE_EQUAL(one, one);
+	BOOST_REQUIRE_NE(one, zero);
+	BOOST_REQUIRE_NE(one, ei);
+	BOOST_REQUIRE_NE(zero, one);
+	BOOST_REQUIRE_EQUAL(zero, zero);
+	BOOST_REQUIRE_NE(zero, ei);
+	BOOST_REQUIRE_NE(ei, one);
+	BOOST_REQUIRE_NE(ei, zero);
+	BOOST_REQUIRE_EQUAL(ei, ei);
+}
