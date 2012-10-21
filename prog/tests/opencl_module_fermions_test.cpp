d30747e3ba4cbaf6c08ed7d6058ceed262218b45
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2012-10-21 17:00:37 +0200

    Added tests for m_tm_sitediagonal

--- prog/tests/opencl_module_fermions_test.cpp
+++ prog/tests/opencl_module_fermions_test.cpp
@@ -327,6 +327,79 @@ void test_gamma5_eo(std::string inputfile)
 	BOOST_MESSAGE("Test done");
 }
 
+void test_m_tm_sitediagonal_plus_minus(std::string inputfile, bool switcher)
+{
+	using namespace hardware::buffers;
+
+	std::string kernelName;
+	if(switcher)
+	  kernelName= "m_tm_sitediagonal";
+	else
+	  kernelName = "m_tm_sitediagonal_minus";
+	printKernelInfo(kernelName);
+	logger.info() << "Init device";
+	meta::Inputparameters params = create_parameters(inputfile);
+	hardware::System system(params);
+	TestGaugefield cpu(&system);
+	Opencl_Module_Fermions * device = cpu.get_device();
+	spinor * sf_in;
+	spinor * sf_out;
+
+	logger.info() << "Fill buffers...";
+	size_t NUM_ELEMENTS_SF = meta::get_eoprec_spinorfieldsize(params);
+
+	sf_in = new spinor[NUM_ELEMENTS_SF];
+	sf_out = new spinor[NUM_ELEMENTS_SF];
+
+	//use the variable use_cg to switch between cold and random input sf
+	if(params.get_solver() == meta::Inputparameters::cg) fill_sf_with_one(sf_in, NUM_ELEMENTS_SF);
+	else fill_sf_with_random(sf_in, NUM_ELEMENTS_SF);
+	BOOST_REQUIRE(sf_in);
+
+	const Spinor in(NUM_ELEMENTS_SF, device->get_device());
+	const Spinor out(NUM_ELEMENTS_SF, device->get_device());
+	in.load(sf_in);
+	out.load(sf_in);
+	hardware::buffers::Plain<hmc_float> sqnorm(1, device->get_device());
+
+	logger.info() << "|phi|^2:";
+	hmc_float cpu_back;
+	device->set_float_to_global_squarenorm_eoprec_device(&in, &sqnorm);
+	sqnorm.dump(&cpu_back);
+	logger.info() << cpu_back;
+
+	//switch according to "use_pointsource"
+	hmc_float cpu_res;
+	if(switcher) {
+		device->M_tm_sitediagonal_device( &in, &out);
+	} else {
+		device->M_tm_sitediagonal_minus_device( &in, &out);
+	}
+	device->set_float_to_global_squarenorm_eoprec_device(&out, &sqnorm);
+	sqnorm.dump(&cpu_res);
+	logger.info() << "result:";
+	logger.info() << cpu_res;
+	logger.info() << "Finalize device";
+	cpu.finalize();
+
+	logger.info() << "Clear buffers";
+	delete[] sf_in;
+	delete[] sf_out;
+
+	testFloatAgainstInputparameters(cpu_res, params);
+	BOOST_MESSAGE("Test done");
+}
+
+void test_m_tm_sitediagonal(std::string inputfile)
+{
+  test_m_tm_sitediagonal_plus_minus(inputfile, true);
+}
+
+void test_m_tm_sitediagonal_minus(std::string inputfile)
+{
+  test_m_tm_sitediagonal_plus_minus(inputfile, false);
+}
+
 void test_dslash_eo(std::string inputfile)
 {
 	using namespace hardware::buffers;
@@ -527,9 +600,19 @@ BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(M_TM_SITEDIAGONAL )
 
-BOOST_AUTO_TEST_CASE( M_TM_SITEDIAGONAL1_)
+BOOST_AUTO_TEST_CASE( M_TM_SITEDIAGONAL_1)
 {
-	BOOST_MESSAGE("NOT YET IMPLEMENTED!");
+	test_m_tm_sitediagonal("/m_tm_sitediagonal_input_1");
+}
+
+BOOST_AUTO_TEST_CASE( M_TM_SITEDIAGONAL_2)
+{
+	test_m_tm_sitediagonal("/m_tm_sitediagonal_input_2");
+}
+
+BOOST_AUTO_TEST_CASE( M_TM_SITEDIAGONAL_3)
+{
+	test_m_tm_sitediagonal("/m_tm_sitediagonal_input_3");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
