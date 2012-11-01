ab7724d4af71c2d413d9ef918f2296fee03c1bae
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2012-11-01 11:26:41 +0100

    Edited solver classes
    refs 345

--- prog/physics/algorithms/solver.hpp
+++ prog/physics/algorithms/solver.hpp
@@ -29,7 +29,7 @@ namespace physics {
       class Solver {
 	
       public:
-	Solver(physics::fermionmatrix::Fermionmatrix & M, cl_mem x, cl_mem b, cl_mem gf, hmc_float prec, int max_iter, hardware::Device device);
+	Solver(hmc_float prec, int _max_iter) : acc_prec(prec), max_iter(_max_iter), iter(0) {};
 	
 	virtual ~Solver();
 	
@@ -41,33 +41,15 @@ namespace physics {
 	/**
 	 * Access to private members
 	 */
-	size_t get_bytes() const noexcept;
-	cl_mem get_x() const noexcept;
-	cl_mem get_b() const noexcept;
-	cl_mem get_gf() const noexcept;
-	physics::fermionmatrix::Fermionmatrix * get_M() const noexcept;
 	hmc_float get_prec() const noexcept;
 	int get_iter();
 	int get_iter_max() const noexcept;
 	
-	/**
-	 * Get the device this buffer is located on
-	 */
-	hardware::Device * get_device() const noexcept;
-	
       private:
-	/**
-	 * The ingredients to the linear system to be solved
-	 */
-	const cl_mem x;
-	const cl_mem b;
-	const cl_mem gf;
-	const physics::fermionmatrix::Fermionmatrix & M;
-	
 	/**
 	 * The acceptance precision for the solve
 	 */
-	const hmc_float prec;
+	const hmc_float acc_prec;
 	
 	/**
 	 * Counter for the iterations during the solver
@@ -79,12 +61,31 @@ namespace physics {
 	 */
 	const int max_iter;
 	
-	/**
-	 * The OpenCL device the solver is working on.
-	 */
-	hardware::Device * const device;
-	
       protected:
+      };
+      /**
+       * A Solver to solve system without even-odd preconditioning
+       */
+      class Solver_noneo : public Solver{
+      public:
+	Solver_noneo(physics::fermionmatrix::Fermionmatrix & _f, cl_mem _x, cl_mem _b, cl_mem _gf, hmc_float prec, int max_iter) : Solver(prec, max_iter), x(_x), b(_b), gf(_gf), f(_f) {};
+	/**
+	 * Access to private members
+	 */
+	cl_mem get_x() const noexcept;
+	cl_mem get_b() const noexcept;
+	cl_mem get_gf() const noexcept;
+	physics::fermionmatrix::Fermionmatrix * get_M() const noexcept;
+      private:
+	/**
+	 * The ingredients to the linear system to be solved
+	 */
+	const cl_mem x;
+	const cl_mem b;
+	const cl_mem gf;
+	const physics::fermionmatrix::Fermionmatrix & f;
+	
+	
       };
     }
   }
