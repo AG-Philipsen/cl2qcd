6aa1b5e45f41b77600bcaf7762b5bbe5cd206106
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2012-11-06 14:32:17 +0100

    Code cleanup

--- prog/opencl_module_hmc.h
+++ prog/opencl_module_hmc.h
@@ -74,9 +74,6 @@ public:
 	void calc_gauge_force();
 	void calc_fermion_force(usetimer * solvertimer, hmc_float kappa = ARG_DEF, hmc_float mubar = ARG_DEF);
 	void calc_fermion_force_detratio(usetimer * solvertimer, const hardware::buffers::SU3 * gaugefield);
-
-	///////////////////////////////////////////////////
-	//Methods on device
 	void md_update_gaugemomentum_device(hmc_float eps);
 	void md_update_gaugefield_device(hmc_float eps);
 	void set_zero_clmem_force_device();
@@ -90,35 +87,11 @@ public:
 protected:
 
 	/**
-	 * comutes work-sizes for a kernel
-	 * @todo autotune
-	 * @param ls local-work-size
-	 * @param gs global-work-size
-	 * @param num_groups number of work groups
-	 * @param name name of the kernel for possible autotune-usage, not yet used!!
+	 * These functions can be removed once this module is turned into a different object
 	 */
 	virtual void get_work_sizes(const cl_kernel kernel, size_t * ls, size_t * gs, cl_uint * num_groups) const override;
-
-	/**
-	 * Print the profiling information to a file.
-	 *
-	 * @param filename Name of file where data is appended.
-	 */
 	void virtual print_profiling(const std::string& filename, int number) const override;
-
-	/**
-	 * Return amount of bytes read and written by a specific kernel per call.
-	 *
-	 * @param in Name of the kernel under consideration.
-	 */
 	virtual size_t get_read_write_size(const std::string& in) const override;
-
-	/**
-	 * Return amount of Floating point operations performed by a specific kernel per call.
-	 * NOTE: this is meant to be the "netto" amount in order to be comparable.
-	 *
-	 * @param in Name of the kernel under consideration.
-	 */
 	virtual uint64_t get_flop_size(const std::string& in) const override;
 
 private:
@@ -130,12 +103,9 @@ private:
 	Opencl_Module_Hmc(const meta::Inputparameters& params, hardware::Device * device);
 
 	/**
-	 * Collect the kernels for OpenCL.
+	 *  These functions can be removed once this module is turned into a different object
 	 */
 	void fill_kernels();
-	/**
-	 * Clear out the kernels,
-	 */
 	void clear_kernels();
 
 	//variables
