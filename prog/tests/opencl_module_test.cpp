8aaf65893df63dc47e25c2055dd82dfd8a4d6308
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2012-11-15 14:45:42 +0100

    Added more tests for stout smear kernel

--- prog/tests/opencl_module_test.cpp
+++ prog/tests/opencl_module_test.cpp
@@ -218,6 +218,16 @@ BOOST_AUTO_TEST_CASE( STOUT_SMEAR_1 )
         test_stout_smear("/stout_smear_input_1");
 }
 
+BOOST_AUTO_TEST_CASE( STOUT_SMEAR_2 )
+{
+        test_stout_smear("/stout_smear_input_2");
+}
+
+BOOST_AUTO_TEST_CASE( STOUT_SMEAR_3 )
+{
+        test_stout_smear("/stout_smear_input_3");
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE ( RECTANGLES )
