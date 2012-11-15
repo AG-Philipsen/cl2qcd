c4c6bf4dd460d2d53adf7265a1024608c04ab3f4
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2012-11-15 14:27:39 +0100

    Modified Stout-smear test to use newer structure
    refs 232

--- prog/tests/Stout_Smear_test.cpp
+++ prog/tests/Stout_Smear_test.cpp
@@ -43,6 +43,13 @@ public:
 		auto inputfile = system->get_inputparameters();
 		init(1, inputfile.get_use_gpu() ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU);
 		meta::print_info_hmc(exec_name.c_str(), inputfile);
+
+
+		logger.info() << "gaugeobservables: ";
+		this->print_gaugeobservables_from_task(0, 0);
+
+
+
 	};
 
 	virtual void init_tasks();
@@ -71,7 +78,7 @@ void Dummyfield::finalize_opencl()
 
 void Dummyfield::fill_buffers()
 {
-	set_gaugefield_cold(get_sgf());
+  //set_gaugefield_cold(get_sgf());
 	copy_gaugefield_to_task(0);
 }
 
@@ -99,41 +106,83 @@ void Dummyfield::get_gaugeobservables_from_task(int dummy, int ntask, hmc_float 
 	gf_code->gaugeobservables(&static_cast<Device*>(opencl_modules[0])->out, plaq, tplaq, splaq, pol);
 }
 
+class TestGaugefield : public Gaugefield_hybrid {
+public:
+	TestGaugefield(const hardware::System * system) : Gaugefield_hybrid(system) {
+		auto inputfile = system->get_inputparameters();
+		init(1, inputfile.get_use_gpu() ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU);
+		std::string name = "test program";
+		meta::print_info_hmc(name.c_str(), inputfile);
+		logger.info() << "gaugeobservables: ";
+		this->print_gaugeobservables_from_task(0, 0);
+	};
+	virtual void finalize_opencl() override;
+
+	hardware::code::Gaugefield * get_device();
+};
+
+hardware::code::Gaugefield* TestGaugefield::get_device()
+{
+	return static_cast<hardware::code::Gaugefield*>(opencl_modules[0]);
+}
+
+void TestGaugefield::finalize_opencl()
+{
+	Gaugefield_hybrid::finalize_opencl();
+}
+
 BOOST_AUTO_TEST_CASE( STOUT_SMEAR )
 {
-
   std::string inputfile = "/stout_smear_input_1";
 	std::string kernelName = "stout_smear";
         printKernelInfo(kernelName);
 	logger.info() << "Init device";
 	meta::Inputparameters params = create_parameters(inputfile);
 	hardware::System system(params);
-	Dummyfield dummy(&system);
+	//Dummyfield dummy(&system);
 
-
+	TestGaugefield dummy2(&system);
+	hardware::code::Gaugefield * device = dummy2.get_device();
+	auto gf_code = device->get_device()->get_gaugefield_code();
+	//out buffer
+	const hardware::buffers::SU3 out(device->get_gaugefield()->get_elements(), device->get_device());
 	hmc_float plaq_cpu, tplaq_cpu, splaq_cpu;
 	hmc_complex pol_cpu;
 
 	logger.info() << "gaugeobservables of in field before: ";
-	dummy.print_gaugeobservables_from_task(0, 0);
+	dummy2.print_gaugeobservables_from_task(0,0);
+	//dummy.print_gaugeobservables_from_task(0, 0);
 	logger.info() << "gaugeobservables of out field before: ";
-	dummy.get_gaugeobservables_from_task(0, 0, &plaq_cpu, &tplaq_cpu, &splaq_cpu, &pol_cpu);
-	logger.info() << "plaq: " << plaq_cpu << "\t" << tplaq_cpu  << "\t" << splaq_cpu  << "\t" << pol_cpu.re  << "\t" << pol_cpu.im ;
-	dummy.runTestKernel();
-	logger.info() << "gaugeobservables of in field after: ";
-	dummy.print_gaugeobservables_from_task(0, 0);
-	logger.info() << "gaugeobservables of out field after: ";
-	dummy.get_gaugeobservables_from_task(0, 0, &plaq_cpu, &tplaq_cpu, &splaq_cpu, &pol_cpu);
+	//dummy.get_gaugeobservables_from_task(0, 0, &plaq_cpu, &tplaq_cpu, &splaq_cpu, &pol_cpu);
+	//logger.info() << "plaq: " << plaq_cpu << "\t" << tplaq_cpu  << "\t" << splaq_cpu  << "\t" << pol_cpu.re  << "\t" << pol_cpu.im ;
+        gf_code->gaugeobservables(&out , &plaq_cpu, &tplaq_cpu, &splaq_cpu, &pol_cpu);
 	logger.info() << "plaq: " << plaq_cpu << "\t" << tplaq_cpu  << "\t" << splaq_cpu  << "\t" << pol_cpu.re  << "\t" << pol_cpu.im ;
 
+	//dummy.runTestKernel();
+	gf_code->stout_smear_device( gf_code->get_gaugefield(), &out);
+
+	logger.info() << "gaugeobservables of in field after: ";
+	//dummy.print_gaugeobservables_from_task(0, 0);
+	dummy2.print_gaugeobservables_from_task(0, 0);
+	logger.info() << "gaugeobservables of out field after: ";
+	//dummy.get_gaugeobservables_from_task(0, 0, &plaq_cpu, &tplaq_cpu, &splaq_cpu, &pol_cpu);
+	//logger.info() << "plaq: " << plaq_cpu << "\t" << tplaq_cpu  << "\t" << splaq_cpu  << "\t" << pol_cpu.re  << "\t" << pol_cpu.im ;
+
+        gf_code->gaugeobservables(&out , &plaq_cpu, &tplaq_cpu, &splaq_cpu, &pol_cpu);
+	logger.info() << "plaq: " << plaq_cpu << "\t" << tplaq_cpu  << "\t" << splaq_cpu  << "\t" << pol_cpu.re  << "\t" << pol_cpu.im ;
+
+	/*
 	logger.info() << "Choosing reference value and acceptance precision";
 	hmc_float ref_val = params.get_test_ref_value();
 	logger.info() << "reference value:\t" << ref_val;
 	hmc_float prec = params.get_solver_prec();
 	logger.info() << "acceptance precision: " << prec;
-
+	
 	logger.info() << "Compare result to reference value";
 	BOOST_REQUIRE_CLOSE(plaq_cpu, ref_val, prec);
 	logger.info() << "Done";
+	*/
+        testFloatAgainstInputparameters(plaq_cpu, params);
+        BOOST_MESSAGE("Test done");
 	BOOST_MESSAGE("Test done");
 }
