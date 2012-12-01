c96ddef14c22bf4e23aafcfe42368d30bc3b66ed
Matthias Bach <bach@compeng.uni-frankfurt.de>
2012-12-01 18:41:43 +0100

    Rewrote dslash benchmark to use new classes
    refs 303

--- prog/benchmarks/dslash_benchmark.cpp
+++ prog/benchmarks/dslash_benchmark.cpp
@@ -1,40 +1,19 @@
-#include "../inverter.h"
-
+#include <fstream>
 #include "../meta/util.hpp"
+#include "../physics/lattices/gaugefield.hpp"
 
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
 int main(int argc, const char* argv[])
 {
+	using namespace hardware::buffers;
+
 	try {
 		meta::Inputparameters parameters(argc, argv);
 		switchLogLevel(parameters.get_log_level());
 
 		meta::print_info_inverter(argv[0], parameters);
-
-		ofstream ofile;
-		ofile.open("inverter.log");
-		if(ofile.is_open()) {
-			meta::print_info_inverter(argv[0], &ofile, parameters);
-			ofile.close();
-		} else {
-			logger.warn() << "Could not log file for inverter.";
-		}
-
-		//get name for file to which correlators are to be stored
-		stringstream corr_fn;
-		switch ( parameters.get_startcondition() ) {
-			case meta::Inputparameters::start_from_source :
-				corr_fn << parameters.get_sourcefile() << "_correlators.dat" ;
-				break;
-			case meta::Inputparameters::hot_start :
-				corr_fn << "conf.hot_correlators.dat" ;
-				break;
-			case meta::Inputparameters::cold_start :
-				corr_fn << "conf.cold_correlators.dat" ;
-				break;
-		}
 
 		if(parameters.get_profile_solver() == false) {
 			logger.warn() << "solver times will not be measured!";
@@ -44,60 +23,50 @@ int main(int argc, const char* argv[])
 		// Initialization
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		init_timer.reset();
 		hardware::System system(parameters, true);
-		Gaugefield_inverter gaugefield(&system);
 
-		//one needs 2 task here
-		int numtasks = 2;
-		if(parameters.get_device_count() != numtasks )
-			logger.warn() << "Exactly 2 devices demanded by benchmark executable. All calculations performed on primary device.";
+		// expect there to be exactly one device
+		if(system.get_devices().size() != 1) {
+			logger.fatal() << "There must be exactly one device chosen for the dslash benchmark to be performed.";
+		}
 
-		cl_device_type primary_device = parameters.get_use_gpu() ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU;
-
-		logger.trace() << "Init gaugefield" ;
-		gaugefield.init(numtasks, primary_device);
+		physics::PRNG prng(system);
+		physics::lattices::Gaugefield gaugefield(system, prng);
 
 		logger.info() << "Gaugeobservables:";
-		gaugefield.print_gaugeobservables(0);
+		print_gaugeobservables(gaugefield, 0);
 
 		//init needed buffers again
 		//these are: 2 eoprec spinorfield, 1 gaugefield
-		const hardware::buffers::Spinor sf1(meta::get_eoprec_spinorfieldsize(parameters), gaugefield.get_task_solver()->get_device());
-		const hardware::buffers::Spinor sf2(meta::get_eoprec_spinorfieldsize(parameters), gaugefield.get_task_solver()->get_device());
-
-		init_timer.add();
+		auto gf_buffer = gaugefield.get_buffers().at(0);
+		auto device = gf_buffer->get_device();
+		const Spinor sf1(meta::get_eoprec_spinorfieldsize(parameters), device);
+		const Spinor sf2(meta::get_eoprec_spinorfieldsize(parameters), device);
 
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// dslash-benchmark
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		perform_timer.reset();
-
 		int hmc_iter = parameters.get_hmcsteps();
 		int iter;
-		auto gf_buffer = gaugefield.get_task_solver()->get_device()->get_gaugefield_code()->get_gaugefield();
+		auto fermion_code = device->get_fermion_code();
 
 		logger.trace() << "Perform " << hmc_iter << "of dslash benchmarking (EVEN + ODD) for each step";
 		for(iter = 0; iter < hmc_iter; iter ++) {
-			gaugefield.get_task_solver()->dslash_eo_device(&sf1, &sf2, gf_buffer, EVEN);
-			gaugefield.get_task_solver()->dslash_eo_device(&sf1, &sf2, gf_buffer, ODD);
+			fermion_code->dslash_eo_device(&sf1, &sf2, gf_buffer, EVEN);
+			fermion_code->dslash_eo_device(&sf1, &sf2, gf_buffer, ODD);
 		}
+		device->synchronize();
 		logger.trace() << "dslash benchmarking done" ;
-		perform_timer.add();
 
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// Final Output
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		total_timer.add();
-		general_time_output(&total_timer, &init_timer, &perform_timer, &plaq_timer, &poly_timer);
-
 		//CP: this is just a fist version and will go into an own file later
-		string profiling_out;
-		profiling_out = string(argv[0]) + string("_profiling_data");
+		const std::string profiling_out = std::string(argv[0]) + std::string("_profiling_data");
 
-		fstream prof_file;
+		std::fstream prof_file;
 		prof_file.open(profiling_out.c_str(), std::ios::out | std::ios::app);
 		if(prof_file.is_open()) {
 			meta::print_info_inverter(argv[0], &prof_file, parameters);
@@ -106,13 +75,7 @@ int main(int argc, const char* argv[])
 			logger.warn() << "Could not open " << profiling_out;
 		}
 
-		gaugefield.get_task_solver()->print_profiling(profiling_out, 0) ;
-
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// free variables
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-		gaugefield.finalize();
+		fermion_code->print_profiling(profiling_out, 0) ;
 
 	} //try
 	//exceptions from Opencl classes
