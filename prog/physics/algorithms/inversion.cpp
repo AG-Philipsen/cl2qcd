579a2409ef88ec524ec1a963d2b12a55de2b17da
Matthias Bach <bach@compeng.uni-frankfurt.de>
2013-01-12 22:52:47 +0100

    Implement even-odd inverion via new classes
    refs 386

--- prog/physics/algorithms/inversion.cpp
+++ prog/physics/algorithms/inversion.cpp
@@ -47,14 +47,7 @@ static void invert_M_nf2_upperflavour(const physics::lattices::Spinorfield* resu
 
 	auto params = system.get_inputparameters();
 
-	auto result_buf = result->get_buffers().at(0);
-	auto source_buf = source->get_buffers().at(0);
-	auto gf_buf = gf.get_buffers().at(0);
-	auto device = result_buf->get_device();
-
-	int converged = -1;
-	hardware::code::Fermions * solver = device->get_fermion_code();
-	auto spinor_code = device->get_spinor_code();
+	int converged;
 
 	if(!params.get_use_eo()) {
 		//noneo case
@@ -92,17 +85,13 @@ static void invert_M_nf2_upperflavour(const physics::lattices::Spinorfield* resu
 		 * non-trivial inversion to the even sites (see DeGran/DeTar p 174ff).
 		 */
 		//init some helping buffers
-		const hardware::buffers::Spinor clmem_source_even  (meta::get_eoprec_spinorfieldsize(params), device);
-		const hardware::buffers::Spinor clmem_source_odd  (meta::get_eoprec_spinorfieldsize(params), device);
-		const hardware::buffers::Spinor clmem_tmp_eo_1  (meta::get_eoprec_spinorfieldsize(params), device);
-		const hardware::buffers::Spinor clmem_tmp_eo_2  (meta::get_eoprec_spinorfieldsize(params), device);
-		const hardware::buffers::Plain<hmc_complex> clmem_one (1, device);
-		const hardware::buffers::Spinor result_buf_eo  (meta::get_eoprec_spinorfieldsize(params), device);
-		const hardware::buffers::Plain<hmc_complex> clmem_mone (1, solver->get_device());
+		const Spinorfield_eo source_even(system);
+		const Spinorfield_eo source_odd(system);
+		const Spinorfield_eo tmp1(system);
+		const Spinorfield_eo tmp2(system);
+		const Spinorfield_eo result_eo(system);
 		hmc_complex one = hmc_complex_one;
-		hmc_complex mone = {-1.,0.};
-		clmem_one.load(&one);
-		clmem_mone.load(&mone);
+		hmc_complex mone = { -1., 0.};
 
 		//convert source and input-vector to eoprec-format
 		/**
@@ -113,11 +102,11 @@ static void invert_M_nf2_upperflavour(const physics::lattices::Spinorfield* resu
 		 * spinor_code->convert_to_eoprec_device(&clmem_source_even, &clmem_source_odd, source_buf);
 		 * workaround:
 		 */
-		spinor_code->convert_to_eoprec_device(&clmem_source_odd, &clmem_source_even, source_buf);
+		convert_to_eoprec(&source_odd, &source_even, *source);
 
-		if(logger.beDebug()) solver->print_info_inv_field(source_buf, false, "\tsource before inversion ");
-		if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_even, true, "\teven source before inversion ");
-		if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_odd, true, "\todd source before inversion ");
+		// TODO readd if(logger.beDebug()) solver->print_info_inv_field(source_buf, false, "\tsource before inversion ");
+		// TODO readd if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_even, true, "\teven source before inversion ");
+		// TODO readd if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_odd, true, "\todd source before inversion ");
 
 		//prepare sources
 		/**
@@ -126,41 +115,42 @@ static void invert_M_nf2_upperflavour(const physics::lattices::Spinorfield* resu
 		 */
 		if(params.get_fermact() == meta::Inputparameters::wilson) {
 			//in this case, the diagonal matrix is just 1 and falls away.
-			solver->dslash_eo_device(&clmem_source_odd, &clmem_tmp_eo_1, gf_buf, EVEN);
-			spinor_code->saxpy_eoprec_device(&clmem_source_even, &clmem_tmp_eo_1, &clmem_one, &clmem_source_even);
+			dslash(&tmp1, gf, source_odd, EVEN);
+			saxpy(&source_even, one, source_even, tmp1);
 		} else if(params.get_fermact() == meta::Inputparameters::twistedmass) {
-			solver->M_tm_inverse_sitediagonal_device(&clmem_source_odd, &clmem_tmp_eo_1);
-			solver->dslash_eo_device(&clmem_tmp_eo_1, &clmem_tmp_eo_2, gf_buf, EVEN);
-			spinor_code->saxpy_eoprec_device(&clmem_source_even, &clmem_tmp_eo_2, &clmem_one, &clmem_source_even);
+			M_tm_inverse_sitediagonal(&tmp1, source_odd);
+			dslash(&tmp2, gf, tmp1, EVEN);
+			saxpy(&source_even, one, source_even, tmp2);
 		}
 
 		//Trial solution
 		///@todo this should go into a more general function
-		spinor_code->set_eoprec_spinorfield_cold_device(&result_buf_eo);
+		result_eo.cold();
 		logger.debug() << "start eoprec-inversion";
 		//even solution
 		if(params.get_solver() == meta::Inputparameters::cg) {
 			//to use cg, one needs an hermitian matrix, which is QplusQminus
 			//the source must now be gamma5 b, to obtain the desired solution in the end
-			solver->gamma5_eo_device(&clmem_source_even);
-			hardware::code::QplusQminus_eo f_eo(solver);
-			if(logger.beDebug()) solver->print_info_inv_field(&result_buf_eo, true, "\tinv field before inversion ");
-			if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_even, true, "\tsource before inversion ");
-			converged = solver->cg_eo(f_eo, &result_buf_eo, &clmem_source_even, gf_buf, params.get_solver_prec());
-			if(logger.beDebug()) solver->print_info_inv_field(&result_buf_eo, true, "\tinv field after inversion ");
-			if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_even, true, "\tsource after inversion ");
+			source_even.gamma5();
+			QplusQminus_eo f_eo(params.get_kappa(), meta::get_mubar(params), system);
+			// TODO readd if(logger.beDebug()) solver->print_info_inv_field(&result_buf_eo, true, "\tinv field before inversion ");
+			// TODO readd if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_even, true, "\tsource before inversion ");
+			converged = cg(&result_eo, f_eo, gf, source_even, system, params.get_solver_prec());
+			// TODO readd if(logger.beDebug()) solver->print_info_inv_field(&result_buf_eo, true, "\tinv field after inversion ");
+			// TODO readd if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_even, true, "\tsource after inversion ");
 			//now, calc Qminus result_buf_eo to obtain x = A^⁻1 b
 			//therefore, use source as an intermediate buffer
-			solver->Qminus_eo(&result_buf_eo, &clmem_source_even, gf_buf, params.get_kappa(), meta::get_mubar(params));
+			Qminus_eo qminus(params.get_kappa(), meta::get_mubar(params), system);
+			qminus(&source_even, gf, result_eo);
 			//save the result to result_buf
-			hardware::buffers::copyData(&result_buf_eo, &clmem_source_even);
+			copyData(&result_eo, source_even);
 		} else {
-			hardware::code::Aee f_eo(solver);
-			if(logger.beDebug()) solver->print_info_inv_field(&result_buf_eo, true, "\tinv field before inversion ");
-			if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_even, true, "\tsource before inversion ");
-			converged = solver->bicgstab_eo(f_eo, &result_buf_eo, &clmem_source_even, gf_buf, params.get_solver_prec());
-			if(logger.beDebug()) solver->print_info_inv_field(&result_buf_eo, true, "\tinv field after inversion ");
-			if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_even, true, "\tsource after inversion ");
+			Aee f_eo(params.get_kappa(), meta::get_mubar(params), system);
+			// TODO readd if(logger.beDebug()) solver->print_info_inv_field(&result_buf_eo, true, "\tinv field before inversion ");
+			// TODO readd if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_even, true, "\tsource before inversion ");
+			converged = bicgstab(&result_eo, f_eo, gf, source_even, system, params.get_solver_prec());
+			// TODO readd if(logger.beDebug()) solver->print_info_inv_field(&result_buf_eo, true, "\tinv field after inversion ");
+			// TODO readd if(logger.beDebug()) solver->print_info_inv_field(&clmem_source_even, true, "\tsource after inversion ");
 		}
 
 		//odd solution
@@ -171,18 +161,18 @@ static void invert_M_nf2_upperflavour(const physics::lattices::Spinorfield* resu
 		 */
 		if(params.get_fermact() == meta::Inputparameters::wilson) {
 			//in this case, the diagonal matrix is just 1 and falls away.
-			solver->dslash_eo_device(&result_buf_eo, &clmem_tmp_eo_1, gf_buf, ODD);
-			spinor_code->saxpy_eoprec_device(&clmem_tmp_eo_1, &clmem_source_odd, &clmem_mone, &clmem_tmp_eo_1);
-			spinor_code->sax_eoprec_device(&clmem_tmp_eo_1, &clmem_mone, &clmem_tmp_eo_1);
+			dslash(&tmp1, gf, result_eo, ODD);
+			saxpy(&tmp1, mone, tmp1, source_odd);
+			sax(&tmp1, mone, tmp1);
 		} else if(params.get_fermact() == meta::Inputparameters::twistedmass) {
-			solver->dslash_eo_device(&result_buf_eo, &clmem_tmp_eo_2, gf_buf, ODD);
-			solver->M_tm_inverse_sitediagonal_device(&clmem_tmp_eo_2, &clmem_tmp_eo_1);
-			solver->M_tm_inverse_sitediagonal_device(&clmem_source_odd, &clmem_tmp_eo_2);
-			spinor_code->saxpy_eoprec_device(&clmem_tmp_eo_1, &clmem_tmp_eo_2, &clmem_mone, &clmem_tmp_eo_1);
-			spinor_code->sax_eoprec_device(&clmem_tmp_eo_1, &clmem_mone, &clmem_tmp_eo_1);
+			dslash(&tmp2, gf, result_eo, ODD);
+			M_tm_inverse_sitediagonal(&tmp1, tmp2);
+			M_tm_inverse_sitediagonal(&tmp2, source_odd);
+			saxpy(&tmp1, mone, tmp1, tmp2);
+			sax(&tmp1, mone, tmp1);
 		}
 
-		 ///CP: whole solution
+		///CP: whole solution
 		//convert source and input-vector to eoprec-format
 		/**
 		 * This currently is a workaround connceted to issue #387
@@ -194,14 +184,12 @@ static void invert_M_nf2_upperflavour(const physics::lattices::Spinorfield* resu
 		 * workaround:
 		 */
 		//CP: suppose the odd sol is saved in inout_eoprec, the even one in clmem_tmp_eo_1
-		spinor_code->convert_from_eoprec_device( &clmem_tmp_eo_1, &result_buf_eo, result_buf);
+		convert_from_eoprec(result, tmp1, result_eo);
 	}
 
-	if(logger.beDebug()) solver->print_info_inv_field(result_buf, false, "\tsolution ");
+	// TODO readd if(logger.beDebug()) solver->print_info_inv_field(result_buf, false, "\tsolution ");
 
-	if (converged < 0) {
-		if(converged == -1) logger.fatal() << "\t\t\tsolver did not solve!!";
-		else logger.fatal() << "\t\t\tsolver got stuck after " << abs(converged) << " iterations!!";
-	} else logger.debug() << "\t\t\tsolver solved in " << converged << " iterations!";
+	// TODO catch or document exceptions
+	logger.debug() << "\t\t\tsolver solved in " << converged << " iterations!";
 }
 
