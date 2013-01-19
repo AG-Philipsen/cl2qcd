c0b7be0db5a1a3ccb78641089dd5db1d571b7e76
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2013-01-19 17:25:40 +0100

    Removed if-else for multiple configs reads from inverter

--- prog/inverter.cpp
+++ prog/inverter.cpp
@@ -46,109 +46,61 @@ int main(int argc, const char* argv[])
 		perform_timer.reset();
 		logger.info() << "Perform inversion(s) on device.." ;
 
-		if(parameters.get_read_multiple_configs()) {
-			const int iter_end = parameters.get_config_read_end();
-			const int iter_start = parameters.get_config_read_start();
-			const int iter_incr = parameters.get_config_read_incr();
-			int iter = 0;
+		const int iter_start = (parameters.get_read_multiple_configs()) ? parameters.get_config_read_start() : 0;
+		// the "+1" is necessary so that all config are read
+		const int iter_end   = (parameters.get_read_multiple_configs()) ? parameters.get_config_read_end() + 1   : 1;
+		const int iter_incr =  (parameters.get_read_multiple_configs()) ? parameters.get_config_read_incr()  : 1;
+		int iter = 0;
 
-			//main loop
-			for(iter = iter_start; iter <= iter_end; iter += iter_incr) {
-				std::string config_name = meta::create_configuration_name(parameters, iter);
-				logger.info() << "Measure fermionic observables on configuration: " << config_name;
-				Gaugefield gaugefield(system, prng, config_name);
-				if(parameters.get_print_to_screen() ) {
-					print_gaugeobservables(gaugefield, iter);
-				}
-				if(parameters.get_measure_correlators() ) {
-					// for the correlator calculation, all sources are needed on the device
-					const std::vector<Spinorfield*> sources = create_swappable_sources(system, prng, parameters.get_num_sources());
-					const std::vector<Spinorfield*> result = create_swappable_spinorfields(system, sources.size(), parameters.get_place_sources_on_host());
-
-					swap_out(sources);
-					swap_out(result);
-
-					perform_inversion(&result, &gaugefield, sources, system);
-
-					logger.info() << "Finished inversion. Starting measurements.";
-					
-					swap_in(sources);
-					swap_in(result);
-
-					//get name for file to which correlators are to be stored
-					std::string corr_fn = meta::get_ferm_obs_corr_file_name(parameters, config_name);
-					ofstream of(corr_fn.c_str(), ios_base::app);
-					if(!of.is_open()) {
-						throw File_Exception(corr_fn);
-					}
-					flavour_doublet_correlators(result, sources, of, parameters);
-					release_spinorfields(result);
-					release_spinorfields(sources);
-				}
-				if(parameters.get_measure_pbp() ) {
-					//get name for file to which pbp is to be stored
-					std::string pbp_fn = meta::get_ferm_obs_pbp_file_name(parameters, config_name);
-					// the chiral condensate needs only one source at a time
-					for(int i_sources = 0; i_sources < parameters.get_num_sources(); i_sources ++) {
-						auto sources = create_sources(system, prng, 1);
-						auto result = create_spinorfields(system, sources.size());
-
-						perform_inversion(&result, &gaugefield, sources, system);
-
-						flavour_doublet_chiral_condensate(result, sources, pbp_fn, iter, system);
-						release_spinorfields(result);
-						release_spinorfields(sources);
-					}
-				}
-			}
-		} else {
-			Gaugefield gaugefield(system, prng);
-
-			logger.info() << gaugefield.get_parameters_source().trajectorynr_source;
-
-			logger.info() << "Gaugeobservables:";
-			print_gaugeobservables(gaugefield, gaugefield.get_parameters_source().trajectorynr_source);
-
-			if(parameters.get_measure_correlators() ) {
-				// for the correlator calculation, all sources are needed on the device
-				const std::vector<Spinorfield*> sources = create_swappable_sources(system, prng, parameters.get_num_sources());
-				const std::vector<Spinorfield*> result = create_swappable_spinorfields(system, sources.size(), parameters.get_place_sources_on_host());
-
-				swap_out(sources);
-				swap_out(result);
-
-				perform_inversion(&result, &gaugefield, sources, system);
-
-				logger.info() << "Finished inversion. Starting measurements.";
-
-				swap_in(sources);
-				swap_in(result);
-
-				//get name for file to which correlators are to be stored
-				std::string corr_fn = meta::get_ferm_obs_corr_file_name(parameters, parameters.get_sourcefile().c_str());
-				ofstream of(corr_fn.c_str(), ios_base::app);
-				if(!of.is_open()) {
-					throw File_Exception(corr_fn);
-				}
-				flavour_doublet_correlators(result, sources, of, parameters);
-				release_spinorfields(result);
-				release_spinorfields(sources);
-			}
-			if(parameters.get_measure_pbp() ) {
-				//get name for file to which pbp is to be stored
-				std::string pbp_fn = meta::get_ferm_obs_pbp_file_name(parameters, parameters.get_sourcefile().c_str());
-				// the chiral condensate needs only one source at a time
-				for(int i_sources = 0; i_sources < parameters.get_num_sources(); i_sources ++) {
-					auto sources = create_sources(system, prng, 1);
-					auto result = create_spinorfields(system, sources.size());
-
-					perform_inversion(&result, &gaugefield, sources, system);
-
-					flavour_doublet_chiral_condensate(result, sources, pbp_fn, gaugefield.get_parameters_source().trajectorynr_source, system);
-					release_spinorfields(result);
-					release_spinorfields(sources);
-				}
-			}
+		//main loop
+		for(iter = iter_start; iter < iter_end; iter += iter_incr) {
+		  std::string config_name = ( parameters.get_read_multiple_configs() ) ? meta::create_configuration_name(parameters, iter) : parameters.get_sourcefile();
+		  logger.info() << config_name;
+		  logger.info() << "Measure fermionic observables on configuration: " << config_name;
+		  Gaugefield gaugefield(system, prng, config_name) ;
+		  if(parameters.get_print_to_screen() ) {
+		    print_gaugeobservables(gaugefield, gaugefield.get_parameters_source().trajectorynr_source);
+		  }
+		  if(parameters.get_measure_correlators() ) {
+		    // for the correlator calculation, all sources are needed on the device
+		    const std::vector<Spinorfield*> sources = create_swappable_sources(system, prng, parameters.get_num_sources());
+		    const std::vector<Spinorfield*> result = create_swappable_spinorfields(system, sources.size(), parameters.get_place_sources_on_host());
+			    
+		    swap_out(sources);
+		    swap_out(result);
+			    
+		    perform_inversion(&result, &gaugefield, sources, system);
+		    
+		    logger.info() << "Finished inversion. Starting measurements.";
+		    
+		    swap_in(sources);
+		    swap_in(result);
+		    
+		    //get name for file to which correlators are to be stored
+		    std::string corr_fn = meta::get_ferm_obs_corr_file_name(parameters, config_name);
+		    ofstream of(corr_fn.c_str(), ios_base::app);
+		    if(!of.is_open()) {
+		      throw File_Exception(corr_fn);
+		    }
+		    flavour_doublet_correlators(result, sources, of, parameters);
+		    release_spinorfields(result);
+		    release_spinorfields(sources);
+		  }
+		  if(parameters.get_measure_pbp() ) {
+		    //get name for file to which pbp is to be stored
+		    std::string pbp_fn = meta::get_ferm_obs_pbp_file_name(parameters, config_name);
+		    // the chiral condensate needs only one source at a time
+		    for(int i_sources = 0; i_sources < parameters.get_num_sources(); i_sources ++) {
+		      auto sources = create_sources(system, prng, 1);
+		      auto result = create_spinorfields(system, sources.size());
+		      
+		      perform_inversion(&result, &gaugefield, sources, system);
+		      
+		      flavour_doublet_chiral_condensate(result, sources, pbp_fn, gaugefield.get_parameters_source().trajectorynr_source, system);
+		      release_spinorfields(result);
+		      release_spinorfields(sources);
+		    }
+		  }
 		}
 		logger.trace() << "Inversion done" ;
 		perform_timer.add();
