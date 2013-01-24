1269ced5ffac421a722545bba23e424efe5d7911
Matthias Bach <bach@compeng.uni-frankfurt.de>
2013-01-24 17:25:38 +0100

    Ported metropolis
    refs 397

--- prog/physics/algorithms/metropolis.hpp
+++ prog/physics/algorithms/metropolis.hpp
@@ -11,17 +11,21 @@
 #include "../lattices/gaugefield.hpp"
 #include "../lattices/spinorfield.hpp"
 #include "../lattices/spinorfield_eo.hpp"
+#include "../lattices/gaugemomenta.hpp"
 #include "../../types_hmc.h"
 
 namespace physics {
 namespace algorithms {
 
 
-hmc_float calc_s_fermion(const physics::lattices::Spinorfield * phi_inv, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield& phi, const hardware::System& system, hmc_float kappa, hmc_float mubar);
-hmc_float calc_s_fermion(const physics::lattices::Spinorfield_eo * phi_inv, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield_eo& phi, const hardware::System& system, hmc_float kappa, hmc_float mubar);
+hmc_float calc_s_fermion(const physics::lattices::Spinorfield * phi_inv, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield& phi, const hardware::System& system, hmc_float kappa = ARG_DEF, hmc_float mubar = ARG_DEF);
+hmc_float calc_s_fermion(const physics::lattices::Spinorfield_eo * phi_inv, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield_eo& phi, const hardware::System& system, hmc_float kappa = ARG_DEF, hmc_float mubar = ARG_DEF);
 
-hmc_float calc_s_fermion_mp(const physics::lattices::Spinorfield * phi_inv, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield& phi, const hardware::System& system, hmc_float kappa, hmc_float mubar);
-hmc_float calc_s_fermion_mp(const physics::lattices::Spinorfield_eo * phi_inv, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield_eo& phi, const hardware::System& system, hmc_float kappa, hmc_float mubar);
+hmc_float calc_s_fermion_mp(const physics::lattices::Spinorfield * phi_inv, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield& phi, const hardware::System& system);
+hmc_float calc_s_fermion_mp(const physics::lattices::Spinorfield_eo * phi_inv, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield_eo& phi, const hardware::System& system);
+
+hmc_observables metropolis(const hmc_float rnd, const hmc_float beta, const physics::lattices::Gaugefield& gf, const physics::lattices::Gaugefield& new_u, const physics::lattices::Gaugemomenta& p, const physics::lattices::Gaugemomenta& new_p, const physics::lattices::Spinorfield& phi, const physics::lattices::Spinorfield * const phi_inv, const hmc_float spinor_energy_init, const hmc_float spinor_energy_mp_init, const hardware::System& system); // TODO port
+hmc_observables metropolis(const hmc_float rnd, const hmc_float beta, const physics::lattices::Gaugefield& gf, const physics::lattices::Gaugefield& new_u, const physics::lattices::Gaugemomenta& p, const physics::lattices::Gaugemomenta& new_p, const physics::lattices::Spinorfield_eo& phi, const physics::lattices::Spinorfield_eo * const phi_inv, const hmc_float spinor_energy_init, const hmc_float spinor_energy_mp_init, const hardware::System& system); // TODO port
 
 }
 }
