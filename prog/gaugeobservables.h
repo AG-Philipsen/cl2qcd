148a80a4b0aa8f3ef4061a3b7f886b4c17381014
Matthias Bach <bach@compeng.uni-frankfurt.de>
2013-02-05 11:02:50 +0100

    Port gaugeobservables binary to new structure
    refs 303

--- prog/gaugeobservables.h
+++ prog/gaugeobservables.h
@@ -8,7 +8,4 @@
 //this includes header used by all executables
 #include "general_header.h"
 
-//should only be included in main prog
-#include "gaugefield_hybrid.h"
-
 #endif /* _GAUGEOBSERVABLESH_ */
