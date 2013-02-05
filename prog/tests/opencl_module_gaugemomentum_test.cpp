243fab912363e5e1ca86b3066b8fd88a996c59f2
Matthias Bach <bach@compeng.uni-frankfurt.de>
2013-02-05 16:21:24 +0100

    Don't use Gaugefield_hybrid in Gaugemomenta test
    refs 400

--- prog/tests/opencl_module_gaugemomentum_test.cpp
+++ prog/tests/opencl_module_gaugemomentum_test.cpp
@@ -1,7 +1,7 @@
-#include "../gaugefield_hybrid.h"
-
 #include "../meta/util.hpp"
 #include "../host_random.h"
+#include "../hardware/device.hpp"
+#include "../hardware/system.hpp"
 
 // use the boost test framework
 #define BOOST_TEST_DYN_LINK
@@ -12,40 +12,6 @@
 
 extern std::string const version;
 std::string const version = "0.1";
-
-class TestGaugefield : public Gaugefield_hybrid {
-
-public:
-	TestGaugefield(const hardware::System * system) : Gaugefield_hybrid(system), prng(*system) {
-		auto inputfile = system->get_inputparameters();
-		init(1, inputfile.get_use_gpu() ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, prng);
-		meta::print_info_hmc("test program", inputfile);
-		print_gaugeobservables(0);
-	};
-
-	virtual void init_tasks();
-	virtual void finalize_opencl();
-
-	hardware::code::Gaugemomentum * get_device();
-private:
-	physics::PRNG prng;
-};
-
-void TestGaugefield::init_tasks()
-{
-	opencl_modules = new hardware::code::Opencl_Module* [get_num_tasks()];
-	opencl_modules[0] = get_device_for_task(0)->get_gaugemomentum_code();
-}
-
-void TestGaugefield::finalize_opencl()
-{
-	Gaugefield_hybrid::finalize_opencl();
-}
-
-hardware::code::Gaugemomentum* TestGaugefield::get_device()
-{
-	return static_cast<hardware::code::Gaugemomentum*>(opencl_modules[0]);
-}
 
 void fill_with_one(hmc_float * sf_in, int size)
 {
@@ -85,9 +51,9 @@ void test_build(std::string inputfile)
 	logger.info() << "Init device";
 	meta::Inputparameters params = create_parameters(inputfile);
 	hardware::System system(params);
-	TestGaugefield cpu(&system);
-	logger.info() << "Finalize device";
-	cpu.finalize();
+for(auto device: system.get_devices()) {
+		device->get_gaugemomentum_code();
+	}
 	BOOST_MESSAGE("Test done");
 }
 
@@ -104,44 +70,41 @@ void test_set_zero_gm(std::string inputfile)
 	logger.info() << "Init device";
 	meta::Inputparameters params = create_parameters(inputfile);
 	hardware::System system(params);
-	TestGaugefield cpu(&system);
-	hardware::code::Gaugemomentum * device = cpu.get_device();
-	hmc_float * gm_in;
+for(auto device: system.get_devices()) {
+		auto code = device->get_gaugemomentum_code();
+		hmc_float * gm_in;
 
-	logger.info() << "create buffers";
-	size_t NUM_ELEMENTS_AE = meta::get_vol4d(params) * NDIM * meta::get_su3algebrasize();
-	gm_in = new hmc_float[NUM_ELEMENTS_AE];
-	fill_with_random(gm_in, NUM_ELEMENTS_AE, 123456);
-	BOOST_REQUIRE(gm_in);
+		logger.info() << "create buffers";
+		size_t NUM_ELEMENTS_AE = meta::get_vol4d(params) * NDIM * meta::get_su3algebrasize();
+		gm_in = new hmc_float[NUM_ELEMENTS_AE];
+		fill_with_random(gm_in, NUM_ELEMENTS_AE, 123456);
+		BOOST_REQUIRE(gm_in);
 
-	hardware::buffers::Gaugemomentum in(meta::get_vol4d(params) * NDIM, device->get_device());
-	device->importGaugemomentumBuffer(&in, reinterpret_cast<ae*>(gm_in));
-	hardware::buffers::Plain<hmc_float> sqnorm(1, device->get_device());
+		hardware::buffers::Gaugemomentum in(meta::get_vol4d(params) * NDIM, code->get_device());
+		code->importGaugemomentumBuffer(&in, reinterpret_cast<ae*>(gm_in));
+		hardware::buffers::Plain<hmc_float> sqnorm(1, device);
 
-	logger.info() << "|in|^2:";
-	device->set_float_to_gaugemomentum_squarenorm_device(&in, &sqnorm);
-	hmc_float cpu_back;
-	sqnorm.dump(&cpu_back);
-	logger.info() << cpu_back;
+		logger.info() << "|in|^2:";
+		code->set_float_to_gaugemomentum_squarenorm_device(&in, &sqnorm);
+		hmc_float cpu_back;
+		sqnorm.dump(&cpu_back);
+		logger.info() << cpu_back;
 
-	logger.info() << "Run kernel";
-	device->set_zero_gaugemomentum(&in);
+		logger.info() << "Run kernel";
+		code->set_zero_gaugemomentum(&in);
 
-	logger.info() << "|out|^2:";
-	device->set_float_to_gaugemomentum_squarenorm_device(&in, &sqnorm);
-	hmc_float cpu_back2;
-	sqnorm.dump(&cpu_back2);
-	logger.info() << cpu_back2;
+		logger.info() << "|out|^2:";
+		code->set_float_to_gaugemomentum_squarenorm_device(&in, &sqnorm);
+		hmc_float cpu_back2;
+		sqnorm.dump(&cpu_back2);
+		logger.info() << cpu_back2;
 
-	logger.info() << "Finalize device";
-	cpu.finalize();
+		logger.info() << "Free buffers";
+		delete[] gm_in;
 
-	logger.info() << "Free buffers";
-	delete[] gm_in;
-
-	testFloatAgainstInputparameters(cpu_back2, params);
+		testFloatAgainstInputparameters(cpu_back2, params);
+	}
 	BOOST_MESSAGE("Test done");
-
 }
 
 void test_gm_squarenorm(std::string inputfile)
@@ -152,40 +115,38 @@ void test_gm_squarenorm(std::string inputfile)
 	logger.info() << "Init device";
 	meta::Inputparameters params = create_parameters(inputfile);
 	hardware::System system(params);
-	TestGaugefield cpu(&system);
-	hardware::code::Gaugemomentum * device = cpu.get_device();
-	hmc_float * gm_in;
+for(auto device: system.get_devices()) {
+		auto code = device->get_gaugemomentum_code();
+		hmc_float * gm_in;
 
-	logger.info() << "create buffers";
-	size_t NUM_ELEMENTS_AE = meta::get_vol4d(params) * NDIM * meta::get_su3algebrasize();
-	gm_in = new hmc_float[NUM_ELEMENTS_AE];
+		logger.info() << "create buffers";
+		size_t NUM_ELEMENTS_AE = meta::get_vol4d(params) * NDIM * meta::get_su3algebrasize();
+		gm_in = new hmc_float[NUM_ELEMENTS_AE];
 
-	//use the variable use_cg to switch between cold and random input sf
-	if(params.get_solver() == meta::Inputparameters::cg) {
-		fill_with_one(gm_in, NUM_ELEMENTS_AE);
-	} else {
-		fill_with_random(gm_in, NUM_ELEMENTS_AE, 123456);
+		//use the variable use_cg to switch between cold and random input sf
+		if(params.get_solver() == meta::Inputparameters::cg) {
+			fill_with_one(gm_in, NUM_ELEMENTS_AE);
+		} else {
+			fill_with_random(gm_in, NUM_ELEMENTS_AE, 123456);
+		}
+		BOOST_REQUIRE(gm_in);
+
+		hardware::buffers::Gaugemomentum in(meta::get_vol4d(params) * NDIM, device);
+		code->importGaugemomentumBuffer(&in, reinterpret_cast<ae*>(gm_in));
+		hardware::buffers::Plain<hmc_float> sqnorm(1, device);
+
+		logger.info() << "Run kernel";
+		logger.info() << "|in|^2:";
+		code->set_float_to_gaugemomentum_squarenorm_device(&in, &sqnorm);
+		hmc_float cpu_back;
+		sqnorm.dump(&cpu_back);
+		logger.info() << cpu_back;
+
+		logger.info() << "Free buffers";
+		delete[] gm_in;
+
+		testFloatAgainstInputparameters(cpu_back, params);
 	}
-	BOOST_REQUIRE(gm_in);
-
-	hardware::buffers::Gaugemomentum in(meta::get_vol4d(params) * NDIM, device->get_device());
-	device->importGaugemomentumBuffer(&in, reinterpret_cast<ae*>(gm_in));
-	hardware::buffers::Plain<hmc_float> sqnorm(1, device->get_device());
-
-	logger.info() << "Run kernel";
-	logger.info() << "|in|^2:";
-	device->set_float_to_gaugemomentum_squarenorm_device(&in, &sqnorm);
-	hmc_float cpu_back;
-	sqnorm.dump(&cpu_back);
-	logger.info() << cpu_back;
-
-	logger.info() << "Finalize device";
-	cpu.finalize();
-
-	logger.info() << "Free buffers";
-	delete[] gm_in;
-
-	testFloatAgainstInputparameters(cpu_back, params);
 	BOOST_MESSAGE("Test done");
 }
 
