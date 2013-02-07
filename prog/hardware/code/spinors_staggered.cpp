3e7ec90c0779614ab2668eae906f42448f9c4f8c
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2013-02-07 13:02:38 +0100

    Added module for staggered spinor operations
    refs 425

---
+++ prog/hardware/code/spinors_staggered.cpp
@@ -0,0 +1,154 @@
+#include "spinors_staggered.hpp"
+
+#include "../../logger.hpp"
+#include "../../meta/util.hpp"
+#include "../device.hpp"
+#include <cassert>
+
+using namespace std;
+
+static std::string collect_build_options(hardware::Device * device, const meta::Inputparameters& params);
+
+static std::string collect_build_options(hardware::Device * device, const meta::Inputparameters& params)
+{
+	using namespace hardware::buffers;
+
+	std::ostringstream options;
+	options << "-D _FERMIONS_"
+	        << " -D SPINORFIELDSIZE=" << meta::get_spinorfieldsize(params) << " -D EOPREC_SPINORFIELDSIZE=" << meta::get_eoprec_spinorfieldsize(params);
+	if(check_Spinor_for_SOA(device)) {
+		options << " -D EOPREC_SPINORFIELD_STRIDE=" << get_Spinor_buffer_stride(meta::get_eoprec_spinorfieldsize(params), device);
+	}
+
+	return options.str();
+}
+
+
+void hardware::code::Spinors_staggered::fill_kernels()
+{
+	basic_fermion_code = get_device()->get_gaugefield_code()->get_sources() << ClSourcePackage(collect_build_options(get_device(), get_parameters())) << "types_fermions.h" << "operations_su3vec.cl" << "operations_spinor.cl" << "spinorfield.cl";
+	ClSourcePackage prng_code = get_device()->get_prng_code()->get_sources();
+	global_squarenorm = createKernel("global_squarenorm") << basic_fermion_code << "spinorfield_squarenorm.cl";
+	_global_squarenorm_reduction = createKernel("global_squarenorm_reduction") << basic_fermion_code << "spinorfield_squarenorm.cl";
+}
+
+void hardware::code::Spinors_staggered::clear_kernels()
+{
+	cl_int clerr = CL_SUCCESS;
+
+	clerr = clReleaseKernel(global_squarenorm);
+	if(clerr != CL_SUCCESS) throw Opencl_Error(clerr, "clReleaseKernel", __FILE__, __LINE__);
+	clerr = clReleaseKernel(_global_squarenorm_reduction);
+	if(clerr != CL_SUCCESS) throw Opencl_Error(clerr, "clReleaseKernel", __FILE__, __LINE__);
+}
+
+
+void hardware::code::Spinors_staggered::get_work_sizes(const cl_kernel kernel, size_t * ls, size_t * gs, cl_uint * num_groups) const
+{
+	Opencl_Module::get_work_sizes(kernel, ls, gs, num_groups);
+
+	if(kernel == global_squarenorm) {
+		if(*ls > 64) {
+			*ls = 64;
+		}
+		return;
+	}
+}
+
+
+void hardware::code::Spinors_staggered::global_squarenorm_reduction(const hardware::buffers::Plain<hmc_float> * out, const hardware::buffers::Plain<hmc_float> * tmp_buf)
+{
+	cl_int clerr = clSetKernelArg(_global_squarenorm_reduction, 0, sizeof(cl_mem), out->get_cl_buffer());
+	if(clerr != CL_SUCCESS) throw Opencl_Error(clerr, "clSetKernelArg", __FILE__, __LINE__);
+
+	clerr = clSetKernelArg(_global_squarenorm_reduction, 1, sizeof(cl_mem), tmp_buf->get_cl_buffer());
+	if(clerr != CL_SUCCESS) throw Opencl_Error(clerr, "clSetKernelArg", __FILE__, __LINE__);
+
+	cl_uint elems = tmp_buf->get_elements();
+	clerr = clSetKernelArg(_global_squarenorm_reduction, 2, sizeof(cl_uint), &elems);
+	if(clerr != CL_SUCCESS) throw Opencl_Error(clerr, "clSetKernelArg", __FILE__, __LINE__);
+
+	get_device()->enqueue_kernel(_global_squarenorm_reduction, 1, 1);
+}
+
+void hardware::code::Spinors_staggered::set_float_to_global_squarenorm_device(const hardware::buffers::Plain<spinor> * a, const hardware::buffers::Plain<hmc_float> * out)
+{
+	//query work-sizes for kernel
+	size_t ls2, gs2;
+	cl_uint num_groups;
+	this->get_work_sizes(global_squarenorm, &ls2, &gs2, &num_groups);
+
+	hardware::buffers::Plain<hmc_float> tmp(num_groups, get_device());
+
+	//set arguments
+	int clerr = clSetKernelArg(global_squarenorm, 0, sizeof(cl_mem), a->get_cl_buffer());
+	if(clerr != CL_SUCCESS) throw Opencl_Error(clerr, "clSetKernelArg", __FILE__, __LINE__);
+
+	//CP: these do not have to be args of the function since they are global objects to the class opencl??
+	clerr = clSetKernelArg(global_squarenorm, 1, sizeof(cl_mem), tmp);
+	if(clerr != CL_SUCCESS) throw Opencl_Error(clerr, "clSetKernelArg", __FILE__, __LINE__);
+
+	clerr = clSetKernelArg(global_squarenorm, 2, sizeof(hmc_float) * ls2, static_cast<void*>(nullptr));
+	if(clerr != CL_SUCCESS) throw Opencl_Error(clerr, "clSetKernelArg", __FILE__, __LINE__);
+
+	get_device()->enqueue_kernel(global_squarenorm , gs2, ls2);
+
+	global_squarenorm_reduction(out, &tmp);
+}
+
+
+size_t hardware::code::Spinors_staggered::get_read_write_size(const std::string& in) const
+{
+	//Depending on the compile-options, one has different sizes...
+	size_t D = meta::get_float_size(get_parameters());
+	size_t S = meta::get_spinorfieldsize(get_parameters());
+	size_t Seo = meta::get_eoprec_spinorfieldsize(get_parameters());
+	//factor for complex numbers
+	int C = 2;
+	//this is the same as in the function above
+	if (in == "global_squarenorm") {
+		return 1000000000000000000000000;
+	}
+	if (in == "global_squarenorm_reduction") {
+		return 1000000000000000000000000;
+	}
+	return 0;
+}
+
+uint64_t hardware::code::Spinors_staggered::get_flop_size(const std::string& in) const
+{
+	uint64_t S = meta::get_spinorfieldsize(get_parameters());
+	uint64_t Seo = meta::get_eoprec_spinorfieldsize(get_parameters());
+	//this is the same as in the function above
+	if (in == "global_squarenorm") {
+		return 1000000000000000000000000;
+	}
+	if (in == "global_squarenorm_reduction") {
+		return 1000000000000000000000000;
+	}
+	return 0;
+}
+
+void hardware::code::Spinors_staggered::print_profiling(const std::string& filename, int number) const
+{
+	Opencl_Module::print_profiling(filename, number);
+	Opencl_Module::print_profiling(filename, global_squarenorm);
+	Opencl_Module::print_profiling(filename, _global_squarenorm_reduction);
+}
+
+hardware::code::Spinors_staggered::Spinors_staggered(const meta::Inputparameters& params, hardware::Device * device)
+	: Opencl_Module(params, device)
+{
+	fill_kernels();
+}
+
+hardware::code::Spinors_staggered::~Spinors_staggered()
+{
+	clear_kernels();
+}
+
+ClSourcePackage hardware::code::Spinors_staggered::get_sources() const noexcept
+{
+	return basic_fermion_code;
+}
+
---
+++ prog/hardware/code/spinors_staggered.hpp
@@ -0,0 +1,90 @@
+/** @file
+ * Heatbath for OpenCL
+ */
+#ifndef _HARDWARE_CODE_SPINORS_STAGGERED
+#define _HARDWARE_CODE_SPINORS_STAGGERED
+
+#include "opencl_module.hpp"
+
+#include "../buffers/plain.hpp"
+#include "../buffers/spinor.hpp"
+#include "../buffers/prng_buffer.hpp"
+
+namespace hardware {
+
+namespace code {
+
+/**
+ * An OpenCL device
+ *
+ * Adds random numbers to basic Opencl_Module class
+ *
+ * @todo Everything is public to faciliate inheritance. Actually, more parts should be private.
+ */
+class Spinors_staggered : public Opencl_Module {
+public:
+	friend hardware::Device;
+
+	virtual ~Spinors_staggered();
+
+	/////////////////////////////////////////
+	//    linear Algebra operations
+	void global_squarenorm_reduction(const hardware::buffers::Plain<hmc_float> * out, const hardware::buffers::Plain<hmc_float> * tmp_buf);
+	void set_float_to_global_squarenorm_device(const hardware::buffers::Plain<spinor> * a, const hardware::buffers::Plain<hmc_float> * out);
+
+	/**
+	 * Print the profiling information to a file.
+	 *
+	 * @param filename Name of file where data is appended.
+	 */
+	void virtual print_profiling(const std::string& filename, int number) const override;
+
+	ClSourcePackage get_sources() const noexcept;
+
+	/**
+	 * Return amount of Floating point operations performed by a specific kernel per call.
+	 * NOTE: this is meant to be the "netto" amount in order to be comparable.
+	 *
+	 * @param in Name of the kernel under consideration.
+	 */
+	virtual uint64_t get_flop_size(const std::string& in) const override;
+
+	/**
+	 * Return amount of bytes read and written by a specific kernel per call.
+	 *
+	 * @param in Name of the kernel under consideration.
+	 */
+	virtual size_t get_read_write_size(const std::string& in) const override;
+
+protected:
+	/**
+	 * Add specific work_size determination for this child class
+	 */
+	virtual void get_work_sizes(const cl_kernel kernel, size_t * ls, size_t * gs, cl_uint * num_groups) const override;
+
+private:
+	Spinors_staggered(const meta::Inputparameters& params, hardware::Device * device);
+
+	/**
+	 * Collect the kernels for OpenCL.
+	 */
+	void fill_kernels();
+
+	/**
+	 * Clear out the kernels,
+	 */
+	void clear_kernels();
+
+	ClSourcePackage basic_fermion_code;
+
+	//Scalar Product
+	cl_kernel global_squarenorm;
+	cl_kernel _global_squarenorm_reduction;
+
+};
+
+}
+
+}
+
+#endif // _HARDWARE_CODE_SPINORS_
