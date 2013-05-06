97365a9b1c6e0739f14de305e9b6a51590a54324
Matthias Bach <bach@compeng.uni-frankfurt.de>
2013-05-06 16:12:30 +0200

    Fix git commit id reporting for out-of-source builds

--- prog/cmake/GenerateGitVersion.cmake
+++ prog/cmake/GenerateGitVersion.cmake
@@ -2,17 +2,17 @@ find_package(Git)
 
 if(GIT_FOUND)
 	execute_process( COMMAND ${GIT_EXECUTABLE} log -n 1 --pretty=format:%H
-	                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+	                 WORKING_DIRECTORY ${SOURCE_DIR}
 	                 OUTPUT_VARIABLE GIT_COMMIT_ID
 	                 OUTPUT_STRIP_TRAILING_WHITESPACE )
 	execute_process( COMMAND ${GIT_EXECUTABLE} diff --shortstat
-	                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+	                 WORKING_DIRECTORY ${SOURCE_DIR}
 	                 OUTPUT_VARIABLE GIT_DIRTY
 	                 OUTPUT_STRIP_TRAILING_WHITESPACE )
 	if( GIT_DIRTY )
 		set( GIT_COMMIT_ID "${GIT_COMMIT_ID}*" )
 	endif( GIT_DIRTY )
-
+	message("Git commit id: ${GIT_COMMIT_ID}")
 else(GIT_FOUND)
 	message(WARNING "Failed to find git, can't write git commit id to executables")
 	set(GIT_COMMIT_ID "N/A")
