c3ebf0ecfb7c1bfe81f0215d7add11af22bc3c50
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2013-05-22 17:07:10 +0200

    Uncommented and modified some methods in
    hardware/buffers/su3vec.cpp refs 477

--- prog/hardware/buffers/su3vec.cpp
+++ prog/hardware/buffers/su3vec.cpp
@@ -14,48 +14,50 @@
 typedef hmc_complex soa_storage_t;
 const size_t soa_storage_lanes = 3;
 
-/*To be added...
+static size_t calculate_su3vec_buffer_size(size_t elems, hardware::Device * device);
 
-static size_t calculate_spinor_buffer_size(size_t elems, hardware::Device * device);
-
-hardware::buffers::Spinor::Spinor(size_t elems, hardware::Device * device)
-	: Buffer(calculate_spinor_buffer_size(elems, device), device),
+hardware::buffers::SU3vec::SU3vec(size_t elems, hardware::Device * device)
+	: Buffer(calculate_su3vec_buffer_size(elems, device), device),
 	  elems(elems),
-	  soa(check_Spinor_for_SOA(device))
+	  soa(check_su3vec_for_SOA(device))
 {
 	// nothing to do
 }
 
-size_t hardware::buffers::check_Spinor_for_SOA(hardware::Device * device)
+size_t hardware::buffers::check_su3vec_for_SOA(hardware::Device * device)
 {
 	return device->get_prefers_soa();
 }
 
-static size_t calculate_spinor_buffer_size(size_t elems, hardware::Device * device)
+static size_t calculate_su3vec_buffer_size(size_t elems, hardware::Device * device)
 {
 	using namespace hardware::buffers;
-	if(check_Spinor_for_SOA(device)) {
-		size_t stride = get_Spinor_buffer_stride(elems, device);
+	if(check_su3vec_for_SOA(device)) {
+		size_t stride = get_su3vec_buffer_stride(elems, device);
 		return stride * soa_storage_lanes * sizeof(soa_storage_t);
 	} else {
-		return elems * sizeof(spinor);
+		return elems * sizeof(su3vec);
 	}
 }
 
-size_t hardware::buffers::get_Spinor_buffer_stride(size_t elems, Device * device)
+size_t hardware::buffers::get_su3vec_buffer_stride(size_t elems, Device * device)
 {
 	return device->recommend_stride(elems, sizeof(soa_storage_t), soa_storage_lanes);
 }
 
-size_t hardware::buffers::Spinor::get_elements() const noexcept
+size_t hardware::buffers::SU3vec::get_elements() const noexcept
 {
 	return elems;
 }
 
-bool hardware::buffers::Spinor::is_soa() const noexcept
+bool hardware::buffers::SU3vec::is_soa() const noexcept
 {
 	return soa;
 }
+
+/*To be added...
+  Remark that we have to write the convertSpinorfieldFromSOA_eo_device and convertSpinorfieldToSOA_eo_device
+  functions in the Spinors_staggered class in hardware::code
 
 void hardware::buffers::Spinor::load(const spinor * ptr, size_t elems, size_t offset) const
 {
@@ -117,20 +119,19 @@ hardware::SynchronizationEvent hardware::buffers::Spinor::dumpRect_rawAsync(void
 	logger.trace() << "Dumping raw data from Spinor buffer.";
 	return Buffer::dump_rectAsync(dest, buffer_origin, host_origin, region, buffer_row_pitch, buffer_slice_pitch, host_row_pitch, host_slice_pitch, event);
 }
+*/
 
-
-size_t hardware::buffers::Spinor::get_storage_type_size() const noexcept
+size_t hardware::buffers::SU3vec::get_storage_type_size() const noexcept
 {
 	return soa ? sizeof(soa_storage_t) : sizeof(spinor);
 }
 
-size_t hardware::buffers::Spinor::get_lane_stride() const noexcept
+size_t hardware::buffers::SU3vec::get_lane_stride() const noexcept
 {
 	return soa ? (get_bytes() / sizeof(soa_storage_t) / soa_storage_lanes) : 0;
 }
 
-size_t hardware::buffers::Spinor::get_lane_count() const noexcept
+size_t hardware::buffers::SU3vec::get_lane_count() const noexcept
 {
 	return soa ? soa_storage_lanes : 1;
 }
-*/
