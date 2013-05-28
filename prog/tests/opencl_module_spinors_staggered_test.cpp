edc9387b04c4f5a84c66cda42112054e9288c01c
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2013-05-28 15:11:32 +0200

    Added tests for scalar_produt_eo and reductions_eo for
    staggered fields refs 427

--- prog/tests/opencl_module_spinors_staggered_test.cpp
+++ prog/tests/opencl_module_spinors_staggered_test.cpp
@@ -905,34 +905,28 @@ void test_sf_cold_staggered_eo(std::string inputfile, bool switcher)
 	BOOST_MESSAGE("Test done");
 }
 
-
-/* To be added...
- *
- *
-
-
-void test_sf_scalar_product_eo(std::string inputfile)
+void test_sf_scalar_product_staggered_eo(std::string inputfile)
 {
 	using namespace hardware::buffers;
 
 	std::string kernelName;
-	kernelName = "scalar_product";
+	kernelName = "scalar_product_eoprec_staggered";
 	printKernelInfo(kernelName);
 	logger.info() << "Init device";
 	meta::Inputparameters params = create_parameters(inputfile);
 	hardware::System system(params);
-	auto * device = system.get_devices().at(0)->get_spinor_code();
+	auto * device = system.get_devices().at(0)->get_spinor_staggered_code();
 
 	logger.info() << "Fill buffers...";
-	size_t NUM_ELEMENTS_SF = meta::get_eoprec_spinorfieldsize(params);
-	const Spinor in(NUM_ELEMENTS_SF, device->get_device());
-	const Spinor in2(NUM_ELEMENTS_SF, device->get_device());
+	size_t NUM_ELEMENTS_SF = hardware::code::get_eoprec_spinorfieldsize(params);
+	const SU3vec in(NUM_ELEMENTS_SF, device->get_device());
+	const SU3vec in2(NUM_ELEMENTS_SF, device->get_device());
 	hardware::buffers::Plain<hmc_complex> sqnorm(1, device->get_device());
 
-	spinor * sf_in;
-	sf_in = new spinor[NUM_ELEMENTS_SF];
-	spinor * sf_in2;
-	sf_in2 = new spinor[NUM_ELEMENTS_SF];
+	su3vec * sf_in;
+	sf_in = new su3vec[NUM_ELEMENTS_SF];
+	su3vec * sf_in2;
+	sf_in2 = new su3vec[NUM_ELEMENTS_SF];
 	//use the variable use_cg to switch between cold and random input sf
 	if(params.get_solver() == meta::Inputparameters::cg) {
 	  fill_sf_with_one(sf_in, NUM_ELEMENTS_SF);
@@ -945,15 +939,23 @@ void test_sf_scalar_product_eo(std::string inputfile)
 	BOOST_REQUIRE(sf_in);
 	BOOST_REQUIRE(sf_in2);
 
+	//The following five lines are to be used to produce the ref_vec file needed to get the ref_value
+        //---> Comment them out when the reference values have been obtained! 
+        /*
+        print_staggeredfield_eo_to_textfile("ref_vec_sp1",sf_in,params); 
+        logger.info() << "Produced the ref_vec_sp1 text file with the staggered field for the ref. code.";   
+        print_staggeredfield_eo_to_textfile("ref_vec_sp2",sf_in2,params); 
+        logger.info() << "Produced the ref_vec_sp2 text file with the staggered field for the ref. code. Returning...";   
+        return;
+	// */
+	
 	in.load(sf_in);
 	in2.load(sf_in2);
-
-	auto spinor_code = device->get_device()->get_spinor_code();
 
 	logger.info() << "Run kernel";
 	logger.info() << "result:";
 	hmc_complex cpu_res_tmp;
-	spinor_code->set_complex_to_scalar_product_eoprec_device(&in, &in2, &sqnorm);
+	device->set_complex_to_scalar_product_eoprec_device(&in, &in2, &sqnorm);
 	sqnorm.dump(&cpu_res_tmp);
 	hmc_float cpu_res = cpu_res_tmp.re + cpu_res_tmp.im;
 	logger.info() << cpu_res;
@@ -961,6 +963,15 @@ void test_sf_scalar_product_eo(std::string inputfile)
 	testFloatAgainstInputparameters(cpu_res, params);
 	BOOST_MESSAGE("Test done");
 }
+
+
+
+
+/* To be added...
+ *
+ *
+
+
 
 
 
@@ -1799,6 +1810,40 @@ BOOST_AUTO_TEST_CASE( SF_SQUARENORM_EO_REDUCTION_3 )
 BOOST_AUTO_TEST_SUITE_END()
 
 
+BOOST_AUTO_TEST_SUITE(SF_SCALAR_PRODUCT_EO)
+
+BOOST_AUTO_TEST_CASE( SF_SCALAR_PRODUCT_EO_1 )
+{
+  test_sf_scalar_product_staggered_eo("/sf_scalar_product_staggered_eo_input_1");
+}
+
+BOOST_AUTO_TEST_CASE( SF_SCALAR_PRODUCT_EO_2 )
+{
+  test_sf_scalar_product_staggered_eo("/sf_scalar_product_staggered_eo_input_2");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(SF_SCALAR_PRODUCT_EO_REDUCTION)
+
+BOOST_AUTO_TEST_CASE( SF_SCALAR_PRODUCT_EO_REDUCTION_1 )
+{
+  test_sf_scalar_product_staggered_eo("/sf_scalar_product_staggered_eo_reduction_input_1");
+}
+
+BOOST_AUTO_TEST_CASE( SF_SCALAR_PRODUCT_EO_REDUCTION_2 )
+{
+  test_sf_scalar_product_staggered_eo("/sf_scalar_product_staggered_eo_reduction_input_2");
+}
+
+BOOST_AUTO_TEST_CASE( SF_SCALAR_PRODUCT_EO_REDUCTION_3 )
+{
+  test_sf_scalar_product_staggered_eo("/sf_scalar_product_staggered_eo_reduction_input_3");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
 BOOST_AUTO_TEST_SUITE(SF_COLD_EO)
 
 BOOST_AUTO_TEST_CASE( SF_COLD_EO_1 )
@@ -1821,40 +1866,6 @@ BOOST_AUTO_TEST_SUITE_END()
 /* To be added...
  *
  *
-
-
-BOOST_AUTO_TEST_SUITE(SF_SCALAR_PRODUCT_EO)
-
-BOOST_AUTO_TEST_CASE( SF_SCALAR_PRODUCT_EO_1 )
-{
-  test_sf_scalar_product_eo("/sf_scalar_product_eo_input_1");
-}
-
-BOOST_AUTO_TEST_CASE( SF_SCALAR_PRODUCT_EO_2 )
-{
-  test_sf_scalar_product_eo("/sf_scalar_product_eo_input_2");
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(SF_SCALAR_PRODUCT_EO_REDUCTION)
-
-BOOST_AUTO_TEST_CASE( SF_SCALAR_PRODUCT_EO_REDUCTION_1 )
-{
-  test_sf_scalar_product_eo("/sf_scalar_product_eo_reduction_input_1");
-}
-
-BOOST_AUTO_TEST_CASE( SF_SCALAR_PRODUCT_EO_REDUCTION_2 )
-{
-  test_sf_scalar_product_eo("/sf_scalar_product_eo_reduction_input_2");
-}
-
-BOOST_AUTO_TEST_CASE( SF_SCALAR_PRODUCT_EO_REDUCTION_3 )
-{
-  test_sf_scalar_product_eo("/sf_scalar_product_eo_reduction_input_3");
-}
-
-BOOST_AUTO_TEST_SUITE_END()
 
 
 
