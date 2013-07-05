835097bd404123968fbc18e5ab1f8e2f47db190b
Matthias Bach <bach@compeng.uni-frankfurt.de>
2013-07-05 11:25:53 +0200

    Split up halo update functionality for asynchroneous use

--- prog/hardware/buffers/halo_update.hpp
+++ prog/hardware/buffers/halo_update.hpp
@@ -22,7 +22,28 @@ namespace hardware {
 namespace buffers {
 
 template <typename T, class BUFFER> void update_halo(std::vector<BUFFER*> buffers, const meta::Inputparameters& params, const float ELEMS_PER_SITE = 1.);
+/**
+ * Update the halo of the given buffers.
+ */
 template <typename T, class BUFFER> void update_halo_soa(std::vector<BUFFER*> buffers, const hardware::System& system, const float ELEMS_PER_SITE = 1., const unsigned CHUNKS_PER_LANE = 1, unsigned reqd_width = 0);
+/**
+ * Initialize the halo update of the given buffers.
+ *
+ * Extraction of the halo data happens synchroneous to the default queue of each buffer.
+ * Therefore commands using the default queue of this buffer can safely operate on the non-halo / own elements of this buffer if queued after this call.
+ *
+ * Note that depending on the exact transfer method used this might already transfer data between devices!
+ */
+template <typename T, class BUFFER> void initialize_update_halo_soa(std::vector<BUFFER*> buffers, const hardware::System& system, const float ELEMS_PER_SITE = 1., const unsigned CHUNKS_PER_LANE = 1, unsigned reqd_width = 0);
+/**
+ * Finish up a previously started halo update.
+ *
+ * Access the the halo data happens synchroneous to the default queue of each buffer.
+ * Therefore commands using the default queue of this buffer can safely operate on the non-halo / own elements of this buffer if queued after this call.
+ *
+ * Note that depending on the exact transfer methods this might cause data transfer between devices.
+ */
+template <typename T, class BUFFER> void finalize_update_halo_soa(std::vector<BUFFER*> buffers, const hardware::System& system, const float ELEMS_PER_SITE = 1., const unsigned CHUNKS_PER_LANE = 1, unsigned reqd_width = 0);
 
 template<typename BUFFER> static hardware::SynchronizationEvent extract_boundary(hardware::Transfer * transfer, const BUFFER * buffer, size_t in_lane_offset, size_t HALO_CHUNK_ELEMS, const float ELEMS_PER_SITE, const unsigned CHUNKS_PER_LANE, const hardware::SynchronizationEvent& event);
 template<typename BUFFER> static hardware::SynchronizationEvent send_halo(hardware::Transfer * transfer, const BUFFER * buffer, size_t in_lane_offset, size_t HALO_CHUNK_ELEMS, const float ELEMS_PER_SITE, const unsigned CHUNKS_PER_LANE, const hardware::SynchronizationEvent& event);
@@ -174,87 +195,112 @@ class ProxyBufferCache {
 }
 }
 
-template <typename T, class BUFFER> void hardware::buffers::update_halo_soa(std::vector<BUFFER*> buffers, const hardware::System& system, const float ELEMS_PER_SITE, const unsigned CHUNKS_PER_LANE, unsigned reqd_width = 0)
-{
-	// TODO all transfer imply that buffers are mapped 1:1 to devices, this must be improved!
-	auto const & params = system.get_inputparameters();
-	const size_t num_buffers = buffers.size();
-
-	if(num_buffers > 1) {
+template<class BUFFER> struct UpdateHaloSOAhelper {
+	UpdateHaloSOAhelper(std::vector<BUFFER*> const & buffers, const hardware::System& system, const float ELEMS_PER_SITE, const unsigned CHUNKS_PER_LANE, unsigned reqd_width) {
+		auto const & params = system.get_inputparameters();
 		const auto main_device = buffers[0]->get_device();
 		const size_4 grid_dims = main_device->get_grid_size();
 		if(grid_dims.x != 1 || grid_dims.y != 1 || grid_dims.z != 1) {
 			throw Print_Error_Message("Only the time-direction can be parallelized");
 		}
-		const unsigned GRID_SIZE = grid_dims.t;
-		const unsigned HALO_SIZE = main_device->get_halo_size();
-		const unsigned VOLSPACE = meta::get_volspace(params) * ELEMS_PER_SITE;
-		if(reqd_width > HALO_SIZE) {
+		grid_size = grid_dims.t;
+		halo_size = main_device->get_halo_size();
+		volspace = meta::get_volspace(params) * ELEMS_PER_SITE;
+		if(reqd_width > halo_size) {
 			std::ostringstream tmp;
-			tmp << "Requested halo width is " << reqd_width << ", but maximum halo width is " << HALO_SIZE;
+			tmp << "Requested halo width is " << reqd_width << ", but maximum halo width is " << halo_size;
 			throw std::invalid_argument(tmp.str());
 		}
 		if(!reqd_width) {
-			reqd_width = HALO_SIZE;
+			reqd_width = halo_size;
 		}
-		const unsigned TOTAL_HALO_ELEMS = HALO_SIZE * VOLSPACE * CHUNKS_PER_LANE;
-		const unsigned TOTAL_HALO_CHUNK_ELEMS = HALO_SIZE * VOLSPACE;
-		const unsigned REQD_HALO_ELEMS = reqd_width * VOLSPACE * CHUNKS_PER_LANE;
-		const unsigned REQD_HALO_CHUNK_ELEMS = reqd_width * VOLSPACE;
-		const unsigned VOL4D_LOCAL = get_vol4d(main_device->get_local_lattice_size()) * ELEMS_PER_SITE;
+		total_halo_elems = halo_size * volspace * CHUNKS_PER_LANE;
+		total_halo_chunk_elems = halo_size * volspace;
+		reqd_halo_elems = reqd_width * volspace * CHUNKS_PER_LANE;
+		reqd_halo_chunk_elems = reqd_width * volspace;
+		vol4d_local = get_vol4d(main_device->get_local_lattice_size()) * ELEMS_PER_SITE;
 
-		logger.trace() << "GRID_SIZE: " << GRID_SIZE;
-		logger.trace() << "HALO_SIZE: " << HALO_SIZE;
+		logger.trace() << "GRID_SIZE: " << grid_size;
+		logger.trace() << "HALO_SIZE: " << halo_size;
 		logger.trace() << "reqd_width: " << reqd_width;
-		logger.trace() << "eff. VOLSPACE: " << VOLSPACE;
-		logger.trace() << "TOTAL HALO ELEMS: " << TOTAL_HALO_ELEMS;
-		logger.trace() << "TOTAL HALO ELEMS per CHUNK: " << TOTAL_HALO_CHUNK_ELEMS;
-		logger.trace() << "REQD HALO ELEMS: " << REQD_HALO_ELEMS;
-		logger.trace() << "REQD HALO ELEMS per CHUNK: " << REQD_HALO_CHUNK_ELEMS;
-		logger.trace() << "eff. VOL4D_LOCAL: " << VOL4D_LOCAL;
+		logger.trace() << "eff. VOLSPACE: " << volspace;
+		logger.trace() << "TOTAL HALO ELEMS: " << total_halo_elems;
+		logger.trace() << "TOTAL HALO ELEMS per CHUNK: " << total_halo_chunk_elems;
+		logger.trace() << "REQD HALO ELEMS: " << reqd_halo_elems;
+		logger.trace() << "REQD HALO ELEMS per CHUNK: " << reqd_halo_chunk_elems;
+		logger.trace() << "eff. VOL4D_LOCAL: " << vol4d_local;
 		logger.trace() << "CHUNKS_PER_LANE: " << CHUNKS_PER_LANE;
 
-		std::vector<hardware::Device*> devices(num_buffers);
-		for(size_t i = 0; i < num_buffers; ++i) {
-			devices[i] = buffers[i]->get_device();
-		}
+	}
+
+	unsigned grid_size;
+	unsigned halo_size;
+	unsigned volspace;
+	unsigned total_halo_elems;
+	unsigned total_halo_chunk_elems;
+	unsigned reqd_halo_elems;
+	unsigned reqd_halo_chunk_elems;
+	unsigned vol4d_local;
+};
+
+template <typename T, class BUFFER> void hardware::buffers::initialize_update_halo_soa(std::vector<BUFFER*> buffers, const hardware::System& system, const float ELEMS_PER_SITE, const unsigned CHUNKS_PER_LANE, unsigned reqd_width = 0)
+{
+	// TODO all transfer imply that buffers are mapped 1:1 to devices, this must be improved!
+	const size_t num_buffers = buffers.size();
+	if(num_buffers > 1) {
+		UpdateHaloSOAhelper<BUFFER> const helper(buffers, system, ELEMS_PER_SITE, CHUNKS_PER_LANE, reqd_width);
+
+		auto const devices = system.get_devices();
 
 		// copy inside of boundaries to host
 		for(size_t i = 0; i < num_buffers; ++i) {
 			const auto buffer = buffers[i];
 			logger.debug() << "Extracting data from buffer " << i;
-			auto const up_transfer = system.get_transfer(i, upper_grid_neighbour(i, GRID_SIZE), UP_TRANSFER);
-			extract_boundary(up_transfer, buffer, VOL4D_LOCAL - REQD_HALO_CHUNK_ELEMS, REQD_HALO_CHUNK_ELEMS, ELEMS_PER_SITE, CHUNKS_PER_LANE, SynchronizationEvent());
-			auto const down_transfer = system.get_transfer(i, lower_grid_neighbour(i, GRID_SIZE), DOWN_TRANSFER);
-			extract_boundary(down_transfer, buffer, 0, REQD_HALO_CHUNK_ELEMS, ELEMS_PER_SITE, CHUNKS_PER_LANE, SynchronizationEvent());
+			auto const up_transfer = system.get_transfer(i, upper_grid_neighbour(i, helper.grid_size), UP_TRANSFER);
+			extract_boundary(up_transfer, buffer, helper.vol4d_local - helper.reqd_halo_chunk_elems, helper.reqd_halo_chunk_elems, ELEMS_PER_SITE, CHUNKS_PER_LANE, SynchronizationEvent());
+			auto const down_transfer = system.get_transfer(i, lower_grid_neighbour(i, helper.grid_size), DOWN_TRANSFER);
+			extract_boundary(down_transfer, buffer, 0, helper.reqd_halo_chunk_elems, ELEMS_PER_SITE, CHUNKS_PER_LANE, SynchronizationEvent());
 		}
 
-		for(auto device: devices) {
+		for(auto * device: devices) {
 			device->flush();
 		}
 
 		// trigger transfers (might be a NOOP)
 		for(size_t i = 0; i < num_buffers; ++i) {
-			auto const up_transfer = system.get_transfer(i, upper_grid_neighbour(i, GRID_SIZE), UP_TRANSFER);
+			auto const up_transfer = system.get_transfer(i, upper_grid_neighbour(i, helper.grid_size), UP_TRANSFER);
 			up_transfer->transfer();
-			auto const down_transfer = system.get_transfer(i, lower_grid_neighbour(i, GRID_SIZE), DOWN_TRANSFER);
+			auto const down_transfer = system.get_transfer(i, lower_grid_neighbour(i, helper.grid_size), DOWN_TRANSFER);
 			down_transfer->transfer();
 		}
+	}
+}
 
+template <typename T, class BUFFER> void hardware::buffers::finalize_update_halo_soa(std::vector<BUFFER*> buffers, const hardware::System& system, const float ELEMS_PER_SITE, const unsigned CHUNKS_PER_LANE, unsigned reqd_width = 0)
+{
+	const size_t num_buffers = buffers.size();
+	if(num_buffers > 1) {
+		UpdateHaloSOAhelper<BUFFER> const helper(buffers, system, ELEMS_PER_SITE, CHUNKS_PER_LANE, reqd_width);
 
-		// copy data from host to halo (of coure getting what the neighbour stored
+		// copy data from host to halo (of course getting what the neighbour stored
 		for(size_t i = 0; i < num_buffers; ++i) {
 			const auto buffer = buffers[i];
 			// our lower halo is the upper bounary of our lower neighbour
 			// its storage location is wrapped around to be the last chunk of data in our buffer, that is after local data and upper halo
-			auto const up_transfer = system.get_transfer(lower_grid_neighbour(i, GRID_SIZE), i, UP_TRANSFER);
+			auto const up_transfer = system.get_transfer(lower_grid_neighbour(i, helper.grid_size), i, UP_TRANSFER);
 			logger.debug() << "Sending data to buffer " << i;
-			send_halo(up_transfer, buffer, VOL4D_LOCAL + 2 * TOTAL_HALO_CHUNK_ELEMS - REQD_HALO_CHUNK_ELEMS, REQD_HALO_CHUNK_ELEMS, ELEMS_PER_SITE, CHUNKS_PER_LANE, SynchronizationEvent());
+			send_halo(up_transfer, buffer, helper.vol4d_local + 2 * helper.total_halo_chunk_elems - helper.reqd_halo_chunk_elems, helper.reqd_halo_chunk_elems, ELEMS_PER_SITE, CHUNKS_PER_LANE, SynchronizationEvent());
 			// our upper halo is the lower bounary of our upper neighbour, it's stored right after our local data
-			auto const down_transfer = system.get_transfer(upper_grid_neighbour(i, GRID_SIZE), i, DOWN_TRANSFER);
-			send_halo(down_transfer, buffer, VOL4D_LOCAL, REQD_HALO_CHUNK_ELEMS, ELEMS_PER_SITE, CHUNKS_PER_LANE, SynchronizationEvent());
+			auto const down_transfer = system.get_transfer(upper_grid_neighbour(i, helper.grid_size), i, DOWN_TRANSFER);
+			send_halo(down_transfer, buffer, helper.vol4d_local, helper.reqd_halo_chunk_elems, ELEMS_PER_SITE, CHUNKS_PER_LANE, SynchronizationEvent());
 		}
 	}
+}
+
+template <typename T, class BUFFER> void hardware::buffers::update_halo_soa(std::vector<BUFFER*> buffers, const hardware::System& system, const float ELEMS_PER_SITE, const unsigned CHUNKS_PER_LANE, unsigned reqd_width = 0)
+{
+	initialize_update_halo_soa<T>(buffers, system, ELEMS_PER_SITE, CHUNKS_PER_LANE, reqd_width);
+	finalize_update_halo_soa<T>(buffers, system, ELEMS_PER_SITE, CHUNKS_PER_LANE, reqd_width);
 }
 
 #endif /* _HARDWARE_BUFFERS_HALO_UPDATE_ */
