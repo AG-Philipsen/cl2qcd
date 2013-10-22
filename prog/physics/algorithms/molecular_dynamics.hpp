36c2d2ae30e9cf69bfdf119876559d8fc734a880
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2013-10-22 15:43:42 +0200

    MD stagg. update functions modified using
    Rooted_Staggeredfield_eo class refs 503

--- prog/physics/algorithms/molecular_dynamics.hpp
+++ prog/physics/algorithms/molecular_dynamics.hpp
@@ -10,7 +10,7 @@
 #include "../lattices/gaugefield.hpp"
 #include "../lattices/spinorfield.hpp"
 #include "../lattices/spinorfield_eo.hpp"
-#include "../lattices/staggeredfield_eo.hpp"
+#include "../lattices/rooted_staggeredfield_eo.hpp"
 #include "../lattices/gaugemomenta.hpp"
 #include "rational_approximation.hpp"
 
@@ -27,12 +27,12 @@ void md_update_spinorfield_mp(const physics::lattices::Spinorfield_eo * out, con
 
 void md_update_gaugemomentum(const physics::lattices::Gaugemomenta * const inout, hmc_float eps, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield& phi, const hardware::System& system, hmc_float kappa = ARG_DEF, hmc_float mubar = ARG_DEF);
 void md_update_gaugemomentum(const physics::lattices::Gaugemomenta * const inout, hmc_float eps, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield_eo& phi, const hardware::System& system, hmc_float kappa = ARG_DEF, hmc_float mubar = ARG_DEF);
-void md_update_gaugemomentum(const physics::lattices::Gaugemomenta * const inout, hmc_float eps, const physics::lattices::Gaugefield& gf, const physics::lattices::Staggeredfield_eo& phi, const physics::algorithms::Rational_Coefficients& coeff, const hardware::System& system, hmc_float mass);
+void md_update_gaugemomentum(const physics::lattices::Gaugemomenta * const inout, hmc_float eps, const physics::lattices::Gaugefield& gf, const physics::lattices::Rooted_Staggeredfield_eo& phi, const hardware::System& system, hmc_float mass, hmc_float mubar = ARG_DEF); //mubar is so far NOT IMPLEMENTED!!
 
 void md_update_gaugemomentum_gauge(const physics::lattices::Gaugemomenta * const gm, hmc_float eps, const physics::lattices::Gaugefield& gf, const hardware::System& system);
 void md_update_gaugemomentum_fermion(const physics::lattices::Gaugemomenta * const inout, hmc_float eps, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield& phi, const hardware::System& system, hmc_float kappa = ARG_DEF, hmc_float mubar = ARG_DEF);
 void md_update_gaugemomentum_fermion(const physics::lattices::Gaugemomenta * const inout, hmc_float eps, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield_eo& phi, const hardware::System& system, hmc_float kappa = ARG_DEF, hmc_float mubar = ARG_DEF);
-void md_update_gaugemomentum_fermion(const physics::lattices::Gaugemomenta * const inout, hmc_float eps, const physics::lattices::Gaugefield& gf, const physics::lattices::Staggeredfield_eo& phi, const physics::algorithms::Rational_Coefficients& coeff, const hardware::System& system, hmc_float mass);
+void md_update_gaugemomentum_fermion(const physics::lattices::Gaugemomenta * const inout, hmc_float eps, const physics::lattices::Gaugefield& gf, const physics::lattices::Rooted_Staggeredfield_eo& phi, const hardware::System& system, hmc_float mass, hmc_float mubar = ARG_DEF); //mubar is so far NOT IMPLEMENTED!!
 void md_update_gaugemomentum_detratio(const physics::lattices::Gaugemomenta * const inout, hmc_float eps, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield& phi, const hardware::System& system);
 void md_update_gaugemomentum_detratio(const physics::lattices::Gaugemomenta * const inout, hmc_float eps, const physics::lattices::Gaugefield& gf, const physics::lattices::Spinorfield_eo& phi_mp, const hardware::System& system);
 }
