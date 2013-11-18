cf4d6601f914cbf8fa86a96a006b856cf89a507d
Matthias Bach <bach@compeng.uni-frankfurt.de>
2013-11-18 22:07:47 +0100

    Added copyright and license commentes to CMake files
    refs 513

--- prog/CTestConfig.cmake
+++ prog/CTestConfig.cmake
@@ -4,6 +4,25 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
+##
+## Copyright 2012, 2013 Lars Zeidlewicz, Christopher Pinke,
+## Matthias Bach, Christian Schäfer, Stefano Lottini, Alessandro Sciarra
+##
+## This file is part of CL2QCD.
+##
+## CL2QCD is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## CL2QCD is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with CL2QCD.  If not, see <http://www.gnu.org/licenses/>.
+
 set(CTEST_PROJECT_NAME "clhmc")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
--- prog/cmake/Macros.cmake
+++ prog/cmake/Macros.cmake
@@ -1,6 +1,21 @@
 # Utility functions for specifying the build of OpTiMaL
 #
 # Copyright (c) 2012 Matthias Bach <bach@compeng.uni-frankfurt.de>
+#
+# This file is part of CL2QCD.
+#
+# CL2QCD is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CL2QCD is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CL2QCD.  If not, see <http://www.gnu.org/licenses/>.
 
 # Create a list of files just as you'd use set.
 # Will automatically replace all filenames by absolute ones
