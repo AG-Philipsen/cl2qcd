bc4c48d7002346a1bc2006a738d3c008dc23b515
Matthias Bach <bach@compeng.uni-frankfurt.de>
2013-11-18 20:27:20 +0100

    Added copyright and license commentes to CPP implementation
    files refs 513

--- prog/hardware/code/opencl_module.cpp
+++ prog/hardware/code/opencl_module.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2012, 2013 Lars Zeidlewicz, Christopher Pinke,
+ * Matthias Bach, Christian Schäfer, Stefano Lottini, Alessandro Sciarra
+ *
+ * This file is part of CL2QCD.
+ *
+ * CL2QCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CL2QCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CL2QCD.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "opencl_module.hpp"
 
 #include <fstream>
--- prog/meta/util.cpp
+++ prog/meta/util.cpp
@@ -1,9 +1,24 @@
 /** @file
  * Generic utility functions
- * 
+ *
  * (c) 2012 Matthias Bach <bach@compeng.uni-frankfurt.de>
  * (c) 2012 Christopher Pinke <pinke@compeng.uni-frankfurt.de>
  * (c) 2013 Alessandro Sciarra <sciarra@compeng.uni-frankfurt.de>
+ *
+ * This file is part of CL2QCD.
+ *
+ * CL2QCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CL2QCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CL2QCD.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "util.hpp"
