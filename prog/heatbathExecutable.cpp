394ac1d4fe8ed08002ecca619847b46348baf425
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2013-12-05 22:28:56 +0100

    Moved heatbathExecutable class to heatbathExecutable.h
    refs 550

--- prog/heatbathExecutable.cpp
+++ prog/heatbathExecutable.cpp
@@ -1,114 +1,104 @@
 #include "heatbathExecutable.h"
 
-class heatbathExecutable: public generalExecutable
+inline heatbathExecutable::heatbathExecutable(int argc, const char* argv[]) :
+		generalExecutable(argc, argv)
 {
-public:
-	heatbathExecutable(int argc, const char* argv[]) : generalExecutable(argc, argv)
+	initializationTimer.reset();
+	prng = new physics::PRNG(*system);
+	gaugefield = new physics::lattices::Gaugefield(*system, *prng);
+	meta::print_info_heatbath(ownName, parameters);
+	writeHeatbathLogfile();
+	setIterationParameters();
+	initializationTimer.add();
+}
+
+inline void heatbathExecutable::performHeatbathAndMeasureGaugeObservables()
+{
+	performanceTimer.reset();
+	performThermalization();
+	performHeatbathAndMeasurements();
+	performanceTimer.add();
+}
+
+inline void heatbathExecutable::writeHeatbathLogfile()
+{
+	outputToFile.open(filenameForHeatbathLogfile,
+			std::ios::out | std::ios::app);
+	if (outputToFile.is_open()) {
+		meta::print_info_heatbath(ownName, &outputToFile, parameters);
+		outputToFile.close();
+	} else {
+		throw File_Exception(filenameForHeatbathLogfile);
+	}
+}
+
+inline void heatbathExecutable::setIterationParameters()
+{
+	thermalizationSteps = parameters.get_thermalizationsteps();
+	heatbathSteps = parameters.get_heatbathsteps();
+	overrelaxSteps = parameters.get_overrelaxsteps();
+	writeFrequency = parameters.get_writefrequency();
+	saveFrequency = parameters.get_savefrequency();
+}
+
+inline void heatbathExecutable::performThermalization()
+{
+	logger.info() << "Start thermalization";
+	int iteration = 0;
+	writeGaugeObservablesToScreen(iteration);
+	for (; iteration < thermalizationSteps; iteration++)
 	{
-		initializationTimer.reset();
-		prng = new physics::PRNG(*system);
-		gaugefield = new physics::lattices::Gaugefield(*system, *prng);
-		meta::print_info_heatbath(ownName, parameters);
-		writeHeatbathLogfile();
-		setIterationParameters();
-		initializationTimer.add();
+		physics::algorithms::heatbath(*gaugefield, *prng);
 	}
+	logger.info() << "thermalization done";
+}
 
-	void performHeatbathAndMeasureGaugeObservables()
+inline void heatbathExecutable::writeGaugeObservablesToFile(int& iteration)
+{
+	if (((iteration + 1) % writeFrequency) == 0) {
+		filenameForGaugeobservables = meta::get_gauge_obs_file_name(parameters,
+				"");
+		print_gaugeobservables(*gaugefield, iteration,
+				filenameForGaugeobservables);
+	}
+}
+
+inline void heatbathExecutable::writeGaugeObservablesToScreen(int& iteration)
+{
+	if (parameters.get_print_to_screen() || (iteration == 0)) {
+		print_gaugeobservables(*gaugefield, iteration);
+	}
+}
+
+inline void heatbathExecutable::writeGaugeObservablesToScreenAndFile(int iteration) {
+	writeGaugeObservablesToScreen(iteration);
+	writeGaugeObservablesToFile(iteration);
+}
+
+inline void heatbathExecutable::saveGaugefield(int iteration)
+{
+	if (((saveFrequency != 0) && ((iteration + 1) % saveFrequency) == 0)
+			|| (iteration == heatbathSteps - 1)) {
+		gaugefield->save(iteration + 1);
+	}
+}
+
+inline void heatbathExecutable::measureGaugeObservables(int& iteration)
+{
+	writeGaugeObservablesToScreenAndFile(iteration);
+}
+
+inline void heatbathExecutable::performHeatbathAndMeasurements()
+{
+	logger.info() << "Start heatbath";
+	for (int iteration = 0; iteration < heatbathSteps; iteration++)
 	{
-		performanceTimer.reset();
-		performThermalization();
-		performHeatbathAndMeasurements();
-		performanceTimer.add();
+		physics::algorithms::heatbath(*gaugefield, *prng, overrelaxSteps);
+		measureGaugeObservables(iteration);
+		saveGaugefield(iteration);
 	}
-
-private:
-	physics::PRNG * prng;
-	physics::lattices::Gaugefield * gaugefield;
-	const std::string 	filenameForHeatbathLogfile 		= "heatbath.log";
-	std::string filenameForGaugeobservables;
-	int thermalizationSteps;
-	int heatbathSteps;
-	int overrelaxSteps;
-	int writeFrequency;
-	int saveFrequency;
-
-	void writeHeatbathLogfile()
-	{
-		outputToFile.open(filenameForHeatbathLogfile, std::ios::out | std::ios::app);
-		if (outputToFile.is_open()) {
-			meta::print_info_heatbath(ownName, &outputToFile, parameters);
-			outputToFile.close();
-		} else {
-			throw File_Exception(filenameForHeatbathLogfile);
-		}
-	}
-
-	void setIterationParameters()
-	{
-		thermalizationSteps = 	parameters.get_thermalizationsteps();
-		heatbathSteps = 		parameters.get_heatbathsteps();
-		overrelaxSteps = 		parameters.get_overrelaxsteps();
-		writeFrequency = 		parameters.get_writefrequency();
-		saveFrequency = 		parameters.get_savefrequency();
-	}
-
-	void performThermalization()
-	{
-		logger.info() << "Start thermalization";
-		int iteration = 0;
-		writeGaugeObservablesToScreen(iteration);
-		for (; iteration < thermalizationSteps; iteration++)
-		{
-			physics::algorithms::heatbath(*gaugefield, *prng);
-		}
-		logger.info() << "thermalization done";
-	}
-
-	void writeGaugeObservablesToFile(int& iteration)
-	{
-		if (((iteration + 1) % writeFrequency) == 0) {
-			filenameForGaugeobservables = meta::get_gauge_obs_file_name(parameters, "");
-			print_gaugeobservables(*gaugefield, iteration, filenameForGaugeobservables);
-		}
-	}
-
-	void writeGaugeObservablesToScreen(int& iteration)
-	{
-		if ( parameters.get_print_to_screen() || (iteration == 0) ) {
-				print_gaugeobservables(*gaugefield, iteration);
-		}
-	}
-
-	void writeGaugeObservablesToScreenAndFile(int iteration)
-	{
-		writeGaugeObservablesToScreen(iteration);
-		writeGaugeObservablesToFile(iteration);
-	}
-
-	void saveGaugefield(int iteration)
-	{
-		if (((saveFrequency != 0) && ((iteration + 1) % saveFrequency) == 0)
-				|| (iteration == heatbathSteps - 1)) {
-			gaugefield->save(iteration + 1);
-		}
-	}
-
-	void measureGaugeObservables(int& iteration)
-	{
-		writeGaugeObservablesToScreenAndFile(iteration);
-	}
-
-	void performHeatbathAndMeasurements() {
-		logger.info() << "Start heatbath";
-		for (int iteration = 0; iteration < heatbathSteps; iteration++) {
-			physics::algorithms::heatbath(*gaugefield, *prng, overrelaxSteps);
-			measureGaugeObservables(iteration);
-			saveGaugefield(iteration);
-		}
-		logger.info() << "heatbath done";
-	}
-};
+	logger.info() << "heatbath done";
+}
 
 int main(int argc, const char* argv[])
 {
--- prog/heatbathExecutable.h
+++ prog/heatbathExecutable.h
@@ -13,4 +13,49 @@
 #include "physics/lattices/gaugefield.hpp"
 #include "physics/algorithms/heatbath.hpp"
 
+class heatbathExecutable: public generalExecutable
+{
+public:
+	/**
+	 * Constructor.
+	 * Initializes generalExecutable object, Random Number Generator and Iteration variables.
+	 */
+	heatbathExecutable(int argc, const char* argv[]);
+
+	/**
+	 * Performs heatbath algorithms (including thermalization) on gaugefield object and
+	 * measures of gauge observables on each iteration (not during thermalization).
+	 */
+	void performHeatbathAndMeasureGaugeObservables();
+
+private:
+	physics::PRNG * prng;
+	physics::lattices::Gaugefield * gaugefield;
+	const std::string 	filenameForHeatbathLogfile 		= "heatbath.log";
+	std::string filenameForGaugeobservables;
+	int thermalizationSteps;
+	int heatbathSteps;
+	int overrelaxSteps;
+	int writeFrequency;
+	int saveFrequency;
+
+	void writeHeatbathLogfile();
+
+	void setIterationParameters();
+
+	void performThermalization();
+
+	void writeGaugeObservablesToFile(int& iteration);
+
+	void writeGaugeObservablesToScreen(int& iteration);
+
+	void writeGaugeObservablesToScreenAndFile(int iteration);
+
+	void saveGaugefield(int iteration);
+
+	void measureGaugeObservables(int& iteration);
+
+	void performHeatbathAndMeasurements();
+};
+
 #endif /* HEATBATHEXECUTABLE_H_ */
