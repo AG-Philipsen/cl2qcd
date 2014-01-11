b4b243e421e4f358d35fcfc63e1a05274ab6e00b
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-01-11 20:19:38 +0100

    Added class dslashBenchmark
    refs 565

---
+++ executables/dslashBenchmark.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2012, 2013 Lars Zeidlewicz, Christopher Pinke,
+ * Matthias Bach, Christian Schäfer, Stefano Lottini, Alessandro Sciarra
+ *
+ * This file is part of CL2QCD.
+ *
+ * CL2QCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CL2QCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CL2QCD.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * @file
+ * Declaration of the hmcExecutable class.
+ * This class provides features for the generation of gauge configurations
+ * according to the Hybrid Monte Carlo (HMC) algorithm.
+ */
+
+#ifndef DSLASHBENCHMARK_H_
+#define DSLASHBENCHMARK_H_
+
+#include "benchmarkExecutable.h"
+//#include "../physics/algorithms/hmc.hpp"
+
+class dslashBenchmark : public benchmarkExecutable
+{
+public:
+  dslashBenchmark(int argc, const char* argv[]);
+
+protected:
+	/*
+	 * Calls the dslash_eo kernel a certain number of times.
+	 * Per iteration, the kernel is called with EVEN and ODD parameters.
+	 */
+	void performBenchmarkForSpecificKernels();
+};
+
+#endif /* HMCEXECUTABLE_H_ */
+
