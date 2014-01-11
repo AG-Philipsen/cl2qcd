b575a7325699163bdc0b5b835d776e288e545d4b
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-01-11 19:55:32 +0100

    Added benchmarkExecutable class
    refs 565

---
+++ executables/benchmarkExecutable.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2012, 2013 Lars Zeidlewicz, Christopher Pinke,
+ * Matthias Bach, Christian Schäfer, Stefano Lottini, Alessandro Sciarra
+ *
+ * This file is part of CL2QCD.
+ *
+ * CL2QCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CL2QCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CL2QCD.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * @file
+ * Declaration of the benchmarkExecutable class.
+ * This class provides features for the benchmarking of kernels.
+ */
+
+#ifndef BENCHMARKEXECUTABLE_H_
+#define BENCHMARKEXECUTABLE_H_
+
+#include "generalExecutable.h"
+
+class benchmarkExecutable : public generalExecutable
+{
+public:
+	benchmarkExecutable(int argc, const char* argv[]);
+
+	void benchmark();
+
+protected:
+	int benchmarkSteps;
+
+	virtual void performBenchmarkForSpecificKernels() {};
+};
+
+#endif /* BENCHMARKEXECUTABLE_H_ */
