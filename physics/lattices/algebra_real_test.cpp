60788ce3c1c99a72bcc7ccb86b35db188dde0b22
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2014-01-27 15:28:28 +0100

    Added tests for the physics/lattices/algebra_real package
    refs 564

--- physics/lattices/algebra_real_test.cpp
+++ physics/lattices/algebra_real_test.cpp
@@ -1,5 +1,5 @@
 /** @file
- * Unit test for the physics::lattices::Scalar<hmc_complex> class
+ * Unit test for the real algebra operations in physics::lattices
  *
  * Copyright 2012, 2013 Lars Zeidlewicz, Christopher Pinke,
  * Matthias Bach, Christian Schäfer, Stefano Lottini, Alessandro Sciarra
@@ -27,87 +27,75 @@
 
 // use the boost test framework
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE physics::lattice::Scalar<hmc_complex>
+#define BOOST_TEST_MODULE physics::lattices::algebra_real
 #include <boost/test/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
 
-/*
-BOOST_AUTO_TEST_CASE(initialization)
+
+void test_cgm_update(int switcher)
 {
+	//switch is to select among alpha, beta and zeta
 	using namespace physics::lattices;
 
 	const char * _params[] = {"foo"};
 	meta::Inputparameters params(1, _params);
 	hardware::System system(params);
 	logger.debug() << "Devices: " << system.get_devices().size();
-
+	
+	const int numeq = 16;
+	Vector<hmc_float> out(numeq, system);
+	Vector<hmc_float> v1(numeq, system);
+	Vector<hmc_float> v2(numeq, system);
+	Vector<hmc_float> v3(numeq, system);
+	Scalar<hmc_float> s1(system);
+	Scalar<hmc_float> s2(system);
+	Scalar<hmc_float> s3(system);
+	
+	std::vector<hmc_float> ref_v1(16, 1.);
+	std::vector<hmc_float> ref_v2(16, 2.);
+	std::vector<hmc_float> ref_v3(16, 3.);
+	std::vector<hmc_float> out_got(16);
+	hmc_float scalar = 3.14;
+	v1.store(ref_v1);
+	v2.store(ref_v2);
+	v3.store(ref_v3);
+	s1.store(scalar);
+	s2.store(scalar);
+	s3.store(scalar);
+	
+	if(switcher == 0){ //zeta
+	  update_zeta_cgm(&out, v1, v2, s1, s2, s3, v3, numeq);
+	  out_got = out.get();
+	  for(int i=0; i<numeq; i++)
+	    BOOST_REQUIRE_CLOSE(out_got[i], -0.145985401460, 1.e-8);
+	}
+	if(switcher == 1){ //beta
+	  update_beta_cgm(&out, s1, v1, v2, numeq);
+	  out_got = out.get();
+	  for(int i=0; i<numeq; i++)
+	    BOOST_REQUIRE_CLOSE(out_got[i], 1.57, 1.e-8);
+	}
+	if(switcher == 2){ //alpha
+	  update_alpha_cgm(&out, s1, v1, v2, v3, s2, numeq);
+	  out_got = out.get();
+	  for(int i=0; i<numeq; i++)
+	    BOOST_REQUIRE_CLOSE(out_got[i], 0.666666666667, 1.e-8);
+	}
+	
 }
 
 
-BOOST_AUTO_TEST_CASE(multiplication)
+BOOST_AUTO_TEST_CASE(update_zeta)
 {
-	using namespace physics::lattices;
-
-	const char * _params[] = {"foo"};
-	meta::Inputparameters params(1, _params);
-	hardware::System system(params);
-	logger.debug() << "Devices: " << system.get_devices().size();
-
-	Scalar<hmc_complex> left(system);
-	Scalar<hmc_complex> right(system);
-	Scalar<hmc_complex> res(system);
-
-	left.store( {1., 2.});
-	right.store( {3., 4.});
-
-	multiply(&res, left, right);
-
-	hmc_complex ref = { -5., 10.};
-	BOOST_REQUIRE_EQUAL(ref, res.get());
+	test_cgm_update(0);
 }
 
-BOOST_AUTO_TEST_CASE(division)
+BOOST_AUTO_TEST_CASE(update_beta)
 {
-	using namespace physics::lattices;
-
-	const char * _params[] = {"foo"};
-	meta::Inputparameters params(1, _params);
-	hardware::System system(params);
-	logger.debug() << "Devices: " << system.get_devices().size();
-
-	Scalar<hmc_complex> left(system);
-	Scalar<hmc_complex> right(system);
-	Scalar<hmc_complex> res(system);
-
-	left.store( {1., 2.});
-	right.store( {3., 4.});
-
-	divide(&res, left, right);
-
-	hmc_complex res_host = res.get();
-	BOOST_REQUIRE_CLOSE(0.44, res_host.re, 1);
-	BOOST_REQUIRE_CLOSE(0.08, res_host.im, 1);
+	test_cgm_update(1);
 }
 
-BOOST_AUTO_TEST_CASE(conversion)
+BOOST_AUTO_TEST_CASE(update_alpha)
 {
-	using namespace physics::lattices;
-
-	const char * _params[] = {"foo"};
-	meta::Inputparameters params(1, _params);
-	hardware::System system(params);
-	logger.debug() << "Devices: " << system.get_devices().size();
-
-	Scalar<hmc_float> real_dev(system);
-	Scalar<hmc_complex> complex_dev(system);
-
-	hmc_float real_host = 1.37;
-	hmc_complex complex_host = {2.34, 5.67};
-
-	real_dev.store(real_host);
-	complex_dev.store(complex_host);
-	convert(&complex_dev, real_dev);
-	hmc_complex ref = {real_host, 0.};
-	BOOST_REQUIRE_EQUAL(ref, complex_dev.get());
+	test_cgm_update(2);
 }
-*/
