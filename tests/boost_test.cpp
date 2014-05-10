20cbc759be92bb866fa135cb37de26fea2a92c00
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-05-10 13:34:47 +0200

    Refactoring of boost-test
    refs 657

--- tests/boost_test.cpp
+++ tests/boost_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012, 2013 Lars Zeidlewicz, Christopher Pinke,
+ * Copyright 2012, 2013, 2014 Lars Zeidlewicz, Christopher Pinke,
  * Matthias Bach, Christian Schäfer, Stefano Lottini, Alessandro Sciarra
  *
  * This file is part of CL2QCD.
@@ -24,54 +24,47 @@
 #define BOOST_TEST_MODULE staple_test
 #include <boost/test/unit_test.hpp>
 
-void printWorkaroundInfo(){
-	std::cout << "If this test fails, a workaround can be used that performs the test with std args." << std::endl;
-	std::cout << "To enable this, set" << std::endl;
-	std::cout << "\tBOOST_FAILURE_WORKAROUND" << std::endl;
-	std::cout << "to 1 in \"test_util.h\"" << std::endl;
+void printInfo(int expectedNumberOfParameters){
+	std::cout << "Testing passing of arguments from boost master_test_suite..." << std::endl;
+	std::cout << "Getting number of arguments, this should be equal to " << expectedNumberOfParameters << "!" << std::endl;
+	std::cout << "If this test fails, it might indicate a problem of boost with the used compiler..." << std::endl;
 }
 
-BOOST_AUTO_TEST_CASE( BOOST_ARGC_1 )
+void checkArgc(int expectedNumberOfParameters)
 {
-	int param_expect = 1;
-	std::cout << "Testing getting args from boost master_test_suite..." << std::endl;
-	std::cout << "Getting number of arguments, this should be equal to " << param_expect << "!" << std::endl;
-	printWorkaroundInfo();
-	int num_par = 	boost::unit_test::framework::master_test_suite().argc;
-	BOOST_REQUIRE_EQUAL(num_par, param_expect);
-	BOOST_MESSAGE("Test done");
+		printInfo(expectedNumberOfParameters);
+		int numberOfParametersFromBoost = 	boost::unit_test::framework::master_test_suite().argc;
+		BOOST_REQUIRE_EQUAL(numberOfParametersFromBoost, expectedNumberOfParameters);
 }
 
-BOOST_AUTO_TEST_CASE( BOOST_ARGC_2 )
-{
-	int param_expect = 2;
-	std::cout << "Testing getting args from boost master_test_suite..." << std::endl;
-	std::cout << "Getting number of arguments, this should be equal to " << param_expect << "!" << std::endl;
-	printWorkaroundInfo();
-	int num_par = 	boost::unit_test::framework::master_test_suite().argc;
-	BOOST_REQUIRE_EQUAL(num_par, param_expect);
-	BOOST_MESSAGE("Test done");
-}
+BOOST_AUTO_TEST_SUITE ( BOOST_ARGUMENTS )
 
-BOOST_AUTO_TEST_CASE( BOOST_ARGV )
-{
-	int param_expect = 4;
-	int num_par = 	boost::unit_test::framework::master_test_suite().argc;
-	printWorkaroundInfo();
-	BOOST_REQUIRE_EQUAL(param_expect, num_par);
-	if(num_par < param_expect){
-		std::cout << "Got only " << num_par << " Inputparameters, expected " << param_expect << "! Use inputfile values instead!"<< std::endl;
-	} else if(num_par > param_expect){
-		std::cout << "Got " << num_par << " Inputparameters, expected only " << param_expect << "! Use only the first " << param_expect << " values" << std::endl;
+	BOOST_AUTO_TEST_CASE( BOOST_ARGC_1 )
+	{
+		int expectedNumberOfParameters = 1;
+		checkArgc(expectedNumberOfParameters);
 	}
 	
-	//get input file location that has been passed as an argument
-	std::string inputfile_location = boost::unit_test::framework::master_test_suite().argv[1];
-	std::cout << "inputfile used: " << inputfile_location<< std::endl;
-	//get use_gpu = true/false that has been passed as an argument
-	std::string gpu_opt =  boost::unit_test::framework::master_test_suite().argv[2];
-	std::cout << "GPU usage: " << gpu_opt << std::endl;
-	//get use_rec12 = true/false that has been passed as an argument
-	std::string rec12_opt =  boost::unit_test::framework::master_test_suite().argv[3];
-	std::cout << "rec12 usage: " << rec12_opt << std::endl;
-}
+	BOOST_AUTO_TEST_CASE( BOOST_ARGC_2 )
+	{
+		int expectedNumberOfParameters = 2;
+		checkArgc(expectedNumberOfParameters);
+	}
+
+	void checkArgv(int position, std::string expectedContent)
+	{
+		std::string argument = boost::unit_test::framework::master_test_suite().argv[position];
+		BOOST_REQUIRE_EQUAL(argument, expectedContent);
+	}
+	
+	BOOST_AUTO_TEST_CASE( BOOST_ARGV )
+	{
+		int expectedNumberOfParameters = 4;
+		checkArgc(expectedNumberOfParameters);
+		
+		checkArgv(1, "firstArgument");
+		checkArgv(2, "secondArgument");
+		checkArgv(3, "thirdArgument");
+	}
+	
+BOOST_AUTO_TEST_SUITE_END()
