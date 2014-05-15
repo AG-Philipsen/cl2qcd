bdca4d851d35b5ce825797713e5de1dd5a2c73ed
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2014-05-15 17:13:55 +0200

    Added printing result and ref. value in kernelTester

--- hardware/code/kernelTester.cpp
+++ hardware/code/kernelTester.cpp
@@ -46,9 +46,12 @@ KernelTester::KernelTester(std::string kernelNameIn, std::string inputfileIn, in
 KernelTester::~KernelTester()
 {
 	for (int iteration = 0; iteration < (int) kernelResult.size(); iteration ++) {
+		logger.info() << "    Result = " << kernelResult[iteration];
+		logger.info() << "Ref. Value = " << referenceValue[iteration];
 		BOOST_CHECK_CLOSE(kernelResult[iteration], referenceValue[iteration], testPrecision);
 	}
 	delete parameters;
 	delete system;
+	device = NULL;
 }
 
