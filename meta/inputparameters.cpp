60fad0e7a465a6dc27b3c57c699929e6f6b67228
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-09-29 11:33:27 +0200

    Closes 715

--- meta/inputparameters.cpp
+++ meta/inputparameters.cpp
@@ -147,7 +147,7 @@ Inputparameters::Inputparameters(int argc, const char** argv, std::string parame
 		std::cout << desc << '\n';
 		throw Inputparameters::parse_aborted();
 	}
-
+	
 	if(vm.count("input-file")) {
 		std::string config_file = vm["input-file"].as<std::string>();
 		ConfigFileNormalizer normalizer;
@@ -170,18 +170,21 @@ Inputparameters::Inputparameters(int argc, const char** argv, std::string parame
 	// handle the enumeration types
 	_startcondition = ::get_startcondition(vm["startcondition"].as<std::string>());
 	gaugeact = ::get_action(vm["gaugeact"].as<std::string>());
-	fermact = ::get_action(vm["fermact"].as<std::string>());
-	fermact_mp = ::get_action(vm["fermact_mp"].as<std::string>());
+	if(parameterSet != "gaugeobservables" && parameterSet != "su3heatbath")
+	{
+		fermact = ::get_action(vm["fermact"].as<std::string>());
+		fermact_mp = ::get_action(vm["fermact_mp"].as<std::string>());
+		_solver = ::get_solver(vm["solver"].as<std::string>());
+		_solver_mp = ::get_solver(vm["solver_mp"].as<std::string>());
+		sourcetype = ::get_sourcetype(vm["sourcetype"].as<std::string>() );
+		sourcecontent = ::get_sourcecontent(vm["sourcecontent"].as<std::string>() );
+		pbp_version_ = ::get_pbp_version(vm["pbp_version"].as<std::string>() );
+	}
 	if(parameterSet != "inverter" && parameterSet != "gaugeobservables" && parameterSet != "su3heatbath"){
 	  integrator0 = ::get_integrator(vm["integrator0"].as<std::string>());
 	  integrator1 = ::get_integrator(vm["integrator1"].as<std::string>());
 	  integrator2 = ::get_integrator(vm["integrator2"].as<std::string>());
 	}
-	_solver = ::get_solver(vm["solver"].as<std::string>());
-	_solver_mp = ::get_solver(vm["solver_mp"].as<std::string>());
-	sourcetype = ::get_sourcetype(vm["sourcetype"].as<std::string>() );
-	sourcecontent = ::get_sourcecontent(vm["sourcecontent"].as<std::string>() );
-	pbp_version_ = ::get_pbp_version(vm["pbp_version"].as<std::string>() );
 }
 
 static meta::action get_action(std::string s)
