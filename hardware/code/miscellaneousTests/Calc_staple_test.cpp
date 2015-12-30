5ac61880c35da25c3b56fc81b1adf5b11b25151d
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2015-12-30 10:31:18 +0100

    Worked over localQ and staple tests

--- hardware/code/miscellaneousTests/Calc_staple_test.cpp
+++ hardware/code/miscellaneousTests/Calc_staple_test.cpp
@@ -23,28 +23,15 @@
 #define BOOST_TEST_MODULE staple_test
 #include <boost/test/unit_test.hpp>
 
-#include "../GaugefieldTester.hpp"
+#include "testCode.hpp"
 
-struct TestCode : public hardware::code::Opencl_Module
+struct StapleTestCode : public TestCode
 {
-	virtual size_t get_read_write_size(const std::string&) const
-	{
-		return 0;
-	};
-	virtual uint64_t get_flop_size(const std::string&) const
-	{
-		return 0;
-	};
-
-	TestCode(const hardware::code::OpenClKernelParametersInterface & kP, hardware::Device * device) :
-		Opencl_Module(kP, device)
+	StapleTestCode(const hardware::code::OpenClKernelParametersInterface & kP, hardware::Device * device):
+		TestCode(kP, device)
 	{
 		testKernel = createKernel("staple_test") << get_device()->getGaugefieldCode()->get_sources() << "../hardware/code/miscellaneousTests/staple_test.cl";
-	};
-	virtual ~TestCode()
-	{
-		clReleaseKernel(testKernel);
-	};
+	}
 
 	void runTestKernel(const hardware::buffers::SU3 * gf, const hardware::buffers::Plain<hmc_float> * out, const int gs, const int ls)
 	{
@@ -55,48 +42,16 @@ struct TestCode : public hardware::code::Opencl_Module
 
 		get_device()->enqueue_kernel(testKernel, gs, ls);
 	}
-	cl_kernel testKernel;
-	cl_int err;
 };
 
-struct StapleTester : public GaugefieldTester
+struct StapleTester : public OtherKernelTester
 {
 	StapleTester(const ParameterCollection pC, const GaugefieldTestParameters tP, const ReferenceValues rV):
-		GaugefieldTester("StapleTest", pC, tP, rV)
+		OtherKernelTester("StapleTest", pC, tP, rV)
 	{
-		testCode = new TestCode(pC.kernelParameters, device);
-		hardware::buffers::Plain<hmc_float> out (calculateGaugefieldSize(tP.latticeExtents), device);
-
-		if(device->get_device_type() == CL_DEVICE_TYPE_GPU)
-		{
-			gs = calculateGaugefieldSize(tP.latticeExtents);
-			ls = 64;
-		} else {
-			gs = device->get_num_compute_units();
-			ls = 1;
-		}
-
-		testCode->runTestKernel(GaugefieldTester::gaugefieldBuffer, &out, gs, ls);
-
-		host_out = new hmc_float[calculateGaugefieldSize(tP.latticeExtents)];
-		out.dump(host_out);
-
-		hmc_float result = 0;
-		for(int i = 0; i < calculateGaugefieldSize(tP.latticeExtents); i++) {
-			result += host_out[i];
-		}
-		kernelResult.at(0) = result;
+		testCode = new StapleTestCode(pC.kernelParameters, device);
+		testCode->runTestKernel(GaugefieldTester::gaugefieldBuffer, out, gs, ls);
 	}
-	~StapleTester()
-	{
-		if(testCode)
-			delete testCode;
-		if(host_out)
-			delete host_out;
-	}
-	TestCode * testCode;
-	int ls, gs;
-	hmc_float * host_out;
 };
 
 BOOST_AUTO_TEST_CASE( STAPLE_TEST )
