bd59dcdf1cfc1a8b5010537469a5853d28628931
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2018-03-07 14:35:12 +0100

    Fix format of correlators mathematica file

--- tools/correlators/correlators.m
+++ tools/correlators/correlators.m
@@ -1,127 +1,84 @@
 (* ::Package:: *)
 
-(* ::Input:: *)
-(*(**)
-(* * Copyright 2012,2013 Lars Zeidlewicz,Christopher Pinke,*)
-(* * Matthias Bach,Christian Sch\[ADoubleDot]fer,Stefano Lottini,Alessandro Sciarra*)
-(* **)
-(* * This file is part of CL2QCD.*)
-(* **)
-(* * CL2QCD is free software:you can redistribute it and/or modify*)
-(* * it under the terms of the GNU General Public License as published by*)
-(* * the Free Software Foundation,either version 3 of the License,or*)
-(* * (at your option) any later version.*)
-(* **)
-(* * CL2QCD is distributed in the hope that it will be useful,*)
-(* * but WITHOUT ANY WARRANTY;without even the implied warranty of*)
-(* * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the*)
-(* * GNU General Public License for more details.*)
-(* **)
-(* * You should have received a copy of the GNU General Public License*)
-(* * along with CL2QCD.If not,see<http://www.gnu.org/licenses/>.*)
-(**)*)
-(**)
-(*gamma5:=({*)
-(* {1, 0, 0, 0},*)
-(* {0, 1, 0, 0},*)
-(* {0, 0, -1, 0},*)
-(* {0, 0, 0, -1}*)
-(*})*)
+(*
+ * Copyright 2012,2013 Lars Zeidlewicz,Christopher Pinke,
+ * Matthias Bach,Christian Sch\[ADoubleDot]fer,Stefano Lottini,Alessandro Sciarra
+ *
+ * This file is part of CL2QCD.
+ *
+ * CL2QCD is free software:you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation,either version 3 of the License,or
+ * (at your option) any later version.
+ *
+ * CL2QCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CL2QCD. If not,see<http://www.gnu.org/licenses/>.
+*)
 
 
-(* ::Input:: *)
-(*gamma4:=({*)
-(* {0, 0, -1, 0},*)
-(* {0, 0, 0, -1},*)
-(* {-1, 0, 0, 0},*)
-(* {0, -1, 0, 0}*)
-(*})*)
+gamma5:=({
+ {1, 0, 0, 0},
+ {0, 1, 0, 0},
+ {0, 0, -1, 0},
+ {0, 0, 0, -1}
+})
 
+gamma4:=({
+ {0, 0, -1, 0},
+ {0, 0, 0, -1},
+ {-1, 0, 0, 0},
+ {0, -1, 0, 0}
+})
 
-(* ::Input:: *)
-(*gamma3:=({*)
-(* {0, 0, -I, 0},*)
-(* {0, 0, 0, I},*)
-(* {I, 0, 0, 0},*)
-(* {0, -I, 0, 0}*)
-(*})*)
+gamma3:=({
+ {0, 0, -I, 0},
+ {0, 0, 0, I},
+ {I, 0, 0, 0},
+ {0, -I, 0, 0}
+})
 
+gamma2:=({
+ {0, 0, 0, -1},
+ {0, 0, 1, 0},
+ {0, 1, 0, 0},
+ {-1, 0, 0, 0}
+})
 
-(* ::Input:: *)
-(*gamma2:=({*)
-(* {0, 0, 0, -1},*)
-(* {0, 0, 1, 0},*)
-(* {0, 1, 0, 0},*)
-(* {-1, 0, 0, 0}*)
-(*})*)
+gamma1:=({
+ {0, 0, 0, -I},
+ {0, 0, -I, 0},
+ {0, I, 0, 0},
+ {I, 0, 0, 0}
+})
 
+unit:=({
+ {1, 0, 0, 0},
+ {0, 1, 0, 0},
+ {0, 0, 1, 0},
+ {0, 0, 0, 1}
+})
 
-(* ::Input:: *)
-(*gamma1:=({*)
-(* {0, 0, 0, -I},*)
-(* {0, 0, -I, 0},*)
-(* {0, I, 0, 0},*)
-(* {I, 0, 0, 0}*)
-(*})*)
+anticomm[A_,B_]:=A.B+B.A
+correlator[G_,v_]:=-Tr[(Conjugate[v].G).(Transpose[v].G)]
+gamma41:=gamma4.gamma1
+gamma42:=gamma4.gamma2
+gamma43:=gamma4.gamma3
+gamma541:=gamma5.gamma41
+gamma542:=gamma5.gamma42
+gamma543:=gamma5.gamma43
 
+phi:=({
+ {p00, p01, p02, p03},
+ {p10, p11, p12, p13},
+ {p20, p21, p22, p23},
+ {p30, p31, p32, p33}
+})
 
-(* ::Input:: *)
-(*unit:=({*)
-(* {1, 0, 0, 0},*)
-(* {0, 1, 0, 0},*)
-(* {0, 0, 1, 0},*)
-(* {0, 0, 0, 1}*)
-(*})*)
-
-
-(* ::Input:: *)
-(*anticomm[A_,B_]:=A.B+B.A*)
-
-
-(* ::Input:: *)
-(*correlator[G_,v_]:=-Tr[(Conjugate[v].G).(Transpose[v].G)]*)
-
-
-(* ::Input:: *)
-(*gamma41:=gamma4.gamma1*)
-
-
-(* ::Input:: *)
-(*gamma42:=gamma4.gamma2*)
-
-
-(* ::Input:: *)
-(*gamma43:=gamma4.gamma3*)
-
-
-(* ::Input:: *)
-(*gamma541:=gamma5.gamma41*)
-
-
-(* ::Input:: *)
-(*gamma542:=gamma5.gamma42*)
-
-
-(* ::Input:: *)
-(*gamma543:=gamma5.gamma43*)
-
-
-(* ::Input:: *)
-(*phi:=({*)
-(* {p00, p01, p02, p03},*)
-(* {p10, p11, p12, p13},*)
-(* {p20, p21, p22, p23},*)
-(* {p30, p31, p32, p33}*)
-(*})*)
-
-
-(* ::Input:: *)
-(*correlator[unit,phi]*)
-
-
-(* ::Input:: *)
-(*correlator[gamma5,phi]*)
-
-
-(* ::Input:: *)
-(*correlator[gamma43,phi]*)
+correlator[unit,phi]
+correlator[gamma5,phi]
+correlator[gamma43,phi]
