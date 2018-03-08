360177b8cbe43ce6fc4d477b8ed3b706ca1d64ef
Alessandro Sciarra <sciarra@th.physik.uni-frankfurt.de>
2018-03-08 16:01:35 +0100

    Add -- to separate boost from user arguments
    In version 1.60 of Boost, user command line arguments should be
    given after boost arguments separating them by a --. Since doing it
    should still work with previous versions of boost (in the sense that an
    additional -- option is given to the test without being then used), we
    decided to go fot it.

--- hardware/hardwareTestUtilities.cpp
+++ hardware/hardwareTestUtilities.cpp
@@ -26,29 +26,28 @@ std::pair<bool, bool> checkForBoostRuntimeArguments()
 	bool useGpu = false;
 	bool useRec12 = false;
 	int num_par = boost::unit_test::framework::master_test_suite().argc;
-	if(num_par != 0)
-	{
+	if(num_par > 1) { //argv[0] is the executable name
+		/*
+		 * Here, if the boost version in use is previous to 1.60 but the user uses a "--" to separate boost arguments
+		 * from user arguments (as it is mandatory from version 1.60 on), there could be an argv[i] set to "--".
+		 * It is harmless now, but maybe not in the future, keep it in mind.
+		 */
 		logger.info() << "Found " << num_par << " runtime arguments, checking for gpu and rec12 options...";
-		for (int i = 1; i< num_par; i++)
-		{
+		for (int i = 1; i < num_par; i++) {
 			std::string currentArgument = boost::unit_test::framework::master_test_suite().argv[i];
-			if (currentArgument.find("--use_gpu") != std::string::npos)
-			{
-				if(currentArgument.find("true") != std::string::npos)
-				{
+			if (currentArgument.find("--use_gpu") != std::string::npos) {
+				if(currentArgument.find("true") != std::string::npos) {
 					useGpu = true;
 				}
 			}
-			if (currentArgument.find("--use_rec12") != std::string::npos)
-			{
-				if(currentArgument.find("true") != std::string::npos)
-				{
+			if (currentArgument.find("--use_rec12") != std::string::npos) {
+				if(currentArgument.find("true") != std::string::npos) {
 					useRec12 = true;
 				}
 			}
 		}
 	}
-	return std::pair<bool, bool>{useGpu, useRec12};
+	return std::pair<bool, bool> {useGpu, useRec12};
 }
 
 bool checkBoostRuntimeArgumentsForGpuUsage()
@@ -102,12 +101,10 @@ void endTestBecauseOfUnknownError()
 
 void handleExceptionInTest(hardware::OpenclException & exception)
 {
-	if ( checkIfNoOpenCLDevicesWereFound( exception ) )
-	{
+	if ( checkIfNoOpenCLDevicesWereFound( exception ) ) {
 		endTestAsNoDevicesWereFound();
 	}
-	else
-	{
+	else {
 		endTestBecauseOfUnknownError();
 	}
 }
