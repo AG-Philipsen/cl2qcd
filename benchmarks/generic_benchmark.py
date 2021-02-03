ed4df9d3b26fe7ac10a545b7ba4d712162631a54
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2021-02-03 15:42:55 +0100

    Improve generic benchmark python wrapper script

--- benchmarks/generic_benchmark.py
+++ benchmarks/generic_benchmark.py
@@ -26,19 +26,20 @@ import re
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Benchmark a kernel performance')
-	parser.add_argument('-k', '--kernel', required=True, choices=['dks', 'dslash', 'su3heatbath'], help='Benchmark kernel, required')
-	parser.add_argument('-p', '--path', default='./', help='Path to benchmark executable (default: ./')
-	parser.add_argument('-n', '--numDevices', default=1, type=int, help='Number of devices to use (default: 1)')
-	parser.add_argument('--ntMax', default=128, type=int, help='Maximal lattice extent in temporal direction (default: 128)')
-	parser.add_argument('--ntMin', default=4, type=int, help='Minimal lattice extent in temporal direction (default: 4)')
-	parser.add_argument('--ntIncr', default=4, type=int, help='Increment of lattice extent in temporal direction (default: 4)')
-	parser.add_argument('--nsMax', default=64, type=int, help='Maximal lattice extent in spatial direction (default: 64)')
-	parser.add_argument('--nsMin', default=16, type=int, help='Minimal lattice extent in spatial direction (default: 16)')
-	parser.add_argument('--nsIncr', default=8, type=int, help='Increment of lattice extent in spatial direction (default: 8)')
-	parser.add_argument('--calls', default=2000, type=int, help='Number of kernel calls (default: 2000)')
-	parser.add_argument('--outFilePrefix', default="benchmark_", help='Prefix for output file (default: benchmark_)')
-	parser.add_argument('--doNotUseGPU', default=False, action='store_true', help='Use CPU instead of GPU')
-	parser.add_argument('-q', '--quite', default=False, action='store_true', help='Print benchmark output to standard output')
+	parser.add_argument('-k', '--kernel', required=True, choices=['dks', 'dslash', 'su3heatbath'], help='Kernel to benchmark, required.')
+	parser.add_argument('-p', '--path', default='./', help='Path to benchmark executable (default: "./").')
+	parser.add_argument('-s', '--nSpace', nargs='+', type=int, help='Spacial extend of the lattice to use. It accepts multiple values.')
+	parser.add_argument('-t', '--nTime', nargs='+', type=int, help='Temporal extend of the lattice to use. It accepts multiple values.')
+	parser.add_argument('--calls', default=2500, type=int, help='Number of kernel calls (default: 2500).')
+	parser.add_argument('--outFilePrefix', default="benchmark_", help='Prefix for output file (default: "benchmark_").')
+	parser.add_argument('--doNotUseGPU', default=False, action='store_true', help='Use CPU instead of GPU.')
+	parser.add_argument('-q', '--quite', default=False, action='store_true', help='Print benchmark output to standard output.')
+	parser.add_argument('--keepAuxFiles', default=False, action='store_true', help='Do not remove CL2QCD auxiliary files.')
+
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('-n', '--numDevices', default=1, type=int, help='Number of devices to use.')
+	group.add_argument('-d', '--devices', nargs='+', type=int, help='Which devices to use.')
+
 	args = parser.parse_args()
 
 	# further setup based on command line options
@@ -58,17 +59,27 @@ if __name__ == '__main__':
 		useGPU = 'true'
 		useCPU = 'false'
 
+	space_dims = args.nSpace if args.nSpace else [16, 24, 32, 48]
+	time_dims = args.nTime if args.nTime else [4, 8, 12, 16, 24, 32, 64, 96, 128]
+
 	# open file for results
 	f = open(f'{args.outFilePrefix}{args.kernel}_{args.numDevices}_devices.dat', "w")
-	f.write("#Ns\tNt\tGFLOPS\t\tGB/s\t\ttime{msec}\n")
+	f.write("#{0:9}{1:15}{2:15}{3:15}{4:10}\n".format('Ns', 'Nt', 'GFLOPS', 'GB/s', 'ms/call'))
 
-	for Ns in range(args.nsMin, args.nsMax+1, args.nsIncr):
-		for Nt in range(args.ntMin, args.ntMax+1, args.ntIncr):
-			# do not simulate all sizes...
-			if Nt > 32:
-				if not Nt % 32 == 0:
-					continue
-			print(f'\n# Testing {labelOutput} kernel on {Ns}^3*{Nt} lattice with {args.numDevices} device(s)..."')
+	cl2qcd_command_line_common_options = [executable, f'--useGPU={useGPU}', f'--useCPU={useCPU}', f'--nBenchmarkIterations={args.calls}']
+	if args.numDevices != None:
+		cl2qcd_command_line_common_options += [f'--nDevices={args.numDevices}']
+	if args.devices != None:
+		for id in args.devices:
+			cl2qcd_command_line_common_options += [f'--deviceId={id}']
+
+	benchmark_number = 0
+	total_number_of_benchmarks = len(space_dims) * len(time_dims)
+	for Ns in space_dims:
+		for Nt in time_dims:
+			benchmark_number += 1
+			print(f'\n# Benchmark {benchmark_number} of {total_number_of_benchmarks}')
+			print(f'# Testing {labelOutput} kernel on {Nt}x{Ns}^3 lattice on {args.numDevices} device(s) [{args.calls} calls]')
 			if not args.quite:
 				print("# CL2QCD output:\n")
 
@@ -76,7 +87,8 @@ if __name__ == '__main__':
 			if not os.path.isfile(executable):
 				raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), executable)
 
-			p = subprocess.Popen([executable, f'--nSpace={Ns}', f'--nTime={Nt}', f'--useGPU={useGPU}', f'--useCPU={useCPU}', f'--nDevices={args.numDevices}', f'--nBenchmarkIterations={args.calls}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			cl2qcd_command_line_options = cl2qcd_command_line_common_options + [f'--nSpace={Ns}', f'--nTime={Nt}']
+			p = subprocess.Popen(cl2qcd_command_line_options, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			out, err = p.communicate()
 			out = out.decode('utf8')
 			lines = out.split("\n")
@@ -119,7 +131,16 @@ if __name__ == '__main__':
 				print(f'# Measured device time per call of {time} ms')
 			else:
 				print(f'# Measured hosttime per call of {time} ms')
-			f.write(f'{Ns}\t{Nt}\t{performance}\t\t{memory}\t\t{time}\n')
+			f.write(f'{Ns:<10d}{Nt:<15}{performance:<15.3f}{memory:<15.3f}{time:<10.3f}\n')
 
 	print('')
 	f.close()
+
+	# Remove auxiliary files of CL2QCD
+	if not args.keepAuxFiles:
+		try:
+			os.remove(f'{args.kernel}_benchmark.log')
+			os.remove("general_time_output")
+			os.remove(f'{args.kernel}_benchmark_profiling_data')
+		except OSError as e:  ## if failed, report it back to the user ##
+			print (f'Error: {e.filename} - {e.strerror}.')
